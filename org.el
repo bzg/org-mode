d8b7e8a87a5db2ccbc21871e12cbabd1d3ca82cb
Carsten Dominik <carsten.dominik@gmail.com>
2008-02-01 23:30:07 +0100

    org-plain-link-re has been improved to allow ; and , in URL's.

--- org.el
+++ org.el
@@ -5035,7 +5035,7 @@ This should be called after the variable `org-link-types' has changed."
 	org-plain-link-re
 	(concat
 	 "\\<\\(" (mapconcat 'identity org-link-types "\\|") "\\):"
-	 "\\([^]\t\n\r<>,;() ]+\\)")
+	 "\\([^]\t\n\r<>() ]+[^]\t\n\r<>,.;() ]\\)")
 	org-bracket-link-regexp
 	"\\[\\[\\([^][]+\\)\\]\\(\\[\\([^][]+\\)\\]\\)?\\]"
 	org-bracket-link-analytic-regexp
