9bca3981f7f391be29bbd547f759a2cf04eb1734
Carsten Dominik <carsten.dominik@gmail.com>
2008-02-19 09:18:46 +0100

    Release 5.22a.

--- org.el
+++ org.el
@@ -5,7 +5,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 5.22
+;; Version: 5.22a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
@@ -84,7 +84,7 @@
 
 ;;; Version
 
-(defconst org-version "5.22"
+(defconst org-version "5.22a"
   "The version number of the file org.el.")
 
 (defun org-version (&optional here)
