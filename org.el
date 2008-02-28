d0fafb8828285b8eb4339d64b529319e70ca4417
Bastien Guerry <bzg@altern.org>
2008-02-28 17:12:11 +0000

    (org-mouse-1-follows-link): Use the value of `mouse-1-click-follows-link' by default.

--- org.el
+++ org.el
@@ -1255,7 +1255,7 @@ Needs to be set before org.el is loaded."
   :group 'org-link-follow
   :type 'boolean)
 
-(defcustom org-mouse-1-follows-link t
+(defcustom org-mouse-1-follows-link mouse-1-click-follows-link
   "Non-nil means, mouse-1 on a link will follow the link.
 A longer mouse click will still set point.  Does not work on XEmacs.
 Needs to be set before org.el is loaded."
