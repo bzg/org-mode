3885aa55d5b3422e087c1904687bce27723a8996
Carsten Dominik <carsten.dominik@gmail.com>
2008-03-28 10:26:54 +0100

    Pushed version number to 6.00pre-1

--- UTILITIES/set-version.pl
+++ UTILITIES/set-version.pl
@@ -8,17 +8,17 @@ print STDERR "Changing version to \"$version\" and date to \"$date\" in all rele
 print STDERR join("\n",glob("lisp/*.el")),"\n";
 $cmd = qq{s/^(;; Version:)\\s+(\\S+)[ \t]*\$/\$1 $version/;s/^(\\(defconst org-version )"(\\S+)"/\$1"$version"/};
 $c1 = "perl -pi -e '$cmd' lisp/*.el";
-#system($c1);
+system($c1);
 
 print STDERR "doc/org.texi\n";
 $cmd = qq{s/^(\\\@set VERSION)\\s+(\\S+)[ \t]*\$/\$1 $version/;s/^(\\\@set DATE)\\s+(.*)\$/\$1 $date/;};
 $c1 = "perl -pi -e '$cmd' doc/org.texi";
-#system($c1);
+system($c1);
 
 print STDERR "doc/orgcard.tex\n";
 $cmd = qq{s/^\\\\def\\\\orgversionnumber\\{\\S+\\}/\\\\def\\\\orgversionnumber{$version}/;s/\\\\def\\\\versionyear\\{\\S+\\}/\\\\def\\\\versionyear{$year}/;s/\\\\def\\\\year\\{\\S+\\}/\\\\def\\\\year{$year}/;};
 $c1 = "perl -pi -e '$cmd' doc/orgcard.tex";
-#system($c1);
+system($c1);
 
 
 
--- lisp/org-bbdb.el
+++ lisp/org-bbdb.el
@@ -5,7 +5,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 1.0
+;; Version: 6.00pre-1
 ;;
 ;; This file is part of GNU Emacs.
 ;;
