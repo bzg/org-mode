3798fbb8f5575a8ecad620a5f2a5be22b7fc20c3
Carsten Dominik <carsten.dominik@gmail.com>
2008-04-18 18:03:08 +0200

    New version number, effort estimates work now.

--- lisp/org-faces.el
+++ lisp/org-faces.el
@@ -5,7 +5,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.02pre02
+;; Version: 6.02pre-03
 ;;
 ;; This file is part of GNU Emacs.
 ;;
