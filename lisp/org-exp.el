66f801cbffa4e737c759ad76b0357755436ccef8
Carsten Dominik <carsten.dominik@gmail.com>
2008-05-08 17:17:58 +0200

    Version number change to 6.03pre01.
    Yes, we are moving to the 6.03 release.

--- lisp/org-exp.el
+++ lisp/org-exp.el
@@ -5,7 +5,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.02b
+;; Version: 6.03pre01
 ;;
 ;; This file is part of GNU Emacs.
 ;;
