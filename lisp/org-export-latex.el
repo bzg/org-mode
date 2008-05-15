b4159c50173b2e6a4b01acea8cc59c33f561ad92
Carsten Dominik <carsten.dominik@gmail.com>
2008-05-15 04:40:55 +0200

    Release 6.03.

--- lisp/org-export-latex.el
+++ lisp/org-export-latex.el
@@ -4,7 +4,7 @@
 ;;
 ;; Emacs Lisp Archive Entry
 ;; Filename: org-export-latex.el
-;; Version: 6.03pre03
+;; Version: 6.03
 ;; Author: Bastien Guerry <bzg AT altern DOT org>
 ;; Maintainer: Bastien Guerry <bzg AT altern DOT org>
 ;; Keywords: org, wp, tex
--- lisp/org-irc.el
+++ lisp/org-irc.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Philip Jackson <emacs@shellarchive.co.uk>
 ;; Keywords: erc, irc, link, org
-;; Version: 6.03pre03
+;; Version: 6.03
 ;;
 ;; This file is part of GNU Emacs.
 ;;
