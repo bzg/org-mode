177e154e6c6a3f64ce50f588ccb533a4452daac3
Carsten Dominik <carsten.dominik@gmail.com>
2008-07-24 06:42:03 -0700

    Release 6.06a.

--- lisp/org.el
+++ lisp/org.el
@@ -5,7 +5,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.06
+;; Version: 6.06a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
@@ -91,7 +91,7 @@
 
 ;;; Version
 
-(defconst org-version "6.06"
+(defconst org-version "6.06a"
   "The version number of the file org.el.")
 
 (defun org-version (&optional here)
