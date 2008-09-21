a8b3081610c356514929752dd2d0ca847e767db5
Carsten Dominik <carsten.dominik@gmail.com>
2008-09-21 08:26:30 +0200

    Release 6.07b.

--- lisp/org-mac-message.el
+++ lisp/org-mac-message.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2008 Free Software Foundation, Inc.
 
 ;; Author: John Wiegley <johnw@gnu.org>
-;; Version: 6.07a
+;; Version: 6.07b
 ;; Keywords: outlines, hypermedia, calendar, wp
 
 ;; This file is part of GNU Emacs.
