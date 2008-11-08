c70827980952abdff0edf177371042806271c0d4
Carsten Dominik <carsten.dominik@gmail.com>
2008-11-08 16:43:48 +0100

    Release 6.11

--- lisp/org-bibtex.el
+++ lisp/org-bibtex.el
@@ -5,7 +5,7 @@
 ;; Author: Bastien Guerry <bzg at altern dot org>
 ;;         Carsten Dominik <carsten dot dominik at gmail dot com>
 ;; Keywords: org, wp, remember
-;; Version: 6.11pre01
+;; Version: 6.11
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-list.el
+++ lisp/org-list.el
@@ -6,7 +6,7 @@
 ;;         Bastien Guerry <bzg AT altern DOT org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.11pre01
+;; Version: 6.11
 ;;
 ;; This file is part of GNU Emacs.
 ;;
