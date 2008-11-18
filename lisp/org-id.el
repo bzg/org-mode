9f1e6fa95264abb84a0afd04f55b175210f1c0e1
Carsten Dominik <carsten.dominik@gmail.com>
2008-11-18 08:59:39 +0100

    Avoid error when ID is created in buffer without a file.
    When ID's are created, Org tries to remember in which file the id is
    located.  However, if an ID is created in a remember buffer, no file
    is known.  This patch makes sure that this condition does not lead to
    an error.

--- lisp/org-id.el
+++ lisp/org-id.el
@@ -376,15 +376,16 @@ Store the relation between files and corresponding ID's."
 
 (defun org-id-add-location (id file)
   "Add the ID with location FILE to the database of ID loations."
-  (unless org-id-locations (org-id-locations-load))
-  (catch 'exit
-    (let ((locs org-id-locations) list)
-      (while (setq list (pop locs))
-	(when (equal (file-truename file) (file-truename (car list)))
-	  (setcdr list (cons id (cdr list)))
-	  (throw 'exit t))))
-    (push (list file id) org-id-locations))
-  (org-id-locations-save))
+  (when (and id file) ; don't error when called from a buffer with no file
+    (unless org-id-locations (org-id-locations-load))
+    (catch 'exit
+      (let ((locs org-id-locations) list)
+	(while (setq list (pop locs))
+	  (when (equal (file-truename file) (file-truename (car list)))
+	    (setcdr list (cons id (cdr list)))
+	    (throw 'exit t))))
+      (push (list file id) org-id-locations))
+    (org-id-locations-save)))
 
 ;; Finding entries with specified id
 
