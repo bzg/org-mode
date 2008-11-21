db5c609be967e811e5f0689f4411f59cd072d0c8
Carsten Dominik <carsten.dominik@gmail.com>
2008-11-21 13:35:45 +0100

    6.13pre04

--- lisp/org-bibtex.el
+++ lisp/org-bibtex.el
@@ -5,7 +5,7 @@
 ;; Author: Bastien Guerry <bzg at altern dot org>
 ;;         Carsten Dominik <carsten dot dominik at gmail dot com>
 ;; Keywords: org, wp, remember
-;; Version: 6.13pre03
+;; Version: 6.13pre04
 ;;
 ;; This file is part of GNU Emacs.
 ;;
