5810f0a8c9c9e2f2001a3632d6c8f749ecb8bc21
Carsten Dominik <carsten.dominik@gmail.com>
2008-11-22 10:25:55 +0100

    6.13pre05
    Getting close.....

--- lisp/org-agenda.el
+++ lisp/org-agenda.el
@@ -6,7 +6,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.13pre04
+;; Version: 6.13pre05
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-attach.el
+++ lisp/org-attach.el
@@ -4,7 +4,7 @@
 
 ;; Author: John Wiegley <johnw@newartisans.com>
 ;; Keywords: org data task
-;; Version: 6.13pre04
+;; Version: 6.13pre05
 
 ;; This file is part of GNU Emacs.
 ;;
