cf75cdb7f32b9ba88de8607d1c4c52bdcef144da
Carsten Dominik <carsten.dominik@gmail.com>
2008-12-10 23:43:24 +0100

    Fix bugs with new figure <div>.

--- lisp/org-exp.el
+++ lisp/org-exp.el
@@ -3716,15 +3716,14 @@ lang=\"%s\" xml:lang=\"%s\">
 	   (attr (org-find-text-property-in-string 'org-attributes src))
 	   (label (org-find-text-property-in-string 'org-label src)))
       (format "<div %sclass=\"figure\">
-<p><img src=\"%s\"%s>
-%s
+<p><img src=\"%s\"%s></p>%s
 </div>"
 	      (if label (format "id=\"%s\" " label) "")
 	      src
 	      (if (string-match "\\<alt=" (or attr ""))
 		  (concat " " attr )
 		(concat " " attr " alt=\"" src "\""))
-	      (if caption (concat "<p>" caption))))))
+	      (if caption (concat "\n<p>" caption "</p>") "")))))
 
 (defvar org-table-colgroup-info nil)
 (defun org-format-table-ascii (lines)
