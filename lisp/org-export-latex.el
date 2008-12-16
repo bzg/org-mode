96d9a2e4672a8f929a73225436774ce08a11cf4b
Carsten Dominik <carsten.dominik@gmail.com>
2008-12-16 10:40:42 +0100

    Release 6.15a

--- lisp/org-export-latex.el
+++ lisp/org-export-latex.el
@@ -4,7 +4,7 @@
 ;;
 ;; Emacs Lisp Archive Entry
 ;; Filename: org-export-latex.el
-;; Version: 6.15
+;; Version: 6.15a
 ;; Author: Bastien Guerry <bzg AT altern DOT org>
 ;; Maintainer: Bastien Guerry <bzg AT altern DOT org>
 ;; Keywords: org, wp, tex
