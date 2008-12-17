181235f605a475e57368da4392fdd610e334e942
Carsten Dominik <carsten.dominik@gmail.com>
2008-12-17 09:59:25 +0100

    Release 6.15c.

--- lisp/org-plot.el
+++ lisp/org-plot.el
@@ -5,7 +5,7 @@
 ;; Author: Eric Schulte <schulte dot eric at gmail dot com>
 ;; Keywords: tables, plotting
 ;; Homepage: http://orgmode.org
-;; Version: 6.15b
+;; Version: 6.15c
 ;;
 ;; This file is part of GNU Emacs.
 ;;
