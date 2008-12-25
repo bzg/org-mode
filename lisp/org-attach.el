20f2f8e1e1c9939a163046769b555bd8a52304c0
Carsten Dominik <carsten.dominik@gmail.com>
2008-12-25 11:06:35 +0100

    Release 6.16c

--- lisp/org-attach.el
+++ lisp/org-attach.el
@@ -4,7 +4,7 @@
 
 ;; Author: John Wiegley <johnw@newartisans.com>
 ;; Keywords: org data task
-;; Version: 6.16trans
+;; Version: 6.16c
 
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-mouse.el
+++ lisp/org-mouse.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Piotr Zielinski <piotr dot zielinski at gmail dot com>
 ;; Maintainer: Carsten Dominik <carsten at orgmode dot org>
-;; Version: 6.16trans
+;; Version: 6.16c
 ;;
 ;; This file is part of GNU Emacs.
 ;;
