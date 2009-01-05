35c0798d697e04d2f854e6535a63c0f6be746df6
Carsten Dominik <carsten.dominik@gmail.com>
2009-01-05 13:25:58 +0100

    Release 6.17a

--- lisp/org-mouse.el
+++ lisp/org-mouse.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Piotr Zielinski <piotr dot zielinski at gmail dot com>
 ;; Maintainer: Carsten Dominik <carsten at orgmode dot org>
-;; Version: 6.17
+;; Version: 6.17a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
