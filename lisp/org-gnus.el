19351e35c20cd6dd292069f2d51318ae467a5ff3
Carsten Dominik <carsten.dominik@gmail.com>
2009-01-21 17:59:42 +0100

    Version number: pushed to 6.18trans

--- lisp/org-gnus.el
+++ lisp/org-gnus.el
@@ -6,7 +6,7 @@
 ;;         Tassilo Horn <tassilo at member dot fsf dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.18c
+;; Version: 6.18trans
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-irc.el
+++ lisp/org-irc.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Philip Jackson <emacs@shellarchive.co.uk>
 ;; Keywords: erc, irc, link, org
-;; Version: 6.18c
+;; Version: 6.18trans
 ;;
 ;; This file is part of GNU Emacs.
 ;;
