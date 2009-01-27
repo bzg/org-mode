c848dae4be45ac430c7f6fa0ffc1802986cb9e08
Carsten Dominik <carsten.dominik@gmail.com>
2009-01-27 09:02:13 +0100

    Release 6.19c

--- lisp/org-bibtex.el
+++ lisp/org-bibtex.el
@@ -5,7 +5,7 @@
 ;; Author: Bastien Guerry <bzg at altern dot org>
 ;;         Carsten Dominik <carsten dot dominik at gmail dot com>
 ;; Keywords: org, wp, remember
-;; Version: 6.19b
+;; Version: 6.19c
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-irc.el
+++ lisp/org-irc.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Philip Jackson <emacs@shellarchive.co.uk>
 ;; Keywords: erc, irc, link, org
-;; Version: 6.19b
+;; Version: 6.19c
 ;;
 ;; This file is part of GNU Emacs.
 ;;
