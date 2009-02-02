93ec228f363ba8b6f76409cd7a2f817394a41331
Carsten Dominik <carsten.dominik@gmail.com>
2009-02-03 00:06:22 +0100

    Push version number to 6.21trans

--- lisp/org-attach.el
+++ lisp/org-attach.el
@@ -4,7 +4,7 @@
 
 ;; Author: John Wiegley <johnw@newartisans.com>
 ;; Keywords: org data task
-;; Version: 6.21b
+;; Version: 6.21trans
 
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-list.el
+++ lisp/org-list.el
@@ -7,7 +7,7 @@
 ;;	   Bastien Guerry <bzg AT altern DOT org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.21b
+;; Version: 6.21trans
 ;;
 ;; This file is part of GNU Emacs.
 ;;
