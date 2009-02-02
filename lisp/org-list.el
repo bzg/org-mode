2fa17ebf74ff1bd8ff8f1dc4baec950fb4466fdb
Carsten Dominik <carsten.dominik@gmail.com>
2009-02-02 14:23:54 +0100

    Release 6.21a

--- lisp/org-list.el
+++ lisp/org-list.el
@@ -7,7 +7,7 @@
 ;;	   Bastien Guerry <bzg AT altern DOT org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.21
+;; Version: 6.21a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-w3m.el
+++ lisp/org-w3m.el
@@ -5,7 +5,7 @@
 ;; Author: Andy Stewart <lazycat dot manatee at gmail dot com>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.21
+;; Version: 6.21a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
