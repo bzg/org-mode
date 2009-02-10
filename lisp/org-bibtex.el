e592c778468c0877e2ac0dbcf446610ccb5d1ce2
Carsten Dominik <carsten.dominik@gmail.com>
2009-02-10 10:11:23 +0100

    Release 6.22a

--- lisp/org-bibtex.el
+++ lisp/org-bibtex.el
@@ -5,7 +5,7 @@
 ;; Author: Bastien Guerry <bzg at altern dot org>
 ;;         Carsten Dominik <carsten dot dominik at gmail dot com>
 ;; Keywords: org, wp, remember
-;; Version: 6.22
+;; Version: 6.22a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-mac-message.el
+++ lisp/org-mac-message.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2008, 2009 Free Software Foundation, Inc.
 
 ;; Author: John Wiegley <johnw@gnu.org>
-;; Version: 6.22
+;; Version: 6.22a
 ;; Keywords: outlines, hypermedia, calendar, wp
 
 ;; This file is part of GNU Emacs.
