722fc9a490e309ec31a77b2557427c10915750d3
Carsten Dominik <carsten.dominik@gmail.com>
2009-03-14 08:03:10 +0100

    Version number to 6.24trans

--- lisp/org-mouse.el
+++ lisp/org-mouse.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Piotr Zielinski <piotr dot zielinski at gmail dot com>
 ;; Maintainer: Carsten Dominik <carsten at orgmode dot org>
-;; Version: 6.24c
+;; Version: 6.24trans
 ;;
 ;; This file is part of GNU Emacs.
 ;;
