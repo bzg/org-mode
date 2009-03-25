da6ab1279e6a5ea97caab1ed63bb0971518c7d7d
Eric Schulte <schulte.eric@gmail.com>
2009-03-25 14:42:57 -0700

    added stub of the file which will provide R support

---
+++ litorgy/litorgy-R.el
@@ -0,0 +1,46 @@
+;;; litorgy-R.el --- litorgy functions for R code evaluation
+
+;; Copyright (C) 2009 Eric Schulte, Dan Davison, Austin F. Frank
+
+;; Author: Eric Schulte, Dan Davison, Austin F. Frank
+;; Keywords: literate programming, reproducible research
+;; Homepage: http://orgmode.org
+;; Version: 0.01
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; Litorgy support for evaluating R code
+
+;;; Code:
+(require 'litorgy)
+
+(litorgy-add-interpreter "emacs-R")
+
+(defun litorgy-execute:emacs-R (body params)
+  "Execute a block of emacs-R code with litorgy.  This
+function is called by `litorgy-execute-src-block'."
+  (save-window-excursion
+    (let ((vars (litorgy-reference-variables params))
+          results)
+      ;;TODO: implement
+      (error "`litorgy-execute:emacs-R' is not implemented"))))
+
+(provide 'litorgy-R)
+;;; litorgy-R.el ends here
