c442366e233a2081b75a17861a06fc6d9c4a2167
Carsten Dominik <carsten.dominik@gmail.com>
2009-03-30 20:02:07 +0200

    Minor fix.

--- lisp/org.el
+++ lisp/org.el
@@ -14629,7 +14629,7 @@ With optional NODE, go directly to that node."
   (interactive)
   (mapc 'require
 	'(org-agenda org-archive org-attach org-clock org-colview
-		     org-exp org-id org-export-latex org-docbook.el
+		     org-exp org-id org-export-latex org-docbook
 		     org-publish org-remember org-table org-timer)))
 
 ;;;###autoload
