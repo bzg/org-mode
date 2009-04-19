b8a5f1a7fcb5e1806bad9a71048f249dbcb79e97
Carsten Dominik <carsten.dominik@gmail.com>
2009-04-19 22:31:36 +0200

    Release 6.26a

--- lisp/org-attach.el
+++ lisp/org-attach.el
@@ -4,7 +4,7 @@
 
 ;; Author: John Wiegley <johnw@newartisans.com>
 ;; Keywords: org data task
-;; Version: 6.26
+;; Version: 6.26a
 
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-mew.el
+++ lisp/org-mew.el
@@ -5,7 +5,7 @@
 ;; Author: Tokuya Kameshima <kames at fa2 dot so-net dot ne dot jp>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.26
+;; Version: 6.26a
 
 ;; This file is part of GNU Emacs.
 
