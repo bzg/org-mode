ba66d2c8aed9b2a7c1f66c48368f0eded15480db
Carsten Dominik <carsten.dominik@gmail.com>
2009-04-19 08:38:49 +0200

    Release 6.26

--- lisp/org-mac-message.el
+++ lisp/org-mac-message.el
@@ -5,7 +5,7 @@
 ;; Author: John Wiegley <johnw@gnu.org>
 ;;         Christopher Suckling <suckling at gmail dot com>
 
-;; Version: 6.25trans
+;; Version: 6.26
 ;; Keywords: outlines, hypermedia, calendar, wp
 
 ;; This file is part of GNU Emacs.
--- lisp/org.el
+++ lisp/org.el
@@ -6,7 +6,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.25trans
+;; Version: 6.26
 ;;
 ;; This file is part of GNU Emacs.
 ;;
@@ -94,7 +94,7 @@
 
 ;;; Version
 
-(defconst org-version "6.25trans"
+(defconst org-version "6.26"
   "The version number of the file org.el.")
 
 (defun org-version (&optional here)
