69e6d249aaf76784bfb871b8376425be0c0b3980
Carsten Dominik <carsten.dominik@gmail.com>
2009-04-22 09:08:13 +0200

    Push version number to 6.26trans

--- lisp/org-bibtex.el
+++ lisp/org-bibtex.el
@@ -5,7 +5,7 @@
 ;; Author: Bastien Guerry <bzg at altern dot org>
 ;;         Carsten Dominik <carsten dot dominik at gmail dot com>
 ;; Keywords: org, wp, remember
-;; Version: 6.26d
+;; Version: 6.26trans
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-gnus.el
+++ lisp/org-gnus.el
@@ -7,7 +7,7 @@
 ;;         Tassilo Horn <tassilo at member dot fsf dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.26d
+;; Version: 6.26trans
 ;;
 ;; This file is part of GNU Emacs.
 ;;
