08f92aebe5ffa4d2a5d23a1597fb97601f3c6a5d
Eric Schulte <schulte.eric@gmail.com>
2009-06-13 19:35:48 -0700

    passing some session tests, but still failing some tests

--- lisp/langs/org-babel-ruby.el
+++ lisp/langs/org-babel-ruby.el
@@ -70,6 +70,7 @@ specifying a var of the same value."
 (defun org-babel-ruby-table-or-results (results)
   "If the results look like a table, then convert them into an
 Emacs-lisp table, otherwise return the results as a string."
+  (message "results are %s" results)
   (org-babel-read
    (if (string-match "^\\[.+\\]$" results)
        (org-babel-read
