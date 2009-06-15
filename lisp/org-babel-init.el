623831ddabd322f2045598547056108a3435e072
Eric Schulte <schulte.eric@gmail.com>
2009-06-14 18:42:44 -0700

    added util directory, including newest inf-ruby.el, should fix ruby bug

--- lisp/org-babel-init.el
+++ lisp/org-babel-init.el
@@ -31,13 +31,13 @@
 ;;; Code:
 (require 'org)
 (require 'org-exp-blocks)
-(load "htmlize.el") ;; other versions of htmlize can cause export problems
 (require 'org-babel)
 (require 'org-babel-ref)
 (require 'org-babel-exp)
 (require 'org-babel-table)
 (require 'org-babel-comint)
 (require 'org-babel-lob)
+
 ;; language specific files
 (add-to-list 'load-path (expand-file-name "langs" (file-name-directory (or load-file-name buffer-file-name))))
 (require 'org-babel-ruby)
@@ -45,6 +45,17 @@
 (require 'org-babel-sh)
 (require 'org-babel-lisp)
 (require 'org-babel-R)
+
+;; load utility files
+(let ((util-dir (expand-file-name
+                 "util"
+                 (expand-file-name
+                  ".."
+                  (file-name-directory (or load-file-name buffer-file-name))))))
+  ;; other versions of htmlize can cause export problems
+  (load (expand-file-name "htmlize.el" util-dir))
+  ;; the newest version of inf-ruby.el is required by org-babel-ruby.el
+  (load (expand-file-name "inf-ruby.el" util-dir)))
 
 (provide 'org-babel-init)
 ;;; org-babel-init.el ends here
