fe4967229a4c981417110e7505d09485461e58f5
Bastien Guerry <bzg@altern.org>
2009-07-16 21:39:37 +0200

    Fix problem when publishing the region to LaTeX.
    `org-export-latex-first-lines' was rather stupid and would
    discard the end of the region with the region was active.
    Thanks to Holst Thomas for this bug report.

--- lisp/org-latex.el
+++ lisp/org-latex.el
@@ -960,9 +960,10 @@ If END is non-nil, it is the end of the region."
   (save-excursion
     (goto-char (or beg (point-min)))
     (let* ((pt (point))
-	   (end (if (re-search-forward "^\\*+ " end t)
-		    (goto-char (match-beginning 0))
-		  (goto-char (point-max)))))
+	   (end (or end
+		    (if (re-search-forward "^\\*+ " end t)
+			(goto-char (match-beginning 0))
+		      (goto-char (point-max))))))
       (prog1
 	  (org-export-latex-content
 	   (org-export-preprocess-string
