1c25048e48433cae87522de324f33f745071fa86
Carsten Dominik <carsten.dominik@gmail.com>
2009-08-04 23:54:22 +0200

    Release 6.29a

--- lisp/org-src.el
+++ lisp/org-src.el
@@ -7,7 +7,7 @@
 ;;	   Bastien Guerry <bzg AT altern DOT org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.29
+;; Version: 6.29a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-w3m.el
+++ lisp/org-w3m.el
@@ -5,7 +5,7 @@
 ;; Author: Andy Stewart <lazycat dot manatee at gmail dot com>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.29
+;; Version: 6.29a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
