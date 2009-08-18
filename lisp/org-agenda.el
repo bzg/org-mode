bzg/org-mode

commit 8c177dc832980bd8cf23fc2ae72b18e0b472b59e

Avoid text properties on elements in custom variables

lisp/org-agenda.el
@@ -1806,7 +1806,8 @@ s   Search for keywords                 C   Configure custom agenda commands
 	  (delete-region (point) (point-max))
 	  (while (setq entry (pop custom1))
 	    (setq key (car entry) desc (nth 1 entry)
-		  type (nth 2 entry) match (nth 3 entry))
+		  type (nth 2 entry)
+		  match (org-no-properties (copy-sequence (nth 3 entry))))
 	    (if (> (length key) 1)
 		(add-to-list 'prefixes (string-to-char key))
 	      (insert

lisp/org.el
@@ -11241,7 +11241,7 @@ Returns the new tags string, or nil to not change the current settings."
 	      (insert "\n")
 	      (setq tbl (cdr tbl)))))
 	 (t
-	  (setq tg (car e) c2 nil)
+	  (setq tg (copy-sequence (car e)) c2 nil)
 	  (if (cdr e)
 	      (setq c (cdr e))
 	    ;; automatically assign a character.

commit ca699a970dba178fed930872f29d9dff4af32e7c

Agenda: Fix problem with block entries.
This bug was caused by commit
8c177dc832980bd8cf23fc2ae72b18e0b472b59e, and reported by Matt
Lundin.  The problem was that this commit tried to remove the text
propertes of the MATCH part of an agenda command.  However, in block
commands, the MATCH part is not a string.

lisp/org-agenda.el
@@ -1807,7 +1807,7 @@ s   Search for keywords                 C   Configure custom agenda commands
 	  (while (setq entry (pop custom1))
 	    (setq key (car entry) desc (nth 1 entry)
 		  type (nth 2 entry)
-		  match (org-no-properties (copy-sequence (nth 3 entry))))
+		  match (nth 3 entry))
 	    (if (> (length key) 1)
 		(add-to-list 'prefixes (string-to-char key))
 	      (insert
@@ -1833,6 +1833,7 @@ s   Search for keywords                 C   Configure custom agenda commands
 		 (t "???"))
 		(cond
 		 ((stringp match)
+		  (setq match (copy-sequence match))
 		  (org-add-props match nil 'face 'org-warning))
 		 (match
 		  (format "set of %d commands" (length match)))