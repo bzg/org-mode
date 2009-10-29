fbe1b2ccf74ad5820af056e74e15603481ba4f04
Carsten Dominik <carsten.dominik@gmail.com>
2009-10-29 15:21:48 +0100

    Work with abbrev tables only if abbrev.el is loaded

--- lisp/org.el
+++ lisp/org.el
@@ -4141,8 +4141,9 @@ The following commands are available:
       (org-indent-mode 1))
     (org-set-startup-visibility)))
 
-(abbrev-table-put org-mode-abbrev-table
-                  :parents (list text-mode-abbrev-table))
+(eval-after-load "abbrev"
+  '(abbrev-table-put org-mode-abbrev-table
+		     :parents (list text-mode-abbrev-table)))
 
 (put 'org-mode 'flyspell-mode-predicate 'org-mode-flyspell-verify)
 
