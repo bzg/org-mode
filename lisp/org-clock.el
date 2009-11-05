cec499dc6f2eb320e1a7710c31843f7a0f3c33b1
John Wiegley <johnw@newartisans.com>
2009-11-05 02:07:06 -0500

    Fix to idle-time notice in the minibuffer

--- lisp/org-clock.el
+++ lisp/org-clock.el
@@ -796,8 +796,10 @@ if the user really wants to stay clocked in after being idle for
 so long."
   (when (and org-clock-idle-time (not org-clock-resolving-clocks)
 	     org-clock-marker)
-    (let ((org-clock-user-idle-start (current-time))
-	  (org-clock-user-idle-seconds (org-user-idle-seconds))
+    (let ((org-clock-user-idle-seconds (org-user-idle-seconds))
+	  (org-clock-user-idle-start
+	   (time-subtract (current-time)
+			  (seconds-to-time org-clock-user-idle-seconds)))
 	  (org-clock-resolving-clocks-due-to-idleness t))
       (if (> org-clock-user-idle-seconds (* 60 org-clock-idle-time))
 	  (org-clock-resolve
@@ -805,12 +807,12 @@ so long."
 		 org-clock-start-time)
 	   (function
 	    (lambda (clock)
-	      (format "Clocked in & idle for %d mins"
+	      (format "Clocked in & idle for %.1f mins"
 		      (/ (org-float-time
 			  (time-subtract (current-time)
-					 org-clock-user-idle-start)) 60))))
-	   (time-subtract (current-time)
-			  (seconds-to-time org-clock-user-idle-seconds)))))))
+					 org-clock-user-idle-start))
+			 60.0))))
+	   org-clock-user-idle-start)))))
 
 (defun org-clock-in (&optional select)
   "Start the clock on the current item.
