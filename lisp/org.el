c7371cc7feaa7ca8c19f7643d741a3e08afd9abd
Carsten Dominik <carsten.dominik@gmail.com>
2009-11-15 09:46:43 +0100

    Push version number to 6.33trans

--- lisp/org.el
+++ lisp/org.el
@@ -6,7 +6,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.33c
+;; Version: 6.33trans
 ;;
 ;; This file is part of GNU Emacs.
 ;;
@@ -95,7 +95,7 @@
 
 ;;; Version
 
-(defconst org-version "6.33c"
+(defconst org-version "6.33trans"
   "The version number of the file org.el.")
 
 (defun org-version (&optional here)
