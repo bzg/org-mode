a2e0d000c8baae0891dd8205d10b259507f5f68e
Carsten Dominik <carsten.dominik@gmail.com>
2009-11-20 14:56:45 +0100

    org-indent-mode: Fix problem with empty string as line-prefix

--- lisp/org-indent.el
+++ lisp/org-indent.el
@@ -106,8 +106,8 @@ this variable can be set to nil to get rid of the timer."
   ;; Initialize the indentation and star vectors
   (setq org-indent-strings (make-vector (1+ org-indent-max) nil))
   (setq org-indent-stars (make-vector (1+ org-indent-max) nil))
-  (aset org-indent-strings 0 "")
-  (aset org-indent-stars 0 "")
+  (aset org-indent-strings 0 nil)
+  (aset org-indent-stars 0 nil)
   (loop for i from 1 to org-indent-max do
 	(aset org-indent-strings i
 	      (org-add-props
