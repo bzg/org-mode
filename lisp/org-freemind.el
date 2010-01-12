c077142241efc122eb2794ddaea30b6b72a95847
Carsten Dominik <carsten.dominik@gmail.com>
2010-01-12 08:53:44 +0100

    Release 6.34a

--- lisp/org-freemind.el
+++ lisp/org-freemind.el
@@ -5,7 +5,7 @@
 ;; Author: Lennart Borgman (lennart O borgman A gmail O com)
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.34
+;; Version: 6.34a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org-irc.el
+++ lisp/org-irc.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Philip Jackson <emacs@shellarchive.co.uk>
 ;; Keywords: erc, irc, link, org
-;; Version: 6.34
+;; Version: 6.34a
 ;;
 ;; This file is part of GNU Emacs.
 ;;
