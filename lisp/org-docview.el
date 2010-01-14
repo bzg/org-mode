2f2f9441d4c00407a969f26aa34ed41d3298a392
Carsten Dominik <carsten.dominik@gmail.com>
2010-01-14 23:06:55 +0100

    Release 6.34b

--- lisp/org-docview.el
+++ lisp/org-docview.el
@@ -6,7 +6,7 @@
 ;; Author: Jan Böcker <jan.boecker at jboecker dot de>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.34a
+;; Version: 6.34b
 ;;
 ;; This file is part of GNU Emacs.
 ;;
--- lisp/org.el
+++ lisp/org.el
@@ -6,7 +6,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 6.34a
+;; Version: 6.34b
 ;;
 ;; This file is part of GNU Emacs.
 ;;
@@ -95,7 +95,7 @@
 
 ;;; Version
 
-(defconst org-version "6.34a"
+(defconst org-version "6.34b"
   "The version number of the file org.el.")
 
 (defun org-version (&optional here)
