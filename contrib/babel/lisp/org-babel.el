522f30bc9f39236b12d0eabc8a01bb567ecabde8
Eric Schulte <schulte.eric@gmail.com>
2010-02-27 08:26:50 -0700

    babel: tiny doc tweak

--- contrib/babel/lisp/org-babel.el
+++ contrib/babel/lisp/org-babel.el
@@ -81,7 +81,9 @@ then run `org-babel-pop-to-session'."
 
 (defconst org-babel-header-arg-names
   '(cache cmdline colnames dir exports file noweb results session tangle var)
-  "All header arguments used by org-babel")
+  "Common header arguments used by org-babel.  Note that
+individual languages may define their own language specific
+header arguments as well.")
 
 (defvar org-babel-default-header-args
   '((:session . "none") (:results . "replace") (:exports . "code") (:cache . "no") (:noweb . "no"))
