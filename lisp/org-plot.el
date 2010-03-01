bzg/org-mode

commit 09886e4f828ca01540c157c4b7f3ec47be21a5e5

org-plot: now produces more intelligent output when empty lines look like text columns

lisp/org-plot.el
@@ -250,8 +250,10 @@ manner suitable for prepending to a user-specified script."
 		 (setf plot-lines
 		       (cons
 			(format plot-str data-file
-				(or (and (not text-ind) ind
-					 (> ind 0) (format "%d:" ind)) "")
+				(or (and ind (> ind 0)
+                                         (not (or (equal 'histograms (plist-get params :with))
+                                                  (equal 'hist (plist-get params :with))))
+                                         (format "%d:" ind)) "")
 				(+ 1 col)
 				(if text-ind (format ":xticlabel(%d)" ind) "")
 				with
@@ -320,7 +322,6 @@ line directly before or after the table."
 			   (mapcar (lambda (row) (nth ind row)) table)))) 0)
 	      (plist-put params :timeind t)
 	    ;; check for text ind column
-
 	    (if (or (string= (plist-get params :with) "hist")
 		    (> (length
 			(delq 0 (mapcar

commit ff00e36aacefa1afcca80ddec6a6c357513c59ab

plot: replacing sloppy fix with more reasonable way of guessing gnuplot options
  This fixes an error introduced in the recent commit
  09886e4f828ca01540c157c4b7f3ec47be21a5e5

lisp/org-plot.el
@@ -251,8 +251,7 @@ manner suitable for prepending to a user-specified script."
 		       (cons
 			(format plot-str data-file
 				(or (and ind (> ind 0)
-                                         (not (or (equal 'histograms (plist-get params :with))
-                                                  (equal 'hist (plist-get params :with))))
+                                         (not text-ind)
                                          (format "%d:" ind)) "")
 				(+ 1 col)
 				(if text-ind (format ":xticlabel(%d)" ind) "")