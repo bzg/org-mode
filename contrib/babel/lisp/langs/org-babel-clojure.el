54c7b04734bb8ad643156811af36ea5af28553c5
Dan Davison <davison@stats.ox.ac.uk>
2010-03-03 21:59:32 +0000

    babel: clojure: use shell-command-on-region

--- contrib/babel/lisp/langs/org-babel-clojure.el
+++ contrib/babel/lisp/langs/org-babel-clojure.el
@@ -230,13 +230,11 @@ or nil if \"none\" is specified"
           'replace)
          (buffer-string)))
       (value
-       (let ((tmp-src-file (make-temp-file "clojure_babel_input_"))
-             (tmp-results-file (make-temp-file "clojure_babel_results_")))
-         (with-temp-file tmp-src-file
-           (insert (format org-babel-clojure-wrapper-method body tmp-results-file tmp-results-file)))
-         (shell-command
-          (format "%s %s" (mapconcat #'identity (org-babel-clojure-babel-clojure-cmd) " ")
-                  tmp-src-file))
+       (let ((tmp-results-file (make-temp-file "clojure_babel_results_")))
+         (with-temp-buffer
+           (insert (format org-babel-clojure-wrapper-method body tmp-results-file tmp-results-file))
+	   (shell-command-on-region (point-min) (point-max)
+				    (mapconcat #'identity (org-babel-clojure-babel-clojure-cmd) " ")))
          (org-babel-clojure-table-or-string
           (with-temp-buffer (insert-file-contents tmp-results-file) (buffer-string))))))))
 
