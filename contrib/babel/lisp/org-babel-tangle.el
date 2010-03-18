bzg/org-mode

commit 29a2ab0047b3a6f0669f1ab84877e166e00aefff

babel: when tangling do not add extension if filename supplied

contrib/babel/lisp/org-babel-tangle.el
@@ -109,7 +109,7 @@ exported source code blocks by language."
                                       target-file))
                        (file-name (when base-name
                                     ;; decide if we want to add ext to base-name
-                                    (if (and ext (not (string= (file-name-extension base-name) ext)))
+                                    (if (and ext (string= "yes" tangle))
                                         (concat base-name "." ext) base-name))))
                   ;; ;; debugging
                   ;; (message

commit aa0e4b86ce2753909ef71081f8c437ad67af0fb1

babel: Fix org-babel-load-file
This was broken by 29a2ab0047b3a6f0669f1ab84877e166e00aefff

contrib/babel/lisp/org-babel-tangle.el
@@ -54,7 +54,7 @@ file using `load-file'."
            (exported-file (concat base-name ".el")))
       ;; tangle if the org-mode file is newer than the elisp file
       (unless (and (file-exists-p exported-file) (> (age file) (age exported-file)))
-        (org-babel-tangle-file file base-name "emacs-lisp"))
+        (org-babel-tangle-file file exported-file "emacs-lisp"))
       (load-file exported-file)
       (message "loaded %s" exported-file))))
 