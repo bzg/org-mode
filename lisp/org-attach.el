562fa025f83ee57d7880faae16afbb928783ea9e
Carsten Dominik <carsten.dominik@gmail.com>
2010-04-02 09:42:14 +0200

    Fix git cleanup of deleted files

--- lisp/org-attach.el
+++ lisp/org-attach.el
@@ -245,12 +245,12 @@ This checks for the existence of a \".git\" directory in that directory."
       (with-temp-buffer
 	(cd dir)
 	(shell-command "git add .")
-	(shell-command "git ls-files --deleted -z" t)
+	(shell-command "git ls-files --deleted" t)
 	(mapc '(lambda (file)
 		 (unless (string= file "")
 		   (shell-command
 		    (concat "git rm \"" file "\""))))
-	      (split-string (buffer-string) ""))
+	      (split-string (buffer-string) "\n"))
 	(shell-command "git commit -m 'Synchronized attachments'")))))
 
 (defun org-attach-tag (&optional off)
