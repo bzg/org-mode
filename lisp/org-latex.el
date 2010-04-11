bzg/org-mode

commit 1b40601ebd5de3746ab72d19d3258f815a402066

`org-export-as-latex' set the body-only option when called with a simple prefix arg
  This can be useful for exporting a small active region (often a
  table) to latex for pasting into an existing latex buffer.

lisp/org-latex.el
@@ -584,6 +584,7 @@ simply return the content of \begin{document}...\end{document},
 without even the \begin{document} and \end{document} commands.
 when PUB-DIR is set, use this as the publishing directory."
   (interactive "P")
+  (when (and (not body-only) (listp arg)) (setq body-only t))
   (run-hooks 'org-export-first-hook)
 
   ;; Make sure we have a file name when we need it.

commit e26deb647ff1df5ab754fc42da816d60470929aa

fixes BUG in `org-export-as-latex' -- was only exporting the body
  This bug was introduced in commit

    1b40601ebd5de3746ab72d19d3258f815a402066

  which sets the body-only option to true when called with a simple
  prefix argument, however it does not check that the prefix argument
  is non-null.

  Thanks to Valentin Wüstholz for reporting this bug

lisp/org-latex.el
@@ -584,7 +584,7 @@ simply return the content of \begin{document}...\end{document},
 without even the \begin{document} and \end{document} commands.
 when PUB-DIR is set, use this as the publishing directory."
   (interactive "P")
-  (when (and (not body-only) (listp arg)) (setq body-only t))
+  (when (and (not body-only) arg (listp arg)) (setq body-only t))
   (run-hooks 'org-export-first-hook)
 
   ;; Make sure we have a file name when we need it.