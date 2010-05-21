a0a5b10f7a3d32839365ee7cd5197e762e2ce5b6
John Wiegley <johnw@newartisans.com>
2010-05-21 05:01:39 -0400

    Revert "org.el (org-remove-inline-images): Call `clear-image-cache'."
    This reverts commit 0c42220ca025269e39f20191bc3e10d6b55d02ac.

--- lisp/org.el
+++ lisp/org.el
@@ -15729,7 +15729,6 @@ BEG and END default to the buffer boundaries."
 (defun org-remove-inline-images ()
   "Remove inline display of images."
   (interactive)
-  (clear-image-cache)
   (mapc 'delete-overlay org-inline-image-overlays)
   (setq org-inline-image-overlays nil))
 
