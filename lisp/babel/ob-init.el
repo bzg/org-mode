116d9cfa853525574df6d06a6c38f74452de02b6
Eric Schulte <schulte.eric@gmail.com>
2010-06-14 16:36:56 -0700

    ob: adding ob-init.el to load everything

---
+++ lisp/babel/ob-init.el
@@ -0,0 +1,12 @@
+;;; ob-init.el --- working with code blocks in org-mode
+(require 'ob)
+(require 'ob-ref)
+(require 'ob-exp)
+(require 'ob-tangle)
+(require 'ob-lob)
+(require 'ob-comint)
+(require 'ob-table)
+(require 'ob-keys)
+
+(provide 'ob-init)
+;;; ob-init.el ends here
