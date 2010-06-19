f1924d9224cb69033be49063654b234f33ffddfa
David Maus <dmaus@ictsoc.de>
2010-06-19 19:25:39 +0000

    Ignore case of rss element names.
    * org-feed.el (org-feed-parse-rss-feed): Ignore case of rss
    element names.

--- lisp/org-feed.el
+++ lisp/org-feed.el
@@ -579,7 +579,8 @@ Assumes headers are indeed present!"
   "Parse BUFFER for RSS feed entries.
 Returns a list of entries, with each entry a property list,
 containing the properties `:guid' and `:item-full-text'."
-  (let (entries beg end item guid entry)
+  (let ((case-fold-search t)
+	entries beg end item guid entry)
     (with-current-buffer buffer
       (widen)
       (goto-char (point-min))
