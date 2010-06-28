05b7ea50354af98b4852d20a298b4aa874143037
Eric Schulte <schulte.eric@gmail.com>
2010-06-27 23:10:53 -0700

    babel: ensure `declare-function' is available in all Emacsen
      Thanks to Daniel Mahler for finding this problem and proposing the fix
    
    * lisp/babel/ob.el (unless): ensure `declare-function' is available in
      all Emacsen

--- lisp/babel/ob.el
+++ lisp/babel/ob.el
@@ -30,7 +30,9 @@
 
 ;;; Code:
 (eval-when-compile (require 'cl))
-(require 'outline)
+(eval-and-compile
+  (unless (fboundp 'declare-function)
+    (defmacro declare-function (fn file &optional arglist fileonly))))
 
 (defvar org-babel-call-process-region-original)
 (declare-function show-all "outline" ())
