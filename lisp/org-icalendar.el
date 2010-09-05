373224ecdbd32a6e7ed523e28b581f637a7b1621
Bastien Guerry <bzg@altern.org>
2010-09-05 20:42:52 +0200

    org-icalendar.el: exclude tags from the summary of the ical entry.
    This was reported by Guy Wiener <wiener.guy@gmail.com>.
    
    http://article.gmane.org/gmane.emacs.orgmode/29819

--- lisp/org-icalendar.el
+++ lisp/org-icalendar.el
@@ -311,7 +311,9 @@ When COMBINE is non nil, add the category to each line."
 		inc t
 		hd (condition-case nil
 		       (org-icalendar-cleanup-string
-			(org-get-heading))
+			(replace-regexp-in-string 
+			 ":[[:alnum:]_@#%:]+:[ \t]*$" ""
+			 (org-get-heading)))
 		     (error (throw :skip nil)))
 		summary (org-icalendar-cleanup-string
 			 (org-entry-get nil "SUMMARY"))
