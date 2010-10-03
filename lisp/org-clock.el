344785bd8cfa5afd6458658d7ff05cbfa6b1e7d2
Bernt Hansen <bernt@norang.ca>
2010-10-03 11:47:15 -0400

    Fix dangling clocks created by org-clock-out-hook clocking in again
    This is a fixup patch to commit
    17c71a40c6d07baae2e9cac606668683ddb3759c.  The org-clock-clocking-in
    variable needs to be set when we are clocking out - in case the clock
    out hook clocks-in again.  This fixes a bug that creates dangling
    clock entries.

--- lisp/org-clock.el
+++ lisp/org-clock.el
@@ -975,7 +975,8 @@ the clocking selection, associated with the letter `d'."
 	(move-marker org-clock-interrupted-task
 		     (marker-position org-clock-marker)
 		     (marker-buffer org-clock-marker))
-	(org-clock-out t))
+	(let ((org-clock-clocking-in t))
+	  (org-clock-out t)))
 
       (when (equal select '(16))
 	;; Mark as default clocking task
