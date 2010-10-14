82429ef406b368ff98f5b5d592c903c34b73b4f9
Dan Davison <davison@stats.ox.ac.uk>
2010-10-14 13:02:46 +0100

    babel: Simplify reading of header argument values
    	* ob.el (org-babel-parse-header-arguments): Simplify reading
    	of header arg value

--- lisp/ob.el
+++ lisp/ob.el
@@ -858,9 +858,7 @@ may be specified at the top of the current buffer."
                     "\\([^ \f\t\n\r\v]+\\)[ \f\t\n\r\v]+\\([^ \f\t\n\r\v]+.*\\)"
                     arg)
 		   (cons (intern (concat ":" (match-string 1 arg)))
-                         (let ((raw (org-babel-chomp (match-string 2 arg))))
-                           (if (org-babel-number-p raw)
-                               raw (org-babel-read raw))))
+                         (org-babel-read (org-babel-chomp (match-string 2 arg))))
 		 (cons (intern (concat ":" arg)) nil)))
 	     (split-string (concat " " arg-string) "[ \f\t\n\r\v]+:" t)))))
 
