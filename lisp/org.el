6d33af4a5c920f1e3d1584bfb612e6800df52129
Sébastien Vauban <wxhgmqzgwmuf@spammotel.com>
2010-11-24 13:38:26 +0000

    Fix documentation for global tag list during capture
    * lisp/org.el (org-complete-tags-always-offer-all-agenda-tags): Fix
    docstring.

--- lisp/org.el
+++ lisp/org.el
@@ -2732,10 +2732,10 @@ To disable these tags on a per-file basis, insert anywhere in the file:
 (defcustom org-complete-tags-always-offer-all-agenda-tags nil
   "If non-nil, always offer completion for all tags of all agenda files.
 Instead of customizing this variable directly, you might want to
-set it locally for remember buffers, because there no list of
+set it locally for capture buffers, because there no list of
 tags in that file can be created dynamically (there are none).
 
-  (add-hook 'org-remember-mode-hook
+  (add-hook 'org-capture-mode-hook
             (lambda ()
               (set (make-local-variable
                     'org-complete-tags-always-offer-all-agenda-tags)
