d98fc9d57b80e50123bbfa9143cdc20bfaa9dfd6
Lawrence Mitchell <wence@gmx.li>
2011-03-04 16:38:08 +0000

    * org-latex.el: place \title \author \date before \begin{document}.
    * org-latex.el (org-export-latex-make-header): place
    \title \author \date before \begin{document}.

--- lisp/org-latex.el
+++ lisp/org-latex.el
@@ -1352,9 +1352,6 @@ OPT-PLIST is the options plist for current buffer."
      (org-export-apply-macros-in-string org-export-latex-append-header)
      ;; define alert if not yet defined
      "\n\\providecommand{\\alert}[1]{\\textbf{#1}}"
-     ;; beginning of the document
-     "\n\\begin{document}\n\n"
-     ;; insert the title
      (format
       "\n\n\\title{%s}\n"
       ;; convert the title
@@ -1374,6 +1371,9 @@ OPT-PLIST is the options plist for current buffer."
 	     (format-time-string
 	      (or (plist-get opt-plist :date)
 		  org-export-latex-date-format)))
+     ;; beginning of the document
+     "\n\\begin{document}\n\n"
+     ;; insert the title
      ;; insert the title command
      (when (string-match "\\S-" title)
        (if (string-match "%s" org-export-latex-title-command)
