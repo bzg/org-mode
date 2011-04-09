098ef03fa9b524712ee89c60fe0040959463d956
Bastien Guerry <bzg@altern.org>
2011-04-09 16:20:46 +0200

    Minor cleanup.

--- lisp/org-agenda.el
+++ lisp/org-agenda.el
@@ -5899,7 +5899,8 @@ to switch to narrowing."
 	 (efforts (org-split-string
 		   (or (cdr (assoc (concat org-effort-property "_ALL")
 				   org-global-properties))
-		       "0 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00"		      "")))
+		       "0 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00"
+		       "")))
 	 (effort-op org-agenda-filter-effort-default-operator)
 	 (effort-prompt "")
 	 (inhibit-read-only t)
