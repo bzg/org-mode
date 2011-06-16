01ebf445aa8cbe687c8525043d0adafd2663f6be
Eric Schulte <schulte.eric@gmail.com>
2011-06-15 21:49:55 -0700

    ob: adding noweb-ref to the list of header argument names
    * lisp/ob.el (org-babel-header-arg-names): Adding noweb-ref to the
      list of header argument names.

--- lisp/ob.el
+++ lisp/ob.el
@@ -291,7 +291,8 @@ then run `org-babel-pop-to-session'."
 
 (defconst org-babel-header-arg-names
   '(cache cmdline colnames dir exports file noweb results
-    session tangle var eval noeval comments no-expand shebang padline)
+    session tangle var eval noeval comments no-expand shebang
+    padline noweb-ref)
   "Common header arguments used by org-babel.
 Note that individual languages may define their own language
 specific header arguments as well.")
