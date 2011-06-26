8d7d4428ccf1436bc1fdfa92e18deb2e06c7773f
Eric Schulte <schulte.eric@gmail.com>
2011-06-25 17:46:57 -0700

    test: explicitly testing inline call lines at the beginning of a line

--- testing/lisp/test-ob-lob.el
+++ testing/lisp/test-ob-lob.el
@@ -56,7 +56,10 @@
     (should (string= "testing" (caar (org-babel-lob-execute
 				      (org-babel-lob-get-info)))))
     (forward-line 1) (beginning-of-line)
-    (should (= 4 (org-babel-lob-execute (org-babel-lob-get-info))))))
+    (should (= 4 (org-babel-lob-execute (org-babel-lob-get-info))))
+    (forward-line 1)
+    (should (string= "testing" (org-babel-lob-execute
+				(org-babel-lob-get-info))))))
 
 (provide 'test-ob-lob)
 
