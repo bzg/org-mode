0c0bce4a67424a8546dd4023c9fcad0f84f91ff6
Bastien Guerry <bzg@altern.org>
2011-07-27 16:32:41 +0200

    Manually revert 366254217ac6faea20a5d43fae065bd028806eab.
    As David Maus pointed out, the implementation is not good.
    
    * org.el (org-deadline, org-schedule): Don't loop.
    (org-loop-over-siblings-within-active-region-p): Remove.
    * org-archive.el (org-archive-subtree)
    (org-archive-to-archive-sibling, org-toggle-archive-tag):
    Don't loop.
    
    * org-macs.el (org-loop-over-siblings-in-active-region):
    Remove.

--- lisp/org.el
+++ lisp/org.el
@@ -1981,18 +1981,6 @@ heading."
   "The available interpretation symbols for customizing `org-todo-keywords'.
 Interested libraries should add to this list.")
 
-(defcustom org-loop-over-siblings-within-active-region-p nil
-  "Shall some commands act upon siblings in the active region?
-The list of commands is:
-- `org-schedule'
-- `org-deadline'
-- `org-archive-subtree'
-- `org-archive-to-archive-sibling'
-- `org-archive-set-tag'
-- `org-toggle-archive-tag'"
-  :group 'org-todo
-  :group 'org-archive)
-
 (defcustom org-todo-keywords '((sequence "TODO" "DONE"))
   "List of TODO entry keyword sequences and their interpretation.
 \\<org-mode-map>This is a list of sequences.
@@ -11737,40 +11725,39 @@ With argument REMOVE, remove any deadline from the item.
 With argument TIME, set the deadline at the corresponding date.  TIME
 can either be an Org date like \"2011-07-24\" or a delta like \"+2d\"."
   (interactive "P")
-  (org-loop-over-siblings-in-active-region
-   (let* ((old-date (org-entry-get nil "DEADLINE"))
-	  (repeater (and old-date
-			 (string-match
-			  "\\([.+-]+[0-9]+[dwmy]\\(?:[/ ][-+]?[0-9]+[dwmy]\\)?\\) ?"
+  (let* ((old-date (org-entry-get nil "DEADLINE"))
+	 (repeater (and old-date
+			(string-match
+			 "\\([.+-]+[0-9]+[dwmy]\\(?:[/ ][-+]?[0-9]+[dwmy]\\)?\\) ?"
 			  old-date)
-			 (match-string 1 old-date))))
-     (if remove
-	 (progn
-	   (when (and old-date org-log-redeadline)
-	     (org-add-log-setup 'deldeadline nil old-date 'findpos
-				org-log-redeadline))
-	   (org-remove-timestamp-with-keyword org-deadline-string)
-	   (message "Item no longer has a deadline."))
-       (org-add-planning-info 'deadline time 'closed)
-       (when (and old-date org-log-redeadline
-		  (not (equal old-date
-			      (substring org-last-inserted-timestamp 1 -1))))
-	 (org-add-log-setup 'redeadline nil old-date 'findpos
-			    org-log-redeadline))
-       (when repeater
-	 (save-excursion
-	   (org-back-to-heading t)
-	   (when (re-search-forward (concat org-deadline-string " "
-					    org-last-inserted-timestamp)
-				    (save-excursion
-				      (outline-next-heading) (point)) t)
-	     (goto-char (1- (match-end 0)))
-	     (insert " " repeater)
-	     (setq org-last-inserted-timestamp
-		   (concat (substring org-last-inserted-timestamp 0 -1)
-			   " " repeater
-			   (substring org-last-inserted-timestamp -1))))))
-       (message "Deadline on %s" org-last-inserted-timestamp)))))
+			(match-string 1 old-date))))
+    (if remove
+	(progn
+	  (when (and old-date org-log-redeadline)
+	    (org-add-log-setup 'deldeadline nil old-date 'findpos
+			       org-log-redeadline))
+	  (org-remove-timestamp-with-keyword org-deadline-string)
+	  (message "Item no longer has a deadline."))
+      (org-add-planning-info 'deadline time 'closed)
+      (when (and old-date org-log-redeadline
+		 (not (equal old-date
+			     (substring org-last-inserted-timestamp 1 -1))))
+	(org-add-log-setup 'redeadline nil old-date 'findpos
+			   org-log-redeadline))
+      (when repeater
+	(save-excursion
+	  (org-back-to-heading t)
+	  (when (re-search-forward (concat org-deadline-string " "
+					   org-last-inserted-timestamp)
+				   (save-excursion
+				     (outline-next-heading) (point)) t)
+	    (goto-char (1- (match-end 0)))
+	    (insert " " repeater)
+	    (setq org-last-inserted-timestamp
+		  (concat (substring org-last-inserted-timestamp 0 -1)
+			  " " repeater
+			  (substring org-last-inserted-timestamp -1))))))
+      (message "Deadline on %s" org-last-inserted-timestamp))))
 
 (defun org-schedule (&optional remove time)
   "Insert the SCHEDULED: string with a timestamp to schedule a TODO item.
@@ -11778,40 +11765,39 @@ With argument REMOVE, remove any scheduling date from the item.
 With argument TIME, scheduled at the corresponding date.  TIME can
 either be an Org date like \"2011-07-24\" or a delta like \"+2d\"."
   (interactive "P")
-  (org-loop-over-siblings-in-active-region
-   (let* ((old-date (org-entry-get nil "SCHEDULED"))
-	  (repeater (and old-date
-			 (string-match
-			  "\\([.+-]+[0-9]+[dwmy]\\(?:[/ ][-+]?[0-9]+[dwmy]\\)?\\) ?"
-			  old-date)
-			 (match-string 1 old-date))))
-     (if remove
-	 (progn
-	   (when (and old-date org-log-reschedule)
-	     (org-add-log-setup 'delschedule nil old-date 'findpos
-				org-log-reschedule))
-	   (org-remove-timestamp-with-keyword org-scheduled-string)
-	   (message "Item is no longer scheduled."))
-       (org-add-planning-info 'scheduled time 'closed)
-       (when (and old-date org-log-reschedule
-		  (not (equal old-date
-			      (substring org-last-inserted-timestamp 1 -1))))
-	 (org-add-log-setup 'reschedule nil old-date 'findpos
-			    org-log-reschedule))
-       (when repeater
-	 (save-excursion
-	   (org-back-to-heading t)
-	   (when (re-search-forward (concat org-scheduled-string " "
-					    org-last-inserted-timestamp)
-				    (save-excursion
-				      (outline-next-heading) (point)) t)
-	     (goto-char (1- (match-end 0)))
-	     (insert " " repeater)
-	     (setq org-last-inserted-timestamp
-		   (concat (substring org-last-inserted-timestamp 0 -1)
-			   " " repeater
-			   (substring org-last-inserted-timestamp -1))))))
-       (message "Scheduled to %s" org-last-inserted-timestamp)))))
+  (let* ((old-date (org-entry-get nil "SCHEDULED"))
+	 (repeater (and old-date
+			(string-match
+			 "\\([.+-]+[0-9]+[dwmy]\\(?:[/ ][-+]?[0-9]+[dwmy]\\)?\\) ?"
+			 old-date)
+			(match-string 1 old-date))))
+    (if remove
+	(progn
+	  (when (and old-date org-log-reschedule)
+	    (org-add-log-setup 'delschedule nil old-date 'findpos
+			       org-log-reschedule))
+	  (org-remove-timestamp-with-keyword org-scheduled-string)
+	  (message "Item is no longer scheduled."))
+      (org-add-planning-info 'scheduled time 'closed)
+      (when (and old-date org-log-reschedule
+		 (not (equal old-date
+			     (substring org-last-inserted-timestamp 1 -1))))
+	(org-add-log-setup 'reschedule nil old-date 'findpos
+			   org-log-reschedule))
+      (when repeater
+	(save-excursion
+	  (org-back-to-heading t)
+	  (when (re-search-forward (concat org-scheduled-string " "
+					   org-last-inserted-timestamp)
+				   (save-excursion
+				     (outline-next-heading) (point)) t)
+	    (goto-char (1- (match-end 0)))
+	    (insert " " repeater)
+	    (setq org-last-inserted-timestamp
+		  (concat (substring org-last-inserted-timestamp 0 -1)
+			  " " repeater
+			  (substring org-last-inserted-timestamp -1))))))
+      (message "Scheduled to %s" org-last-inserted-timestamp))))
 
 (defun org-get-scheduled-time (pom &optional inherit)
   "Get the scheduled time as a time tuple, of a format suitable
