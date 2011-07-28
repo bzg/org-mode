c8806bdcf78f28c6da81aa9c81f3d18031101f8c
Bastien Guerry <bzg@altern.org>
2011-07-28 17:11:41 +0200

    org-special-blocks.el: This file is now part of GNU Emacs.

--- lisp/org-special-blocks.el
+++ lisp/org-special-blocks.el
@@ -4,7 +4,7 @@
 
 ;; Author: Chris Gray <chrismgray@gmail.com>
 
-;; This file is not currently part of GNU Emacs.
+;; This file is part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
