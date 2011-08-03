c0e2351e1af5c6bc7ac7fc427fb805350b099494
Sergey Litvinov <slitvinov@gmail.com>
2011-08-03 22:02:43 +0200

    Map "cpp" to c++-mode

--- lisp/org-src.el
+++ lisp/org-src.el
@@ -155,7 +155,7 @@ but which mess up the display of a snippet in Org exported files.")
 (defcustom org-src-lang-modes
   '(("ocaml" . tuareg) ("elisp" . emacs-lisp) ("ditaa" . artist)
     ("asymptote" . asy) ("dot" . fundamental) ("sqlite" . sql)
-    ("calc" . fundamental) ("C" . c))
+    ("calc" . fundamental) ("C" . c) ("cpp" . c++))
   "Alist mapping languages to their major mode.
 The key is the language name, the value is the string that should
 be inserted as the name of the major mode.  For many languages this is
