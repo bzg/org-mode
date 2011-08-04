915464159368b491c9bea38e4cae81244790bf6c
Litvinov Sergey <slitvinov@gmail.com>
2011-08-04 01:08:56 +0000

    cpp->c++-mode, ob-C mode tests
    The first patch maps cpp language code to c++-mode. The second patch
    adds tests for ob-C.
    
    >From fba6eef6944766e675e4abe1d11d347b9a728031 Mon Sep 17 00:00:00 2001
    From: Sergey Litvinov <slitvinov@gmail.com>
    Date: Wed, 3 Aug 2011 22:03:19 +0200
    Subject: [PATCH 2/2] Add tests for ob-C.el

---
+++ testing/lisp/test-ob-C.el
@@ -0,0 +1,43 @@
+(require 'ob-C)
+ 
+(ert-deftest ob-C/assert ()
+  (should t))
+
+(ert-deftest ob-C/simple-program ()
+  "Hello world program."
+  (org-test-at-id "fa6db330-e960-4ea2-ac67-94bb845b8577"
+    (org-babel-next-src-block)
+    (should (= 42 (org-babel-execute-src-block)))))
+
+(ert-deftest ob-C/integer-var ()
+  "Test of an integer variable."
+  (org-test-at-id "fa6db330-e960-4ea2-ac67-94bb845b8577"
+    (org-babel-next-src-block 2)
+    (should (= 12 (org-babel-execute-src-block)))))
+
+(ert-deftest ob-C/two-integer-var ()
+  "Test of two input variables"
+  (org-test-at-id "fa6db330-e960-4ea2-ac67-94bb845b8577"
+    (org-babel-next-src-block 3)
+    (should (= 22 (org-babel-execute-src-block)))))
+
+(ert-deftest ob-C/string-var ()
+  "Test of a string input variable"
+  (org-test-at-id "fa6db330-e960-4ea2-ac67-94bb845b8577"
+    (org-babel-next-src-block 4)
+    (should (equal "word 4" (org-babel-execute-src-block)))))
+
+(ert-deftest ob-C/preprocessor ()
+  "Test of a string variable"
+  (org-test-at-id "fa6db330-e960-4ea2-ac67-94bb845b8577"
+    (org-babel-next-src-block 5)
+    (should (= 42 (org-babel-execute-src-block)))))
+
+(ert-deftest ob-C/table ()
+  "Test of a table output"
+  (org-test-at-id "2df1ab83-3fa3-462a-a1f3-3aef6044a874"
+    (org-babel-next-src-block)
+    (should (equal '((1) (2)) (org-babel-execute-src-block)))))
+
+;;; test-ob-C.el ends here
+ 
