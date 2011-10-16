35d924cc80a831d714efe42c4c000de7a62ed634
Carsten Dominik <carsten.dominik@gmail.com>
2011-10-16 20:00:23 +0200

    Fix bug in regexp for #+include
    * lisp/org-publish.el (org-publish-cache-file-needs-publishing): Fix regexp
    to not inlcude newlines.

--- lisp/org-publish.el
+++ lisp/org-publish.el
@@ -1105,7 +1105,7 @@ so that the file including them will be republished as well."
       (setq buf (find-file (expand-file-name filename)))
       (with-current-buffer buf
 	(goto-char (point-min))
-	(while (re-search-forward "^#\\+INCLUDE:[ \t]+\"?\\([^ \t\"]*\\)\"?[ \t]*.*$" nil t)
+	(while (re-search-forward "^#\\+INCLUDE:[ \t]+\"?\\([^ \t\n\r\"]*\\)\"?[ \t]*.*$" nil t)
 	  (let* ((included-file (expand-file-name (match-string 1))))
 	    (add-to-list 'included-files-ctime
 			 (org-publish-cache-ctime-of-src included-file) t))))
