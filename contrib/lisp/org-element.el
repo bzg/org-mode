80dd8f47f0e0990550ec3827781aed34f8cff58b
Bastien Guerry <bzg@altern.org>
2011-12-26 15:34:36 +0100

    org-element.el: Added "This file is not part of GNU Emacs".

--- contrib/lisp/org-element.el
+++ contrib/lisp/org-element.el
@@ -15,6 +15,8 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
+;; This file is not part of GNU Emacs.
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
