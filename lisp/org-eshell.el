f5e5c5811c1cb12209e49a8574b627f6d54ed5d1
Bastien Guerry <bzg@altern.org>
2012-01-04 16:12:43 +0100

    More fixes to the copyright year...
    Conflicts:
    
    	lisp/ob-awk.el
    	lisp/ob-fortran.el
    	lisp/ob-java.el
    	lisp/org-eshell.el

--- lisp/org-eshell.el
+++ lisp/org-eshell.el
@@ -1,6 +1,7 @@
 ;;; org-eshell.el - Support for links to working directories in eshell
-;; Copyright (C) 2011 Free Software Foundation, Inc.
-;;
+
+;; Copyright (C) 2011-2012 Free Software Foundation, Inc.
+
 ;; Author: Konrad Hinsen <konrad.hinsen AT fastmail.net>
 ;; Version: 7.8.03
 ;;
