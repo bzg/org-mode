f2a9d838241652bd42f110aa731c963e16790667
Konrad Hinsen <konrad.hinsen@fastmail.net>
2012-01-20 16:57:37 +0100

    Support for links to IMAP folders in org-vm.el

--- lisp/org.el
+++ lisp/org.el
@@ -1549,6 +1549,7 @@ in the search text."
 
 (defcustom org-link-frame-setup
   '((vm . vm-visit-folder-other-frame)
+    (vm-imap . vm-visit-imap-folder-other-frame)
     (gnus . org-gnus-no-new-news)
     (file . find-file-other-window)
     (wl . wl-other-frame))
