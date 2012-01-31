bzg/org-mode

commit 794319dfd9f11827ca0fce2043dfa95547eafe84

inhibit R evaluation visibility regardless of local user customization
* lisp/ob-R.el (org-babel-R-evaluate-session): Inhibit R evaluation
  visibility regardless of local user customization.

lisp/ob-R.el
@@ -279,8 +279,9 @@ last statement in BODY, as elisp."
      (with-temp-buffer
        (insert (org-babel-chomp body))
        (let ((ess-local-process-name
-	      (process-name (get-buffer-process session))))
-	 (ess-eval-buffer nil)))
+	      (process-name (get-buffer-process session)))
+	     (ess-eval-visibly-p nil))
+	 (ess-eval-buffer)))
      (let ((tmp-file (org-babel-temp-file "R-")))
        (org-babel-comint-eval-invisibly-and-wait-for-file
 	session tmp-file

commit 900b988742f52cf88ae976b89587591a52709a53

fix bug introduced by 794319dfd9f11827ca0fce2043dfa95547eafe84

lisp/ob-R.el
@@ -281,7 +281,7 @@ last statement in BODY, as elisp."
        (let ((ess-local-process-name
 	      (process-name (get-buffer-process session)))
 	     (ess-eval-visibly-p nil))
-	 (ess-eval-buffer)))
+	 (ess-eval-buffer nil)))
      (let ((tmp-file (org-babel-temp-file "R-")))
        (org-babel-comint-eval-invisibly-and-wait-for-file
 	session tmp-file