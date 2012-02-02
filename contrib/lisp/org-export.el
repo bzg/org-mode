bzg/org-mode

commit 10b06ae53cb29f2eedd9a2db405ca6084a8377b2

org-export: Fix bug with false-positive inline tasks
* contrib/lisp/org-export.el (org-export-with-current-buffer-copy):
  Fix bug with false-positive inline tasks.

contrib/lisp/org-export.el
@@ -2012,7 +2012,8 @@ Point is at buffer's beginning when BODY is applied."
        (with-temp-buffer
 	 (let ((buffer-invisibility-spec nil))
 	   (org-clone-local-variables
-	    ,original-buffer "^\\(org-\\|orgtbl-\\|major-mode$\\)")
+	    ,original-buffer
+	    "^\\(org-\\|orgtbl-\\|major-mode$\\|outline-regexp$\\)")
 	   (insert ,buffer-string)
 	   (mapc (lambda (ov)
 		   (move-overlay

commit efc10c9286040790f31ae5103ea8a49e446bd31e

org-export: Fix bug preveting low level headlines to close properly in e-latex
* contrib/lisp/org-export.el (org-export-with-current-buffer-copy):
  Copy `outline-level' value in the temporary buffer.  This fixes
  a bug introduced by commit
  10b06ae53cb29f2eedd9a2db405ca6084a8377b2.

contrib/lisp/org-export.el
@@ -2016,7 +2016,7 @@ Point is at buffer's beginning when BODY is applied."
 	 (let ((buffer-invisibility-spec nil))
 	   (org-clone-local-variables
 	    ,original-buffer
-	    "^\\(org-\\|orgtbl-\\|major-mode$\\|outline-regexp$\\)")
+	    "^\\(org-\\|orgtbl-\\|major-mode$\\|outline-\\(regexp\\|level\\)$\\)")
 	   (insert ,buffer-string)
 	   (mapc (lambda (ov)
 		   (move-overlay