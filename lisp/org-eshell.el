d254b05500657250ab2ddd729b7182e7375b7678
Bastien Guerry <bzg@altern.org>
2012-03-17 14:24:27 +0100

    Revert "Release 7.8.04"
    This reverts commit 78ec8e21b8a5c72797c108621ae18c89e92a3dab.

--- lisp/org-eshell.el
+++ lisp/org-eshell.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2011-2012 Free Software Foundation, Inc.
 
 ;; Author: Konrad Hinsen <konrad.hinsen AT fastmail.net>
-;; Version: 7.8.04
+;; Version: 0.1
 
 ;; This file is part of GNU Emacs.
 
--- lisp/org.el
+++ lisp/org.el
@@ -7,7 +7,7 @@
 ;; Maintainer: Bastien Guerry <bzg at gnu dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 7.8.04
+;; Version: 7.8.03
 ;;
 ;; This file is part of GNU Emacs.
 ;;
@@ -209,7 +209,7 @@ identifier."
 
 ;;; Version
 
-(defconst org-version "7.8.04"
+(defconst org-version "7.8.03"
   "The version number of the file org.el.")
 
 ;;;###autoload
