00365ede15043d6e7745683e439ef36de0f3ee8a
Bastien Guerry <bzg@altern.org>
2012-03-19 22:23:16 +0100

    Bump to version 7.8.06.

--- lisp/org.el
+++ lisp/org.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Bastien Guerry <bzg at gnu dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: http://orgmode.org
-;; Version: 7.8.05
+;; Version: 7.8.06
 ;;
 ;; This file is part of GNU Emacs.
 ;;
@@ -203,7 +203,7 @@ identifier."
 
 ;;; Version
 
-(defconst org-version "7.8.05"
+(defconst org-version "7.8.06"
   "The version number of the file org.el.")
 
 ;;;###autoload
