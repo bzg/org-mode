9602e1cb38cbfe6795d02e2e59abf8cabd199a4c
Bastien Guerry <bzg@altern.org>
2012-03-26 17:24:26 +0200

    org-collector.el: Don't narrow to subtree when collecting properties on the whole buffer.
    * org-collector.el (org-dblock-write:propview): Don't narrow
    to subtree when collecting properties on the whole buffer.
    
    TINYCHANGE

--- contrib/lisp/org-collector.el
+++ contrib/lisp/org-collector.el
@@ -131,7 +131,7 @@ preceeding the dblock, then update the contents of the dblock."
 		  ((setq idpos (org-find-entry-with-id id))
 		   (goto-char idpos))
 		  (t (error "Cannot find entry with :ID: %s" id))))
-	  (org-narrow-to-subtree)
+	  (unless (eq id 'global) (org-narrow-to-subtree))
 	  (setq stringformat (if noquote "%s" "%S"))
 	  (setq table (org-propview-to-table
 		       (org-propview-collect cols stringformat conds match scope inherit
