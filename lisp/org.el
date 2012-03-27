647396464d563634b980127673cf61769663407e
Bastien Guerry <bzg@altern.org>
2012-03-27 12:09:02 +0200

    org.el: Include tags from `org-tag-alist' when completing with the TAB key.
    * org.el (org-fast-tag-selection): Include tags from
    `org-tag-alist' when completing with the TAB key.
    
    Thanks to Mike McLean for reporting this problem.

--- lisp/org.el
+++ lisp/org.el
@@ -13691,9 +13691,10 @@ Returns the new tags string, or nil to not change the current settings."
 		  (condition-case nil
 		      (setq tg (org-icompleting-read
 				"Tag: "
-				(or buffer-tags
-				    (with-current-buffer buf
-				      (org-get-buffer-tags)))))
+				(append (or buffer-tags
+					    (with-current-buffer buf
+					      (org-get-buffer-tags)))
+					(mapcar 'car table))))
 		    (quit (setq tg "")))
 		  (when (string-match "\\S-" tg)
 		    (add-to-list 'buffer-tags (list tg))
