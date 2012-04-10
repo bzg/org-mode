1f80206166b0f3087fcb3649da9751523493db6d
Bastien Guerry <bzg@altern.org>
2012-04-10 18:10:30 +0200

    Revert "org.el (org-fast-tag-selection): Fix an bug when listing tags for completion."
    This reverts commit dca5af369c698961a36a32dc78eaa15e14ea808e.

--- lisp/org.el
+++ lisp/org.el
@@ -13726,11 +13726,10 @@ Returns the new tags string, or nil to not change the current settings."
 		  (condition-case nil
 		      (setq tg (org-icompleting-read
 				"Tag: "
-				(delete-dups
-				 (append (or buffer-tags
-					     (with-current-buffer buf
-					       (mapcar 'car (org-get-buffer-tags))))
-					 (mapcar 'car table)))))
+				(append (or buffer-tags
+					    (with-current-buffer buf
+					      (org-get-buffer-tags)))
+					(mapcar 'car table))))
 		    (quit (setq tg "")))
 		  (when (string-match "\\S-" tg)
 		    (add-to-list 'buffer-tags (list tg))
