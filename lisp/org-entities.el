b87ca833b3949101164cb3a4dca74c08a3c14d81
Mark Shoulson <mark@kli.org>
2012-05-28 22:48:07 -0400

    Add \asciicirc entity
    * org-entities.el (org-entities): Added \asciicirc entity for ^;
      also fixed \circ expansion in latex.
    
    TINYCHANGE

--- lisp/org-entities.el
+++ lisp/org-entities.el
@@ -252,7 +252,7 @@ loaded, add these packages to `org-export-latex-packages-alist'."
 
     "* Other"
     "** Misc. (often used)"
-    ("circ" "\\circ" t "&circ;" "^" "^" "ˆ")
+    ("circ" "\\^{}" nil "&circ;" "^" "^" "ˆ")
     ("vert" "\\vert{}" t "&#124;" "|" "|" "|")
     ("brvbar" "\\textbrokenbar{}" nil "&brvbar;" "|" "¦" "¦")
     ("sect" "\\S" nil "&sect;" "paragraph" "§" "§")
@@ -264,6 +264,7 @@ loaded, add these packages to `org-export-latex-packages-alist'."
     ("plus" "+" nil "+" "+" "+" "+")
     ("under" "\\_" nil "_" "_" "_" "_")
     ("equal" "=" nil "=" "=" "=" "=")
+    ("asciicirc" "\\textasciicircum{}" nil "^" "^" "^" "^")
     ("dagger" "\\textdagger{}" nil "&dagger;" "[dagger]" "[dagger]" "†")
     ("Dagger" "\\textdaggerdbl{}" nil "&Dagger;" "[doubledagger]" "[doubledagger]" "‡")
 
