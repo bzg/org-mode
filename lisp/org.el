aa490ffa04f21a03ec4e52a586500b41a0e90d33
Bastien Guerry <bzg@altern.org>
2012-08-02 11:59:27 +0200

    Small doc fix.
    * org.texi (Fast access to TODO states): Fix documentation
    about allowed characters for fast todo selection.
    
    * org.el (org-todo-keywords): Ditto.
    
    This fixes this wrong change here:
    http://orgmode.org/w/?p=org-mode.git;a=commit;h=b6cb72

--- lisp/org.el
+++ lisp/org.el
@@ -2105,11 +2105,11 @@ cycling, see the manual.
 TODO keywords and interpretation can also be set on a per-file basis with
 the special #+SEQ_TODO and #+TYP_TODO lines.
 
-Each keyword can optionally specify a letter for fast state selection
+Each keyword can optionally specify a character for fast state selection
 \(in combination with the variable `org-use-fast-todo-selection')
 and specifiers for state change logging, using the same syntax that
 is used in the \"#+TODO:\" lines.  For example, \"WAIT(w)\" says that
-the WAIT state can be selected with the \"w\" letter.  \"WAIT(w!)\"
+the WAIT state can be selected with the \"w\" key.  \"WAIT(w!)\"
 indicates to record a time stamp each time this state is selected.
 
 Each keyword may also specify if a timestamp or a note should be
