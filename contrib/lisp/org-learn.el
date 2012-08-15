a4bef9a73d3df8629bb3698738e0eb5e34744a60
Bastien Guerry <bzg@altern.org>
2012-08-16 00:01:59 +0200

    contrib: Do not tell files are part of Emacs if they are not (yet)

--- contrib/lisp/org-learn.el
+++ contrib/lisp/org-learn.el
@@ -7,7 +7,7 @@
 ;; Homepage: http://orgmode.org
 ;; Version: 6.32trans
 ;;
-;; This file is part of GNU Emacs.
+;; This file is not part of GNU Emacs.
 ;;
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
