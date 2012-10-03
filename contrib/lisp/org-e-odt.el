eba20536e966d27c07db166b6e6aa46d81a72937
Bastien Guerry <bzg@altern.org>
2012-10-03 12:13:17 +0200

    Revert "org-e-odt.el: Bugfix: default to the correct directory for styles."
    This reverts commit 27c673781309f1ad58a55c4130b793ff18fc7ac9.

--- contrib/lisp/org-e-odt.el
+++ contrib/lisp/org-e-odt.el
@@ -117,12 +117,9 @@
 (defconst org-e-odt-styles-dir
   (let* ((d (expand-file-name (org-find-library-dir "org-e-odt.el")))
 	 (styles-dir-list (list
-			   (concat d "etc/styles/") ;; ELPA and Git
-			   (concat d "etc/org/")    ;; Org from Emacs
-			   (concat d "../"
-				   (number-to-string emacs-major-version) "."
-				   (number-to-string emacs-minor-version) "/etc/org/")
-			   (concat data-directory "org/"))) ;; Custom install
+			   (concat d "../../etc/styles/") ;; Git
+			   (concat d "../../etc/org/")    ;; system
+			   (concat d "../etc/org/")))     ;; ELPA org-plus
 	 styles-dir)
     (setq styles-dir
 	  (car
@@ -136,8 +133,8 @@
 					"OrgOdtStyles.xml" sd)))
 			     sd))
 			 styles-dir-list))))
-    (or (expand-file-name styles-dir)
-	(message "Cannot find factory styles files for Org ODT")))
+    (or styles-dir
+	(error "Cannot find factory styles files for Org ODT, aborting")))
   "Directory that holds auxiliary XML files used by the ODT exporter.
 
 This directory contains the following XML files -
@@ -1829,7 +1826,7 @@ CONTENTS is nil.  INFO is a plist holding contextual information."
 
 ;;;; Latex Environment
 
-(eval-after-load 'org-odt
+(eval-after-load 'org-e-odt
   '(ad-deactivate 'org-format-latex-as-mathml))
 
 ;; (defadvice org-format-latex-as-mathml	; FIXME
