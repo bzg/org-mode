bzg/org-mode

commit aba9e2b94dadcd655e462252a5c79edf61bfa65c

org-src.el (org-edit-src-exit): Fix bug when saving an empty source buffer
* org-src.el (org-edit-src-exit): Fix bug when saving an empty
source buffer.

Thanks to Hsiu-Khuern Tang for reporting this.

lisp/org-src.el
@@ -689,9 +689,10 @@ with \",*\", \",#+\", \",,*\" and \",,#+\"."
       (kill-buffer buffer))
     (goto-char beg)
     (when allow-write-back-p
-      (delete-region beg (1- end))
-      (insert code)
-      (delete-char 1)
+      (delete-region beg (max beg (1- end)))
+      (unless (string-match "^[ \t]*$" code)
+	(insert code)
+	(delete-char 1))
       (goto-char beg)
       (if single (just-one-space)))
     (if (memq t (mapcar (lambda (overlay)

commit d22e732d7a688622729fb2d4994cf95a94dd0598

org-src.el (org-edit-src-exit): Fix editing source section with blank lines
* org-src.el (org-edit-src-exit): Fix editing source section
with blank lines.

TINYCHANGE

This bug was introduced by commit aba9e2b9.

lisp/org-src.el
@@ -690,7 +690,7 @@ with \",*\", \",#+\", \",,*\" and \",,#+\"."
     (goto-char beg)
     (when allow-write-back-p
       (delete-region beg (max beg (1- end)))
-      (unless (string-match "^[ \t]*$" code)
+      (unless (string-match "\\`[ \t]*\\'" code)
 	(insert code)
 	(delete-char 1))
       (goto-char beg)