bzg/org-mode

commit 907110e9133634670d31f6ca31bc0a3f4cd555ca

org-e-latex: Tables get correct amount of vertical space
* contrib/lisp/org-e-latex.el: Replaced a set of \begin{center}
  and \end{center} environment markers with a \centering
  declaration.

Using both \begin{table} and \being{center} environments leads to double
the vertical space around the float, whereas \centering adds none.

TINYCHANGE

contrib/lisp/org-e-latex.el
@@ -2302,14 +2302,13 @@ This function assumes TABLE has `org' as its `:type' attribute."
 		  (concat
 		   (format "\\begin{%s}%s\n" float-env placement)
 		   (if org-e-latex-table-caption-above caption "")))
-		(when org-e-latex-tables-centered "\\begin{center}\n")
+		(when org-e-latex-tables-centered "\\centering\n")
 		(format "\\begin{%s}%s{%s}\n%s\\end{%s}"
 			table-env
 			(if width (format "{%s}" width) "")
 			alignment
 			contents
 			table-env)
-		(when org-e-latex-tables-centered "\n\\end{center}")
 		(when float-env
 		  (concat (if org-e-latex-table-caption-above "" caption)
 			  (format "\n\\end{%s}" float-env))))))))

commit 359b10b3070588e8b751935b3fbcc2c9d8885a6a

org-e-latex: Fixes bug introduced by commit 907110e
* contrib/lisp/org-e-latex.el: Floating tables get a \centering
  declaration, otherwise the table get wrapped in \begin{center}
  and \end{center} environment.

Thanks to Andreas Leha for reporting it.

TINYCHANGE

contrib/lisp/org-e-latex.el
@@ -2302,20 +2302,22 @@ This function assumes TABLE has `org' as its `:type' attribute."
        (if (or org-e-latex-table-caption-above (string= "" caption)) ""
 	 (concat (org-trim caption) "\\\\\n"))))
      ;; Others.
-     (t (concat (when float-env
-		  (concat
-		   (format "\\begin{%s}%s\n" float-env placement)
-		   (if org-e-latex-table-caption-above caption "")))
-		(when org-e-latex-tables-centered "\\centering\n")
+     (t (concat (if float-env
+		    (concat
+		     (format "\\begin{%s}%s\n" float-env placement)
+		     (if org-e-latex-table-caption-above caption "")
+		     (when org-e-latex-tables-centered "\\centering\n"))
+		  (when org-e-latex-tables-centered "\\begin{center}\n"))
 		(format "\\begin{%s}%s{%s}\n%s\\end{%s}"
 			table-env
 			(if width (format "{%s}" width) "")
 			alignment
 			contents
 			table-env)
-		(when float-env
-		  (concat (if org-e-latex-table-caption-above "" caption)
-			  (format "\n\\end{%s}" float-env))))))))
+		(if float-env
+		    (concat (if org-e-latex-table-caption-above "" caption)
+			    (format "\n\\end{%s}" float-env))
+		  (when org-e-latex-tables-centered "\n\\end{center}")))))))
 
 (defun org-e-latex-table--table.el-table (table contents info)
   "Return appropriate LaTeX code for a table.el table.