72c2dc07a8b3a6463145d323793a3bed01bdc5b6
Bastien Guerry <bzg@altern.org>
2012-12-21 08:55:14 +0100

    test-ob-exp.el (ob-exp/exports-inline): Fix test
    * test-ob-exp.el (ob-exp/exports-inline): Fix test.

--- testing/lisp/test-ob-exp.el
+++ testing/lisp/test-ob-exp.el
@@ -232,7 +232,7 @@ elements in the final html."
 (ert-deftest ob-exp/exports-inline ()
   (org-test-at-id "54cb8dc3-298c-4883-a933-029b3c9d4b18"
     (org-narrow-to-subtree)
-    (let ((html (org-export-as-html nil nil nil 'string 'body-only)))
+    (let ((html (org-export-as-html nil nil 'string 'body-only)))
       (dolist (rx '("middle <\\(code\\|tt\\)>1</\\(code\\|tt\\)> of"
 		    "end of a line. <\\(code\\|tt\\)>2</\\(code\\|tt\\)>"
 		    "<\\(code\\|tt\\)>3</\\(code\\|tt\\)> Here is one"))
