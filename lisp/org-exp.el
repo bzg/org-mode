0f2bb6a56f4ff36cab13d0c6dd888a5b1691dc76
Bastien Guerry <bzg@altern.org>
2013-01-01 17:35:35 +0100

    org-exp.el (org-export-string): Fix number of arguments	passed to the org-export-as-* functions.
    * org-exp.el (org-export-string): Fix number of arguments
    passed to the org-export-as-* functions.
    
    This fixes commit 43a242, thanks to George McNinch for reporting this.

--- lisp/org-exp.el
+++ lisp/org-exp.el
@@ -3047,7 +3047,7 @@ to the value of `temporary-file-directory'."
 	  (eval ;; convert to fmt -- mimicking `org-run-like-in-org-mode'
 	   (list 'let org-local-vars
 		 (list (intern (format "org-export-as-%s" fmt))
-		       nil nil nil ''string t dir))))
+		       nil nil ''string t dir))))
       (delete-file tmp-file))))
 
 ;;;###autoload
