6a0c650dd2c2ba9f922663750e066fb47e2b2111
Bastien Guerry <bzg@altern.org>
2013-01-04 18:15:51 +0100

    org.el (org-get-priority): Save match data even when using `org-get-priority-function'
    * org.el (org-get-priority): Save match data even when using
    `org-get-priority-function'.

--- lisp/org.el
+++ lisp/org.el
@@ -13011,9 +13011,9 @@ and by additional input from the age of a schedules or deadline entry."
 
 (defun org-get-priority (s)
   "Find priority cookie and return priority."
-  (if (functionp org-get-priority-function)
-      (funcall org-get-priority-function)
-    (save-match-data
+  (save-match-data
+    (if (functionp org-get-priority-function)
+	(funcall org-get-priority-function)
       (if (not (string-match org-priority-regexp s))
 	  (* 1000 (- org-lowest-priority org-default-priority))
 	(* 1000 (- org-lowest-priority
