bzg/org-mode

commit 502e538020d02522dd40b4b5f940cf43d19096ab

contrib/lisp/: Replace the deleted function `org-make-link' by `concat'
* org-notmuch.el (org-notmuch-store-link)
(org-notmuch-search-store-link): Use `concat' instead of
`org-make-link'.

* org-git-link.el (org-git-create-git-link)
(org-git-insert-link-interactively): Ditto.

* org-bookmark.el (org-bookmark-store-link): Ditto.

Thanks to Friedrich Delgado for reporting this.

contrib/lisp/org-bookmark.el
@@ -80,7 +80,7 @@ Otherwise prompt the user for the right bookmark to use."
 		  (car bmks)
 		(completing-read "Bookmark: " bmks nil t nil nil (car bmks))))))
     (if bookmark
-	(org-store-link-props :link (org-make-link "bookmark:" bookmark)
+	(org-store-link-props :link (contact "bookmark:" bookmark)
 			      :description bookmark))))
 
 (provide 'org-bookmark)

contrib/lisp/org-git-link.el
@@ -171,7 +171,7 @@
   (let* ((gitdir (first (org-git-find-gitdir (file-truename file))))
          (branchname (org-git-get-current-branch gitdir))
          (timestring (format-time-string "%Y-%m-%d" (current-time))))
-    (org-make-link "git:" file "::" (org-git-create-searchstring branchname timestring))))
+    (contact "git:" file "::" (org-git-create-searchstring branchname timestring))))
 
 (defun org-git-store-link ()
   "Store git link to current file."
@@ -186,7 +186,7 @@
 
 (defun org-git-insert-link-interactively (file searchstring &optional description)
   (interactive "FFile: \nsSearch string: \nsDescription: ")
-  (insert (org-make-link-string (org-make-link "git:" file "::" searchstring) description)))
+  (insert (org-make-link-string (concat "git:" file "::" searchstring) description)))
 
 ;; Calling git
 (defun org-git-show (gitdir object buffer)

contrib/lisp/org-notmuch.el
@@ -58,7 +58,7 @@
       (org-store-link-props :type "notmuch" :from from :to to
        			    :subject subject :message-id message-id)
       (setq desc (org-email-link-description))
-      (setq link (org-make-link "notmuch:"  "id:" message-id))
+      (setq link (concat "notmuch:"  "id:" message-id))
       (org-add-link-props :link link :description desc)
       link)))
 
@@ -82,8 +82,8 @@ Can link to more than one message, if so all matching messages are shown."
 (defun org-notmuch-search-store-link ()
   "Store a link to a notmuch search or message."
   (when (eq major-mode 'notmuch-search-mode)
-    (let ((link (org-make-link "notmuch-search:"
-			       (org-link-escape notmuch-search-query-string)))
+    (let ((link (concat "notmuch-search:"
+			(org-link-escape notmuch-search-query-string)))
 	  (desc (concat "Notmuch search: " notmuch-search-query-string)))
       (org-store-link-props :type "notmuch-search"
 			    :link link

commit 3479cebca63381ff9538d93f97167a15935f10fc

Fix an error introduced in 502e538020d02522dd40b4b5f940cf43d19096ab, org-make-link was replaced by contact instead of concat as the comment suggested
	* org-git-link.el (org-git-create-git-link): Replace contact call by concat
	* org-bookmark.el (org-bookmark-store-link): Replace contact call by concat

TINYCHANGE

contrib/lisp/org-bookmark.el
@@ -80,7 +80,7 @@ Otherwise prompt the user for the right bookmark to use."
 		  (car bmks)
 		(completing-read "Bookmark: " bmks nil t nil nil (car bmks))))))
     (if bookmark
-	(org-store-link-props :link (contact "bookmark:" bookmark)
+	(org-store-link-props :link (concat "bookmark:" bookmark)
 			      :description bookmark))))
 
 (provide 'org-bookmark)

contrib/lisp/org-git-link.el
@@ -171,7 +171,7 @@
   (let* ((gitdir (first (org-git-find-gitdir (file-truename file))))
          (branchname (org-git-get-current-branch gitdir))
          (timestring (format-time-string "%Y-%m-%d" (current-time))))
-    (contact "git:" file "::" (org-git-create-searchstring branchname timestring))))
+    (concat "git:" file "::" (org-git-create-searchstring branchname timestring))))
 
 (defun org-git-store-link ()
   "Store git link to current file."