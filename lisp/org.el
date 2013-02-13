3f0b6082436d23cf799804179c676c7e2b53a658
Bastien Guerry <bzg@altern.org>
2013-02-13 14:25:36 +0100

    org.el (org-options-keywords): Add keywords
    * org.el (org-options-keywords): Add keywords.

--- lisp/org.el
+++ lisp/org.el
@@ -11536,7 +11536,8 @@ keywords relative to each registered export back-end."
     "DESCRIPTION:" "DRAWERS:" "EMAIL:" "EXCLUDE_TAGS:" "FILETAGS:" "INCLUDE:"
     "INDEX:" "KEYWORDS:" "LANGUAGE:" "MACRO:" "OPTIONS:" "PROPERTY"
     "PRIORITIES:" "SELECT_TAGS:" "SEQ_TODO:" "SETUPFILE:" "STARTUP:" "TAGS:"
-    "TITLE:" "TODO:" "TYP_TODO:"))
+    "TITLE:" "TODO:" "TYP_TODO:" "EXPORT_SELECT_TAGS" "EXPORT_EXCLUDE_TAGS"
+    "INFOJS_OPT"))
 
 (defcustom org-structure-template-alist
   '(("s" "#+BEGIN_SRC ?\n\n#+END_SRC"
