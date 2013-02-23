ceeee88f49a12fa77711e9579b9ce567a6b2b250
Bastien Guerry <bzg@altern.org>
2013-02-23 08:59:23 +0100

    Update header information.
    Those three files will be part of Emacs when Org 8.0
    will be merged with Emacs trunk.
    
    Thanks to Nick Dokos for pointing this.

--- lisp/ox-publish.el
+++ lisp/ox-publish.el
@@ -5,7 +5,7 @@
 ;; Maintainer: Carsten Dominik <carsten DOT dominik AT gmail DOT com>
 ;; Keywords: hypermedia, outlines, wp
 
-;; This file is not part of GNU Emacs.
+;; This file is part of GNU Emacs.
 ;;
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
