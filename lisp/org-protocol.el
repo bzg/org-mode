db750eba2820492ce28fb6078dbcd8f154f9d725
Bastien Guerry <bzg@altern.org>
2013-02-28 00:24:46 +0100

    org-protocol.el (org-protocol-capture): Small docstring fix
    * org-protocol.el (org-protocol-capture): Small docstring fix.

--- lisp/org-protocol.el
+++ lisp/org-protocol.el
@@ -391,8 +391,8 @@ The sub-protocol used to reach this function is set in
 The sub-protocol used to reach this function is set in
 `org-protocol-protocol-alist'.
 
-This function detects an URL, title and optional text, separated by '/'
-The location for a browser's bookmark has to look like this:
+This function detects an URL, title and optional text, separated
+by '/'.  The location for a browser's bookmark looks like this:
 
   javascript:location.href='org-protocol://capture://'+ \\
         encodeURIComponent(location.href)+'/' \\
