96aa12eebcfa50cec27fedc00273cbe568e95533
Bastien Guerry <bzg@altern.org>
2013-02-28 09:35:04 +0100

    Revert "org.el (org-speed-command-activate): Only forbid in src code blocks"
    This reverts commit 8ac66556bbac21e19f1494fc634ecb16b87bfd62.

--- lisp/org.el
+++ lisp/org.el
@@ -18833,7 +18833,10 @@ If not, return to the original position and throw an error."
 `org-speed-commands-default' specifies a minimal command set.
 Use `org-speed-commands-user' for further customization."
   (when (or (and (bolp) (looking-at org-outline-regexp)
-		 (not (org-in-block-p '("src"))))
+		 ;; FIXME org-speed-commands-forbidden-blocks?
+		 (not (org-in-block-p
+		       '("src" "example" "verse" "html"
+			 "latex" "ascii" "beamer" "odt"))))
 	    (and (functionp org-use-speed-commands)
 		 (funcall org-use-speed-commands)))
     (cdr (assoc keys (append org-speed-commands-user
