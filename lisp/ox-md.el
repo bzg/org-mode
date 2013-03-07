8bf9e9aff833f246d3a8904c2cf92aaebe9826c8
Bastien Guerry <bzg@altern.org>
2013-03-07 14:44:58 +0100

    ox-md.el: Fix a library keyword in the comment section
    * ox-md.el: Fix a library keyword in the comment section.

--- lisp/ox-md.el
+++ lisp/ox-md.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2012, 2013  Free Software Foundation, Inc.
 
 ;; Author: Nicolas Goaziou <n.goaziou@gmail.com>
-;; Keywords: org, wp, tex
+;; Keywords: org, wp, markdown
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
