df0c8a3bc44286c06b3306503a95382805651e2c
Eric Schulte <schulte.eric@gmail.com>
2013-03-29 08:48:51 -0600

    don't interpret single "-" as a number
    * lisp/ob-core.el (org-babel-number-p): Don't interpret single "-" as a
      number.

--- lisp/ob-core.el
+++ lisp/ob-core.el
@@ -2521,6 +2521,7 @@ appropriate."
 (defun org-babel-number-p (string)
   "If STRING represents a number return its value."
   (if (and (string-match "^-?[0-9]*\\.?[0-9]*$" string)
+	   (string-match "[0-9]+" string)
            (= (length (substring string (match-beginning 0)
 				 (match-end 0)))
 	      (length string)))
