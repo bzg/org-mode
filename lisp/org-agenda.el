a5d6548a57d2313dc4d1352fa9d379315159d2c4
Carsten Dominik <carsten.dominik@gmail.com>
2013-04-05 16:44:57 +0200

    Highlight only headline with agenda subtree restriction
    * lisp/org-agenda.el (org-agenda-set-restriction-lock): Highlight only
      the headline when agenda is restricted to a subtree.  Do not
      highlight the entire subtree.

--- lisp/org-agenda.el
+++ lisp/org-agenda.el
@@ -7069,7 +7069,7 @@ in the file.  Otherwise, restriction will be to the current subtree."
 	     (list (buffer-file-name (buffer-base-buffer))))
 	(org-back-to-heading t)
 	(move-overlay org-agenda-restriction-lock-overlay
-		      (point) (save-excursion (org-end-of-subtree t t) (point)))
+		      (point) (point-at-eol))
 	(move-marker org-agenda-restrict-begin (point))
 	(move-marker org-agenda-restrict-end
 		     (save-excursion (org-end-of-subtree t t)))
