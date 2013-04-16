de4d86c080802fe89c924c8f449fc1ca089ad1f6
Nicolas Goaziou <n.goaziou@gmail.com>
2013-04-16 12:51:08 +0200

    org-man.el: Fix code typo
    * contrib/lisp/org-man.el (org-man-export): Fix code typo.

--- contrib/lisp/org-man.el
+++ contrib/lisp/org-man.el
@@ -64,7 +64,7 @@ PATH should be a topic that can be thrown at the man command."
     (cond
      ((eq format 'html) (format "<a target=\"_blank\" href=\"%s\">%s</a>" path desc))
      ((eq format 'latex) (format "\\href{%s}{%s}" path desc))
-     ((eq format 'texinfo (format "@uref{%s,%s}" path desc)))
+     ((eq format 'texinfo) (format "@uref{%s,%s}" path desc))
      ((eq format 'ascii) (format "%s (%s)" desc path))
      (t path))))
 
