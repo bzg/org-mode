ea98942eecef728de686851fa617140aa5cd3930
Carsten Dominik <carsten.dominik@gmail.com>
2013-05-12 06:08:09 +0200

    Avoid using `case' in default value of defcustom
    * lisp/org-ctags.el (org-ctags-path-to-ctags): Avoid usine `case'.

--- lisp/org-ctags.el
+++ lisp/org-ctags.el
@@ -156,11 +156,8 @@ Format is: /REGEXP/TAGNAME/FLAGS,TAGTYPE/
 See the ctags documentation for more information.")
 
 (defcustom org-ctags-path-to-ctags
-  (case system-type
-    (windows-nt "ctags.exe")
-    (darwin "ctags-exuberant")
-    (t "ctags-exuberant"))
-  "Full path to the ctags executable file."
+  (if (executable-find "ctags-exuberant") "ctags-exuberant" "ctags")
+  "Name of the ctags executable file."
   :group 'org-ctags
   :version "24.1"
   :type 'file)
