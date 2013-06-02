e3cc1ca792bdd4bf6345faf1c9da90182cf7c137
Feng Shu <tumashu@gmail.com>
2013-06-03 07:35:09 +0800

    contrib/lisp/org-contacts.el: Add a complex org-contacts template to the file header
    * contrib/lisp/org-contacts.el: Add a complex org-contacts template to
    the file header, which can help users to know all the org-contacts
    properties quickly.

--- contrib/lisp/org-contacts.el
+++ contrib/lisp/org-contacts.el
@@ -25,13 +25,29 @@
 
 ;; This file contains the code for managing your contacts into Org-mode.
 
-;; To enter new contacts, you can use `org-capture' and a template just like
+;; To enter new contacts, you can use `org-capture' and a minimal template just like
 ;; this:
 
 ;;         ("c" "Contacts" entry (file "~/Org/contacts.org")
 ;;          "* %(org-contacts-template-name)
 ;; :PROPERTIES:
 ;; :EMAIL: %(org-contacts-template-email)
+;; :END:")))
+;;
+;; You can also use a complex template, for example:
+;;
+;;         ("c" "Contacts" entry (file "~/Org/contacts.org")
+;;          "* %(org-contacts-template-name)
+;; :PROPERTIES:
+;; :EMAIL: %(org-contacts-template-email)
+;; :PHONE:
+;; :ALIAS:
+;; :NICKNAME:
+;; :IGNORE:
+;; :ICON:
+;; :NOTE:
+;; :ADDRESS:
+;; :BIRTHDAY:
 ;; :END:")))
 ;;
 ;;; Code:
