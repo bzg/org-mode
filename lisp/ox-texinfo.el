32315be4da09c00602b60236a638f01545ca319c
Nicolas Goaziou <n.goaziou@gmail.com>
2013-09-20 10:41:21 +0200

    ox-texinfo: Fix 40aa589c4601e40e1051007802b539929ddd2f81
    * lisp/ox-texinfo.el (org-texinfo-export-to-texinfo): Fix
      40aa589c4601e40e1051007802b539929ddd2f81.

--- lisp/ox-texinfo.el
+++ lisp/ox-texinfo.el
@@ -1710,7 +1710,7 @@ file-local settings.
 Return output file's name."
   (interactive)
   (let ((outfile (org-export-output-file-name ".texi" subtreep))
-	(org-export-coding-system `,org-texinfo-coding-system))
+	(org-export-coding-system org-texinfo-coding-system))
     (org-export-to-file 'texinfo outfile
       async subtreep visible-only body-only ext-plist)))
 
