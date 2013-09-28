6f90d903e6de1ed54f0c1f19ed57d02e9327891c
Eric Schulte <schulte.eric@gmail.com>
2013-09-28 06:11:36 -0600

    Revert "don't insert a padline at the top of the file"
    This reverts commit 6215960d99b47e2bb5141bcd97b229d9c90c4db2.

--- lisp/ob-tangle.el
+++ lisp/ob-tangle.el
@@ -327,7 +327,7 @@ that the appropriate major-mode is set.  SPEC has the form:
       (funcall
        insert-comment
        (org-fill-template org-babel-tangle-comment-format-beg link-data)))
-    (when (and padline (not (= (point) (point-min)))) (insert "\n"))
+    (when padline (insert "\n"))
     (insert
      (format
       "%s\n"
