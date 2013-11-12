9ff70abc14e901ce481cb605c0d572581dd40716
Nicolas Richard <theonewiththeevillook@yahoo.fr>
2013-11-12 09:02:23 +0100

    Use key-description on the keys mentionned by the functions defined by `orgtbl-mode'.

--- lisp/org-table.el
+++ lisp/org-table.el
@@ -4124,7 +4124,7 @@ to execute outside of tables."
 	 '(arg)
 	 (concat "In tables, run `" (symbol-name fun) "'.\n"
 		 "Outside of tables, run the binding of `"
-		 (mapconcat (lambda (x) (format "%s" x)) keys "' or `")
+		 (mapconcat #'key-description keys "' or `")
 		 "'.")
 	 '(interactive "p")
 	 (list 'if
