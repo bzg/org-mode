942a95cd59c18eb3036d393e7759f86670cae507
Alex Kosorukoff <alex@3form.com>
2014-05-23 21:50:24 +0200

    org-capture.el (org-capture-fill-template): Set `mark-active' to nil
    * org-capture.el (org-capture-fill-template): Set
    `mark-active' to nil.
    
    TINYCHANGE

--- lisp/org-capture.el
+++ lisp/org-capture.el
@@ -1587,7 +1587,7 @@ The template may still contain \"%?\" for cursor positioning."
       (insert template)
       (goto-char (point-min))
       (org-capture-steal-local-variables buffer)
-      (setq buffer-file-name nil)
+      (setq buffer-file-name nil mark-active nil)
 
       ;; %[] Insert contents of a file.
       (goto-char (point-min))
