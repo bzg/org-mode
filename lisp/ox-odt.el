d81e6b52b7a02bb3401f2f17dedc746b63eeecc7
Stefan Monnier <monnier@iro.umontreal.ca>
2014-05-28 23:45:29 -0400

    Backport commit 6711a21 from Emacs master branch
    * lisp/org-compat.el (org-font-lock-ensure): New function.
    * lisp/ox-odt.el (org-odt-do-format-code):
    * lisp/ox-html.el (org-html-fontify-code):
    * lisp/org.el (org-fontify-like-in-org-mode):
    * lisp/org-src.el (org-src-font-lock-fontify-block):
    * lisp/org-clock.el (org-clock-get-clocktable): Use it.
    * lisp/ox-org.el (org-org-publish-to-org):
    Use it. Avoid using find-file from Elisp.
    
    6711a21f1125c0047c56eb266eb374c1ec90a967
    Stefan Monnier
    Wed May 28 23:45:29 2014 -0400

--- lisp/ox-odt.el
+++ lisp/ox-odt.el
@@ -3139,7 +3139,7 @@ and prefix with \"OrgSrc\".  For example,
 		 (with-temp-buffer
 		   (insert code)
 		   (funcall lang-mode)
-		   (font-lock-fontify-buffer)
+		   (org-font-lock-ensure)
 		   (buffer-string))))
 	 (fontifier (if use-htmlfontify-p 'org-odt-htmlfontify-string
 		      'org-odt--encode-plain-text))
