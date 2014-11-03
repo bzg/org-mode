31b56b9c9dc5db714fad0a8072925704a2aa0f88
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2014-11-03 21:58:37 +0100

    Update `customize-package-emacs-version-alist'
    * lisp/org.el (customize-package-emacs-version-alist): Update value.

--- lisp/org.el
+++ lisp/org.el
@@ -5541,7 +5541,7 @@ The following commands are available:
 (add-to-list 'customize-package-emacs-version-alist
 	     '(Org ("6.21b" . "23.1")  ("6.33x" . "23.2")
 		   ("7.8.11" . "24.1") ("7.9.4" . "24.3")
-		   ("8.2.6" . "24.4")))
+		   ("8.2.6" . "24.4") ("8.3" . "25.1")))
 
 (defvar org-mode-transpose-word-syntax-table
   (let ((st (make-syntax-table text-mode-syntax-table)))
