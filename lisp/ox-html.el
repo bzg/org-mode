bzg/org-mode

commit 077a24b8d0c90f864502ac15bce9187de82fa81b

ox-html.el: Trim list items
* ox-html.el (org-html-format-list-item): Trim contents to avoid
  spurious space.

lisp/ox-html.el
@@ -2508,7 +2508,7 @@ INFO is a plist holding contextual information.  See
 			  class (concat checkbox term))
 		  "<dd>"))))
      (unless (eq type 'descriptive) checkbox)
-     contents
+     (org-trim contents)
      (case type
        (ordered "</li>")
        (unordered "</li>")

testing/lisp/test-org-list.el
@@ -792,7 +792,7 @@
 (ert-deftest test-org-list/to-html ()
   "Test `org-list-to-html' specifications."
   (should
-   (equal "<ul class=\"org-ul\">\n<li>a\n</li>\n</ul>"
+   (equal "<ul class=\"org-ul\">\n<li>a</li>\n</ul>"
 	  (let (org-html-indent)
 	    (with-temp-buffer
 	      (insert "<!-- BEGIN RECEIVE ORGLST name -->

commit 86588d65013edcb2614c715551eecaaba35d2a3a

ox-html.el: Fix bug introduced in 077a24b
* ox-html.el (org-html-format-list-item): Check that contents in
  non-nil.

lisp/ox-html.el
@@ -2524,7 +2524,7 @@ INFO is a plist holding contextual information.  See
 			  class (concat checkbox term))
 		  "<dd>"))))
      (unless (eq type 'descriptive) checkbox)
-     (org-trim contents)
+     (and contents (org-trim contents))
      (case type
        (ordered "</li>")
        (unordered "</li>")