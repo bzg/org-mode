18685d98527e0479b8108cac420b23d481fd5bfd
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2015-02-28 00:43:37 +0100

    org-clock: Fix ignored integer values for `org-clock-into-drawer'
    * lisp/org-clock.el (org-clock-into-drawer): Fix ignored integer
      values.
    
    Reported-by: Rainer Stengele <rainer.stengele@online.de>
    <http://permalink.gmane.org/gmane.emacs.orgmode/95504>

--- lisp/org-clock.el
+++ lisp/org-clock.el
@@ -87,6 +87,7 @@ Return value is either a string, an integer, or nil."
           ((org-string-nw-p p)
 	   (if (org-string-match-p "\\`[0-9]+\\'" p) (string-to-number p) p))
 	  ((org-string-nw-p org-clock-into-drawer))
+	  ((integerp org-clock-into-drawer) org-clock-into-drawer)
 	  ((not org-clock-into-drawer) nil)
 	  ((org-log-into-drawer))
 	  (t "LOGBOOK"))))
