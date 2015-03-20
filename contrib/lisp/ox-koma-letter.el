bzg/org-mode

commit 304a5cb03d1bf9d5900ff1e13b8216ba4041bddf

ox-koma-letter: Interpret #+SUBJECT.
* ox-koma-letter.el (org-koma-letter-template): Interpret #+SUBJECT.

contrib/lisp/ox-koma-letter.el
@@ -630,7 +630,11 @@ holding export options."
 		    (mapconcat #'symbol-name with-subject ","))))
 	(let* ((title-as-subject (plist-get info :with-title-as-subject))
 	       (subject* (org-string-nw-p
-			  (org-export-data (plist-get info :subject) info)))
+			  (org-export-data
+			   (org-element-parse-secondary-string
+			    (plist-get info :subject)
+			    (org-element-restriction 'keyword))
+			   info)))
 	       (title* (and (plist-get info :with-title)
 			    (org-string-nw-p
 			     (org-export-data (plist-get info :title) info))))

commit 1ce810f9abc0db10037c88dfb60cf5e4c7656268

ox-koma-letter: fix bug introduced in 304a5cb
* ox-koma-letter.el (org-koma-letter-template): Handle empty SUBJECT.

contrib/lisp/ox-koma-letter.el
@@ -632,7 +632,7 @@ holding export options."
 	       (subject* (org-string-nw-p
 			  (org-export-data
 			   (org-element-parse-secondary-string
-			    (plist-get info :subject)
+			    (or (plist-get info :subject) "")
 			    (org-element-restriction 'keyword))
 			   info)))
 	       (title* (and (plist-get info :with-title)