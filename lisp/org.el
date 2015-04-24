63a315384265a8be7857f33cdad0b2ef0b32637f
Kyle Meyer <kyle@kyleam.com>
2015-04-24 12:53:43 -0400

    org.el: Remove org-timer-cancel-timer binding
    * lisp/org.el (org-mode-map): Remove binding for deleted command
      org-timer-cancel-timer.
    
    The command org-timer-cancel-timer was removed in 173b0cb6d6.

--- lisp/org.el
+++ lisp/org.el
@@ -19887,7 +19887,6 @@ boundaries."
 (org-defkey org-mode-map "\C-c\C-xo"    'org-toggle-ordered-property)
 (org-defkey org-mode-map "\C-c\C-xi"    'org-insert-columns-dblock)
 (org-defkey org-mode-map [(control ?c) (control ?x) ?\;] 'org-timer-set-timer)
-(org-defkey org-mode-map [(control ?c) (control ?x) ?\:] 'org-timer-cancel-timer)
 
 (org-defkey org-mode-map "\C-c\C-x."    'org-timer)
 (org-defkey org-mode-map "\C-c\C-x-"    'org-timer-item)
