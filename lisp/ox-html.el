bzg/org-mode

commit da571868a56d33ef970cf0072f34d0ed8db91c55

ox-html: Fix paragraph class in footnote definitions
* lisp/ox-html.el (org-html-footnote-section):
(org-html-paragraph): Add "footpara" class for footnote definitions.

Reported-by: Rasmus <rasmus@gmx.us>
<http://permalink.gmane.org/gmane.emacs.orgmode/97157>

lisp/ox-html.el
@@ -1649,7 +1649,7 @@ INFO is a plist used as a communication channel."
 	  (loop for (n type raw) in fn-alist collect
 		(cons n (if (eq (org-element-type raw) 'org-data)
 			    (org-trim (org-export-data raw info))
-			  (format "<p>%s</p>"
+			  (format "<p class=\"footpara\">%s</p>"
 				  (org-trim (org-export-data raw info))))))))
     (when fn-alist
       (format
@@ -2988,10 +2988,11 @@ CONTENTS is the contents of the paragraph, as a string.  INFO is
 the plist used as a communication channel."
   (let* ((parent (org-export-get-parent paragraph))
 	 (parent-type (org-element-type parent))
-	 (style '((footnote-definition " class=\"footpara\"")))
+	 (style '((footnote-definition " class=\"footpara\"")
+		  (org-data . " class=\"footpara\"")))
 	 (attributes (org-html--make-attribute-string
 		      (org-export-read-attribute :attr_html paragraph)))
-	 (extra (or (cadr (assoc parent-type style)) "")))
+	 (extra (or (cadr (assq parent-type style)) "")))
     (cond
      ((and (eq parent-type 'item)
 	   (not (org-export-get-previous-element paragraph info))

commit 4542ef8799be563a0d2c84bfdcb4cca8f500f184

ox-html: Fix typo
* lisp/ox-html.el (org-html-paragraph): Fix typo introduced in
  da571868a56d33ef970cf0072f34d0ed8db91c55.

lisp/ox-html.el
@@ -2989,7 +2989,7 @@ the plist used as a communication channel."
   (let* ((parent (org-export-get-parent paragraph))
 	 (parent-type (org-element-type parent))
 	 (style '((footnote-definition " class=\"footpara\"")
-		  (org-data . " class=\"footpara\"")))
+		  (org-data " class=\"footpara\"")))
 	 (attributes (org-html--make-attribute-string
 		      (org-export-read-attribute :attr_html paragraph)))
 	 (extra (or (cadr (assq parent-type style)) "")))