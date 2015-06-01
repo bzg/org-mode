458bde5243d759b5b8cad1d7f61cfe5fa638ba54
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2015-06-01 09:32:32 +0200

    Revert "Silence byte-compiler"
    This reverts commit 722b5e724e6f1fb1ae906e00446cf3f2c18d1da7.

--- lisp/org.el
+++ lisp/org.el
@@ -76,7 +76,6 @@
 (require 'calendar)
 (require 'find-func)
 (require 'format-spec)
-(autoload 'format-spec "format-spec")	; Silence "make single".
 
 (or (equal this-command 'eval-buffer)
     (condition-case nil
