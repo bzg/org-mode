c15db80e32d22dace184203f6f03a0b586bb4b50
Sergey Gordienko <gordienkos@gmail.com>
2015-06-26 17:44:20 +0300

    org-mobile: Tiny fix in creating index.org file
    * lisp/org-mobile.el (org-mobile-create-index-file):
    Eliminate cloning of `#+READONLY' property in the `index.org' file.
    A sublists of `org-todo-keywords' don't need to contain the `|'.
    
    TINYCHANGE

--- lisp/org-mobile.el
+++ lisp/org-mobile.el
@@ -439,14 +439,14 @@ agenda view showing the flagged items."
 	(if org-mobile-use-encryption
 	    org-mobile-encryption-tempfile
 	  target-file)
+      (insert "#+READONLY\n")
       (while (setq entry (pop def-todo))
-	(insert "#+READONLY\n")
 	(setq kwds (mapcar (lambda (x) (if (string-match "(" x)
 					   (substring x 0 (match-beginning 0))
 					 x))
 			   (cdr entry)))
 	(insert "#+TODO: " (mapconcat 'identity kwds " ") "\n")
-	(setq dwds (member "|" kwds)
+	(setq dwds (or (member "|" kwds) (last kwds))
 	      twds (org-delete-all dwds kwds)
 	      todo-kwds (org-delete-all twds todo-kwds)
 	      done-kwds (org-delete-all dwds done-kwds)))
