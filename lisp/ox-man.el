3a81c7ee6f392463db1db4b3e77a0e0411032382
Paul Eggert <eggert@cs.ucla.edu>
2015-08-05 19:06:57 -0700

    Backport commit 0c856a2 from Emacs master branch
    * lisp/ob-core.el (org-babel-check-evaluate)
    (org-babel-confirm-evaluate):
    * lisp/ob-fortran.el (org-babel-fortran-var-to-fortran):
    * lisp/ox-latex.el (org-latex-compile):
    * lisp/ox-man.el (org-man-compile):
    * lisp/ox-odt.el (org-odt-template):
    * lisp/ox-texinfo.el (org-texinfo-compile):
    Change (message (format ...)) to (message ...), and likewise
    for ‘error’.  This lessens the probability of confusion when the
    output of ‘format’ contains ‘%’.
    
    Fix some confusion with ‘format’
    0c856a2c459016f1f9cce173f5b2b49be36e3452
    Paul Eggert
    Wed Aug 5 19:07:28 2015 -0700

--- lisp/ox-man.el
+++ lisp/ox-man.el
@@ -1196,7 +1196,7 @@ Return PDF file name or an error if it couldn't be produced."
 				(file-name-directory full-name)
 			      default-directory))
          errors)
-    (message (format "Processing Groff file %s..." file))
+    (message "Processing Groff file %s..." file)
     (save-window-excursion
       (cond
        ;; A function is provided: Apply it.
