c27c101fa3ea4f8c8169a5ec52beb2b188aeb6b5
Bastien <bzg@gnu.org>
2015-08-16 15:14:27 +0200

    org.el: Fix `org-comment-string' fontification
    * org.el (org-set-font-lock-defaults): Don't fontify
    `org-comment-string' when not in a headline.

--- lisp/org.el
+++ lisp/org.el
@@ -6365,7 +6365,6 @@ needs to be inserted at a specific position in the font-lock sequence.")
 			     ":\\).*$")
 		     '(1 'org-tag-group prepend)))
 	   ;; Special keywords
-	   (list (concat "\\<" org-comment-string) '(0 'org-special-keyword t))
 	   (list (concat "\\<" org-deadline-string) '(0 'org-special-keyword t))
 	   (list (concat "\\<" org-scheduled-string) '(0 'org-special-keyword t))
 	   (list (concat "\\<" org-closed-string) '(0 'org-special-keyword t))
