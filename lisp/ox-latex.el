6f41105bd97c4a64272ec23493d9ae3ced4c4cbe
Bastien <bzg@gnu.org>
2015-08-18 19:23:34 +0200

    ox-latex.el: Resurrect `org-latex-with-hyperref'
    * ox-latex.el (latex): Resurrect :latex-hyperref-p.
    (org-latex-with-hyperref): Resurrect.
    (org-latex-template): Use :latex-hyperref-p.
    
    * org.texi (Beamer specific export settings)
    (@LaTeX{} specific export settings): Mention both
    `org-latex-hyperref-template' and `org-latex-with-hyperref'.
    
    Thanks to Scott Randby for raising this issue.

--- lisp/ox-latex.el
+++ lisp/ox-latex.el
@@ -123,6 +123,7 @@
     (:latex-format-drawer-function nil nil org-latex-format-drawer-function)
     (:latex-format-headline-function nil nil org-latex-format-headline-function)
     (:latex-format-inlinetask-function nil nil org-latex-format-inlinetask-function)
+    (:latex-hyperref-p nil "texht" org-latex-with-hyperref t)
     (:latex-hyperref-template nil nil org-latex-hyperref-template t)
     (:latex-image-default-height nil nil org-latex-image-default-height)
     (:latex-image-default-option nil nil org-latex-image-default-option)
@@ -589,6 +590,11 @@ This command only applies to the table of contents generated with
 the toc:nil option, not to those generated with #+TOC keyword."
   :group 'org-export-latex
   :type 'string)
+
+(defcustom org-latex-with-hyperref t
+  "Toggle insertion of `org-latex-hyperref-template' in the preamble."
+  :group 'org-export-latex
+  :type 'boolean)
 
 (defcustom org-latex-hyperref-template
   "\\hypersetup{\n pdfauthor={%a},\n pdftitle={%t},\n pdfkeywords={%k},
@@ -1541,9 +1547,10 @@ holding export options."
 	(when (and separate subtitle)
 	  (concat formatted-subtitle "\n"))))
      ;; Hyperref options.
-     (let ((template (plist-get info :latex-hyperref-template)))
-       (and (stringp template)
-            (format-spec template spec)))
+     (when (plist-get info :latex-hyperref-p)
+       (let ((template (plist-get info :latex-hyperref-template)))
+	 (and (stringp template)
+	      (format-spec template spec))))
      ;; Document start.
      "\\begin{document}\n\n"
      ;; Title command.
