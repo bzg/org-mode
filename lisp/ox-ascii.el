bzg/org-mode

commit d6a718a93fc53fff68c9497ab181938b09950f10

ox-ascii: Fix external id links
* lisp/ox-ascii.el (org-ascii--describe-links): Fix export when id link
  targets an external file.

Reported-by: Samuel Wales <samologist@gmail.com>
<http://permalink.gmane.org/gmane.emacs.orgmode/101978>

lisp/ox-ascii.el
@@ -916,19 +916,23 @@ channel."
 	;; Id and custom-id links: Headlines refer to their numbering.
 	((member type '("custom-id" "id"))
 	 (let ((dest (org-export-resolve-id-link link info)))
-	   (concat
-	    (org-ascii--fill-string
-	     (format
-	      "[%s] %s"
-	      anchor
-	      (if (not dest) (org-ascii--translate "Unknown reference" info)
-		(format
-		 (org-ascii--translate "See section %s" info)
-		 (if (org-export-numbered-headline-p dest info)
-		     (mapconcat #'number-to-string
-				(org-export-get-headline-number dest info) ".")
-		   (org-export-data (org-element-property :title dest) info)))))
-	     width info) "\n\n")))
+	   (if (concat
+		(org-ascii--fill-string
+		 (format
+		  "[%s] %s"
+		  anchor
+		  (if (stringp dest)	; External file.
+		      dest
+		    (format
+		     (org-ascii--translate "See section %s" info)
+		     (if (org-export-numbered-headline-p dest info)
+			 (mapconcat #'number-to-string
+				    (org-export-get-headline-number dest info)
+				    ".")
+		       (org-export-data
+			(org-element-property :title dest) info)))))
+		 width info)
+		"\n\n"))))
 	;; Do not add a link that cannot be resolved and doesn't have
 	;; any description: destination is already visible in the
 	;; paragraph.

commit 8e4650c887e9affee9f7c94c087f7c511b0cbea3

ox-ascii: Fix code typo
* lisp/ox-ascii.el (org-ascii--describe-links): Fix code typo introduced
  in d6a718a.

lisp/ox-ascii.el
@@ -916,23 +916,22 @@ channel."
 	;; Id and custom-id links: Headlines refer to their numbering.
 	((member type '("custom-id" "id"))
 	 (let ((dest (org-export-resolve-id-link link info)))
-	   (if (concat
-		(org-ascii--fill-string
-		 (format
-		  "[%s] %s"
-		  anchor
-		  (if (stringp dest)	; External file.
-		      dest
-		    (format
-		     (org-ascii--translate "See section %s" info)
-		     (if (org-export-numbered-headline-p dest info)
-			 (mapconcat #'number-to-string
-				    (org-export-get-headline-number dest info)
-				    ".")
-		       (org-export-data
-			(org-element-property :title dest) info)))))
-		 width info)
-		"\n\n"))))
+	   (concat
+	    (org-ascii--fill-string
+	     (format
+	      "[%s] %s"
+	      anchor
+	      (if (stringp dest)	; External file.
+		  dest
+		(format
+		 (org-ascii--translate "See section %s" info)
+		 (if (org-export-numbered-headline-p dest info)
+		     (mapconcat #'number-to-string
+				(org-export-get-headline-number dest info)
+				".")
+		   (org-export-data (org-element-property :title dest) info)))))
+	     width info)
+	    "\n\n")))
 	;; Do not add a link that cannot be resolved and doesn't have
 	;; any description: destination is already visible in the
 	;; paragraph.