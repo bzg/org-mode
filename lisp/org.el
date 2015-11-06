fa1a9aad8e574b399db168c5ea73a79fcb5b1017
Aaron Ecay <aaronecay@gmail.com>
2015-11-06 12:28:59 +0000

    org.el: Don’t special-case ‘define-obsolete-function-alias’ for emacs <= 23

--- lisp/org.el
+++ lisp/org.el
@@ -8116,13 +8116,7 @@ even level numbers will become the next higher odd number."
 	    ((> change 0) (1+ (* 2 (/ (+ level (* 2 change)) 2))))
 	    ((< change 0) (max 1 (1+ (* 2 (/ (+ level (* 2 change)) 2))))))
     (max 1 (+ level (or change 0)))))
-
-(if (boundp 'define-obsolete-function-alias)
-    (if (or (featurep 'xemacs) (< emacs-major-version 23))
-	(define-obsolete-function-alias 'org-get-legal-level
-	  'org-get-valid-level)
-      (define-obsolete-function-alias 'org-get-legal-level
-	'org-get-valid-level "23.1")))
+(define-obsolete-function-alias 'org-get-legal-level 'org-get-valid-level "23.1")
 
 (defun org-promote ()
   "Promote the current heading higher up the tree."
