e42454088ff1176cce9cc65a4bd1c6f17fe958ec
Charles Berry <ccberry@ucsd.edu>
2016-04-22 21:04:16 -0700

    ob-comint.el: Fix bug and remove unused let binding
    * ob-comint.el: (tramp-flush-directory-property declaration): The
      file is now "tramp-cache".
    
    * ob-comint.el (org-babel-comint-eval-invisibly-and-wait-for-file):
      Remove unused let binding for `v' (which is let bound by
      `with-parsed-tramp-file-name').
    
    Bug reported by Vikas Rawal
    Subject: Using tramp to run R source code blocks on remote server
    Archived-At: <http://permalink.gmane.org/gmane.emacs.orgmode/106557>

--- lisp/ob-comint.el
+++ lisp/ob-comint.el
@@ -35,7 +35,7 @@
 (require 'comint)
 (eval-when-compile (require 'cl))
 (declare-function with-parsed-tramp-file-name "tramp" (filename var &rest body))
-(declare-function tramp-flush-directory-property "tramp" (vec directory))
+(declare-function tramp-flush-directory-property "tramp-cache" (vec directory))
 
 (defun org-babel-comint-buffer-livep (buffer)
   "Check if BUFFER is a comint buffer with a live process."
@@ -151,9 +151,8 @@ FILE exists at end of evaluation."
    (if (= (aref string (1- (length string))) ?\n) string (concat string "\n")))
   ;; From Tramp 2.1.19 the following cache flush is not necessary
   (if (file-remote-p default-directory)
-      (let (v)
-	(with-parsed-tramp-file-name default-directory nil
-	  (tramp-flush-directory-property v ""))))
+      (with-parsed-tramp-file-name default-directory nil
+	(tramp-flush-directory-property v "")))
   (while (not (file-exists-p file)) (sit-for (or period 0.25))))
 
 (provide 'ob-comint)
