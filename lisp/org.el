0d72c3493782e418a4aab8ce6f3cb552fdb089e5
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2016-06-24 09:21:38 +0200

    Improve a docstring
    * lisp/org.el (org-delete-all): Improve docstring.

--- lisp/org.el
+++ lisp/org.el
@@ -22720,7 +22720,9 @@ The function returns the new ALIST."
     rtn))
 
 (defun org-delete-all (elts list)
-  "Remove all elements in ELTS from LIST."
+  "Remove all elements in ELTS from LIST.
+Comparison is done with `equal'.  It is a destructive operation
+that may remove elements by altering the list structure."
   (while elts
     (setq list (delete (pop elts) list)))
   list)
