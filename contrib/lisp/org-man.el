f2bfa80c4c803a36b87b6fb540797a1f97718691
Robert Klein <roklein@roklein.de>
2016-08-08 03:24:23 +0200

    Update man link definition
    * contrib/lisp/org-man.el ("man"):
    
    Update to use org-link-set-parameters.

--- contrib/lisp/org-man.el
+++ contrib/lisp/org-man.el
@@ -25,8 +25,10 @@
 
 (require 'org)
 
-(org-add-link-type "man" 'org-man-open 'org-man-export)
-(add-hook 'org-store-link-functions 'org-man-store-link)
+(org-link-set-parameters "man"
+			 :follow #'org-man-open
+			 :export #'org-man-export
+			 :store #'org-man-store-link)
 
 (defcustom org-man-command 'man
   "The Emacs command to be used to display a man page."
