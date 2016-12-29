57ca9ba80a8527e2ecbcf17c057d9eea52edf2e3
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2016-12-29 01:19:00 +0100

    org-clock: Fix ":formula %" with disparate duration formats
    * lisp/org-clock.el (org-clock-time%): Handle various duration
      formats.  Refactor code.
    
    * testing/lisp/test-org-clock.el (test-org-clock/clocktable): Add
      tests.
    
    Reported-by: Andreas Mueller <lists@0x7.ch>
    <http://permalink.gmane.org/gmane.emacs.orgmode/111067>

--- lisp/org-clock.el
+++ lisp/org-clock.el
@@ -2852,23 +2852,16 @@ TOTAL s a time string like 10:21 specifying the total times.
 STRINGS is a list of strings that should be checked for a time.
 The first string that does have a time will be used.
 This function is made for clock tables."
-  (let ((re "\\([0-9]+\\):\\([0-9]+\\)")
-	tot s)
-    (save-match-data
-      (catch 'exit
-	(if (not (string-match re total))
-	    (throw 'exit 0.)
-	  (setq tot (+ (string-to-number (match-string 2 total))
-		       (* 60 (string-to-number (match-string 1 total)))))
-	  (if (= tot 0.) (throw 'exit 0.)))
-	(while (setq s (pop strings))
-	  (if (string-match "\\([0-9]+\\):\\([0-9]+\\)" s)
-	      (throw 'exit
-		     (/ (* 100.0 (+ (string-to-number (match-string 2 s))
-				    (* 60 (string-to-number
-					   (match-string 1 s)))))
-			tot))))
-	0))))
+  (save-match-data
+    (let ((total (org-duration-string-to-minutes total)))
+      (if (= total 0) 0
+	(cl-some (lambda (s)
+		   ;; Any number can express a duration.  See
+		   ;; `org-hh:mm-string-to-minutes' for details.
+		   (and (string-match-p "[0-9]" s)
+			(/ (* 100.0 (org-duration-string-to-minutes s))
+			   total)))
+		 strings)))))
 
 ;; Saving and loading the clock
 
