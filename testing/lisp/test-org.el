bzg/org-mode

commit aef7eef0f9070e2cd487f9e6a31bd0aee9ed15b4

Fix `org-get-repeat'
* lisp/org.el (org-get-repeat): Prevent false positive.

lisp/org.el
@@ -13199,11 +13199,13 @@ on INACTIVE-OK."
   (save-match-data
     (save-excursion
       (org-back-to-heading t)
-      (and (re-search-forward (if tagline
-				  (concat tagline "\\s-*" org-repeat-re)
-				org-repeat-re)
-			      (org-entry-end-position) t)
-	   (match-string-no-properties 1)))))
+      (let ((end (org-entry-end-position))
+	    (regexp (if tagline (concat tagline "\\s-*" org-repeat-re)
+		      org-repeat-re)))
+	(catch :repeat
+	  (while (re-search-forward regexp end t)
+	    (when (org-at-timestamp-p)
+	      (throw :repeat (match-string-no-properties 1)))))))))
 
 (defvar org-last-changed-timestamp)
 (defvar org-last-inserted-timestamp)

commit 737ada43c6539b6ae49550d474da7427a7120a2f

Fix "Fix `org-get-repeat'"
* lisp/org.el (org-get-repeat): Make sure matching group refers to the
  proper regexp.
* testing/lisp/test-org.el (test-org/get-repeat): New test.

The bug was introduced in aef7eef0f.

lisp/org.el
@@ -13204,7 +13204,7 @@ on INACTIVE-OK."
 		      org-repeat-re)))
 	(catch :repeat
 	  (while (re-search-forward regexp end t)
-	    (when (org-at-timestamp-p)
+	    (when (save-match-data (org-at-timestamp-p))
 	      (throw :repeat (match-string-no-properties 1)))))))))
 
 (defvar org-last-changed-timestamp)

testing/lisp/test-org.el
@@ -5595,6 +5595,28 @@ Paragraph<point>"
    (org-test-with-temp-text "<2012-03-29 Thu>"
      (org-timestamp-has-time-p (org-element-context)))))
 
+(ert-deftest test-org/get-repeat ()
+  "Test `org-get-repeat' specifications."
+  (should
+   (org-test-with-temp-text "* H\n<2012-03-29 Thu 16:40 +2y>"
+     (org-get-repeat)))
+  (should-not
+   (org-test-with-temp-text "* H\n<2012-03-29 Thu 16:40>"
+     (org-get-repeat)))
+  ;; Return proper repeat string.
+  (should
+   (equal "+2y"
+	  (org-test-with-temp-text "* H\n<2014-03-04 Tue 16:40 +2y>"
+	    (org-get-repeat))))
+  ;; Prevent false positive (commented or verbatim time stamps)
+  (should-not
+   (org-test-with-temp-text "* H\n# <2012-03-29 Thu 16:40>"
+     (org-get-repeat)))
+  (should-not
+   (org-test-with-temp-text
+       "* H\n#+BEGIN_EXAMPLE\n<2012-03-29 Thu 16:40>\n#+END_EXAMPLE"
+     (org-get-repeat))))
+
 (ert-deftest test-org/timestamp-format ()
   "Test `org-timestamp-format' specifications."
   ;; Regular test.