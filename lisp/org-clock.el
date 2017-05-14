93e5d92383a9935160903d9da57f0bf33bc065ed
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2017-05-15 00:34:58 +0200

    org-clock: Disambiguate character syntax
    * lisp/org-clock.el (org-clock-put-overlay): Escape character in
      character syntax.

--- lisp/org-clock.el
+++ lisp/org-clock.el
@@ -1961,7 +1961,8 @@ will be easy to remove."
 			 (make-string
 			  (max 0 (- (- 60 (current-column))
 				    (- (match-end 4) (match-beginning 4))
-				    (length (org-get-at-bol 'line-prefix)))) ?·)
+				    (length (org-get-at-bol 'line-prefix))))
+			  ?\·)
 			 '(face shadow))
 		     (org-add-props
 			 (format " %9s " (org-minutes-to-clocksum-string time))
