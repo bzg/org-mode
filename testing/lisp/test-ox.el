7cd7b90dcb7ef87f1eb63d93ab725407e92f9979
Kaushal Modi <kaushal.modi@gmail.com>
2017-06-09 15:28:49 -0400

    Update remote file syntax for Tramp
    * testing/lisp/test-ox.el (test-org-export/file-uri):
    * doc/org.texi (External links): The method part of remote file names
    is mandatory now in the emacs master (26+).  A valid remote file name
    starts with "/method:host:" or "/method:user@host:".  ssh is used as
    an example method here.
    
    This change in the examples is backward compatible with emacs 25, 24, ..
    
    Suggested by: Michael Albinus <michael.albinus@gmx.de>
    <http://lists.gnu.org/archive/html/emacs-orgmode/2017-06/msg00151.html>
    
    Relevant commit in emacs master causing this change:
    <http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=ed33337c3e0d0b1a8b140e23168421ea43d79324>

--- testing/lisp/test-ox.el
+++ testing/lisp/test-ox.el
@@ -3252,8 +3252,8 @@ Another text. (ref:text)
   (should (equal (concat (if (memq system-type '(windows-nt cygwin)) "file:///" "file://") (expand-file-name "/local.org"))
 		 (org-export-file-uri "/local.org")))
   ;; Remote files start with "file://"
-  (should (equal "file://myself@some.where:papers/last.pdf"
-		 (org-export-file-uri "/myself@some.where:papers/last.pdf")))
+  (should (equal "file://ssh:myself@some.where:papers/last.pdf"
+		 (org-export-file-uri "/ssh:myself@some.where:papers/last.pdf")))
   (should (equal "file://localhost/etc/fstab"
 		 (org-export-file-uri "//localhost/etc/fstab")))
   ;; Expand filename starting with "~".
