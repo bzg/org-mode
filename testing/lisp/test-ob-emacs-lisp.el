5e0db079884f0544049de24e43f928b724fa2990
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2017-07-23 22:41:27 +0200

    ob-emacs-lisp: Fix pp results
    * lisp/ob-emacs-lisp.el (org-babel-expand-body:emacs-lisp): Move "pp" handling...
    (org-babel-execute:emacs-lisp): ... here.
    
    * testing/lisp/test-ob-emacs-lisp.el (ob-emacs-lisp/commented-last-block-line):
      Small refactoring.
    
    Reported-by: Chunyang Xu <mail@xuchunyang.me>
    <http://lists.gnu.org/archive/html/emacs-orgmode/2017-07/msg00355.html>

--- testing/lisp/test-ob-emacs-lisp.el
+++ testing/lisp/test-ob-emacs-lisp.el
@@ -64,17 +64,17 @@
 	     (buffer-substring-no-properties (point-at-bol) (point-at-eol))))))
 
 (ert-deftest ob-emacs-lisp/commented-last-block-line ()
-  (org-test-with-temp-text-in-file "
+  (should
+   (string= ": 2"
+	    (org-test-with-temp-text-in-file "
 #+begin_src emacs-lisp :var a=2
 2;;
 #+end_src"
-    (org-babel-next-src-block)
-    (org-babel-execute-maybe)
-    (re-search-forward "results" nil t)
-    (forward-line)
-    (should (string=
-	     ": 2"
-	     (buffer-substring-no-properties (point-at-bol) (point-at-eol))))))
+	      (org-babel-next-src-block)
+	      (org-babel-execute-maybe)
+	      (re-search-forward "results" nil t)
+	      (buffer-substring-no-properties (line-beginning-position 2)
+					      (line-end-position 2))))))
 
 (provide 'test-ob-emacs-lisp)
 
