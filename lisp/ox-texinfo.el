bzg/org-mode

commit 1b415735ea236a5e9dfff06428378b6ba5f9e80b

ox-texinfo: Small menu refactoring
* lisp/ox-texinfo.el (org-texinfo--menu-entries): Use
  `org-export-collect-headlines'.

lisp/ox-texinfo.el
@@ -1214,13 +1214,10 @@ holding contextual information."
 	 (cached-entries (gethash scope cache 'no-cache)))
     (if (not (eq cached-entries 'no-cache)) cached-entries
       (puthash scope
-	       (org-element-map (org-element-contents scope) 'headline
-		 (lambda (h)
-		   (and (not (org-not-nil (org-element-property :COPYING h)))
-			(not (org-element-property :footnote-section-p h))
-			(not (org-export-low-level-p h info))
-			h))
-		 info nil 'headline)
+	       (cl-remove-if
+		(lambda (h)
+		  (org-not-nil (org-export-get-node-property :COPYING h t)))
+		(org-export-collect-headlines info nil scope))
 	       cache))))
 
 ;;;; Node Property

commit 8112629d378c6cc85fb4440580ee4550b6c8e48b

ox-texinfo: Fix code typo
* lisp/ox-texinfo.el (org-texinfo--menu-entries): Only collect direct
  children.

The regression was introduced in
1b415735ea236a5e9dfff06428378b6ba5f9e80b.

lisp/ox-texinfo.el
@@ -1216,7 +1216,7 @@ holding contextual information."
 	       (cl-remove-if
 		(lambda (h)
 		  (org-not-nil (org-export-get-node-property :COPYING h t)))
-		(org-export-collect-headlines info nil scope))
+		(org-export-collect-headlines info 1 scope))
 	       cache))))
 
 ;;;; Node Property