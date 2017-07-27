720b213a3556fa4c771bc32dd0852ddbd1adb6e0
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2017-07-27 13:52:12 +0200

    ox-texinfo: Default process includes "--no-split" option
    * lisp/ox-texinfo.el (org-texinfo-info-process): Add "--no-split"
      option.
    
    Reported-by: Jonas Bernoulli <jonas@bernoul.li>
    <http://lists.gnu.org/archive/html/emacs-orgmode/2017-07/msg00458.html>

--- lisp/ox-texinfo.el
+++ lisp/ox-texinfo.el
@@ -351,7 +351,7 @@ The function should return the string to be exported."
 
 ;;;; Compilation
 
-(defcustom org-texinfo-info-process '("makeinfo %f")
+(defcustom org-texinfo-info-process '("makeinfo --no-split %f")
   "Commands to process a Texinfo file to an INFO file.
 
 This is a list of strings, each of them will be given to the
@@ -361,6 +361,8 @@ base name (i.e. without directory and extension parts), %o by the
 base directory of the file and %O by the absolute file name of
 the output file."
   :group 'org-export-texinfo
+  :version "26.1"
+  :package-version '(Org . "9.1")
   :type '(repeat :tag "Shell command sequence"
 		 (string :tag "Shell command")))
 
