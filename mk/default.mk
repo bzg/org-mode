8c1c549376e94f787879c08c5dee1c510f901bbe
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2017-08-07 11:51:43 +0200

    Add Vala to automated tests
    * mk/default.mk (BTEST_OB_LANGUAGES): Add vala.

--- mk/default.mk
+++ mk/default.mk
@@ -43,7 +43,7 @@ BTEST_POST  =
               # -L <path-to>/ert      # needed for Emacs23, Emacs24 has ert built in
               # -L <path-to>/ess      # needed for running R tests
               # -L <path-to>/htmlize  # need at least version 1.34 for source code formatting
-BTEST_OB_LANGUAGES = awk C fortran maxima lilypond octave perl python
+BTEST_OB_LANGUAGES = awk C fortran maxima lilypond octave perl python vala
               # R                     # requires ESS to be installed and configured
               # ruby                  # requires inf-ruby to be installed and configured
 # extra packages to require for testing
