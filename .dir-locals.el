290bbc14adab05c7636f2afff2a4cd5529e7caea
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2017-12-17 10:56:01 +0100

    Update ".dir-locals.el"
    * .dir-locals.el (org-mode): Add Org mode specific options so manual
       is more homogeneous.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,15 @@
-((nil . ((indent-tabs-mode . t)
-	 (tab-width . 8)
-	 (fill-column . 70)
-	 (sentence-end-double-space . t))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (fill-column . 70)
+  (sentence-end-double-space . t))
+ (org-mode
+  (indent-tabs-mode)
+  (org-edit-src-content-indentation . 2)
+  (org-adapt-indentation)))
+
+
+
