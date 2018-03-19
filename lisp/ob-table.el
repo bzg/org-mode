02b1574ea970f33032d74b522e7b0a894409701f
Vladimir Panteleev <git@thecybershadow.net>
2018-03-19 00:23:14 +0000

    ob-table: Mention passing ranges as lists in org-sbe's documentation
    * ob-table.el (org-sbe): Add documentation note.

--- lisp/ob-table.el
+++ lisp/ob-table.el
@@ -89,6 +89,8 @@ NOTE: By default, string variable names are interpreted as
 references to source-code blocks, to force interpretation of a
 cell's value as a string, prefix the identifier a \"$\" (e.g.,
 \"$$2\" instead of \"$2\" or \"$@2$2\" instead of \"@2$2\").
+This will not work with a range; instead, pass it as a list,
+e.g. (org-sbe fun (r (list $1..$2))).
 
 NOTE: It is also possible to pass header arguments to the code
 block.  In this case a table cell should hold the string value of
