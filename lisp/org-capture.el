b4422add3745c26ec3b2e11b8da425844b2e9d3d
Feng Shu <tumashu@163.com>
2018-04-16 09:02:13 +0800

    org-capture: Do not save buffer when running `org-capture-finalize'
    * lisp/org-capture.el (org-capture-finalize): Do not run
      `save-buffer'.

--- lisp/org-capture.el
+++ lisp/org-capture.el
@@ -791,7 +791,6 @@ captured item after finalizing."
 	(org-encrypt-entry)))
 
     ;; Kill the indirect buffer
-    (save-buffer)
     (let ((return-wconf (org-capture-get :return-to-wconf 'local))
 	  (new-buffer (org-capture-get :new-buffer 'local))
 	  (kill-buffer (org-capture-get :kill-buffer 'local))
