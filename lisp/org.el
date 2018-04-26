b1d084c1ab877a58b342ae4a8b8d844b9b54e625
Bastien <bzg@gnu.org>
2018-04-26 21:52:39 +0200

    org.el: Fix bug when links contain special characters
    * lisp/org.el (org-open-link-from-string): Fix bug when links
    contain special characters.
    
    Thanks to Joon Ro for reporting this.

--- lisp/org.el
+++ lisp/org.el
@@ -10539,7 +10539,7 @@ Raise an error when there is nothing to follow.  "
     (with-temp-buffer
       (let ((org-inhibit-startup (not reference-buffer)))
 	(org-mode)
-	(insert s)
+	(insert (org-link-escape s))
 	(goto-char (point-min))
 	(when reference-buffer
 	  (setq org-link-abbrev-alist-local
