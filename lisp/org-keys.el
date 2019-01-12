354e2d4a2db2944e77056c35aadad820f926fd15
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2019-01-12 12:01:38 +0100

    org-keys: Fix last commit

--- lisp/org-keys.el
+++ lisp/org-keys.el
@@ -449,7 +449,7 @@ COMMANDS is a list of alternating OLDDEF NEWDEF command names."
 (define-key org-mode-map (kbd "<backtab>") #'org-shifttab)
 
 ;;;; RET key with modifiers
-(org-defkey org-mode-map (kbd "S-RET>") #'org-table-copy-down)
+(org-defkey org-mode-map (kbd "S-RET") #'org-table-copy-down)
 (org-defkey org-mode-map (kbd "M-S-RET") #'org-insert-todo-heading)
 (org-defkey org-mode-map (kbd "ESC S-RET") #'org-insert-todo-heading)
 (org-defkey org-mode-map (kbd "M-RET") #'org-meta-return)
