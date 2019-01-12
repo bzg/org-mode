f9596eb47219794ae66c29bf4a091c478ccf5830
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2019-01-12 11:49:50 +0100

    Fix following timestamp in planning lines
    * lisp/org.el (org-open-at-point): Open calendar when called on
      a timestamp in a planning line.
    
    Reported-by: Bob Hepple <bob.hepple@gmail.com>
    <http://lists.gnu.org/r/emacs-orgmode/2019-01/msg00158.html>

--- lisp/org.el
+++ lisp/org.el
@@ -10208,7 +10208,7 @@ a link."
 	     (org-element-context)
 	     '(clock comment comment-block footnote-definition
 		     footnote-reference headline inline-src-block inlinetask
-		     keyword link node-property src-block timestamp)
+		     keyword link node-property planning src-block timestamp)
 	     t))
 	   (type (org-element-type context))
 	   (value (org-element-property :value context)))
@@ -10251,6 +10251,10 @@ a link."
 		       (= (org-element-property :post-affiliated context)
 			  (line-beginning-position)))))))
 	(org-footnote-action))
+       ;; On a planning line.  Check if we are really on a timestamp.
+       ((and (eq type 'planning)
+	     (org-in-regexp org-ts-regexp-both nil t))
+	(org-follow-timestamp-link))
        ;; On a clock line, make sure point is on the timestamp
        ;; before opening it.
        ((and (eq type 'clock)
