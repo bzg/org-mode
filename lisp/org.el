3abedb8b64b4b139b68082215db11f99a7f78030
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2019-02-13 15:43:29 +0100

    Bump version keyword

--- lisp/org.el
+++ lisp/org.el
@@ -7,7 +7,7 @@
 ;; Maintainer: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: https://orgmode.org
-;; Version: 9.2.1
+;; Version: 9.2.2
 ;;
 ;; This file is part of GNU Emacs.
 ;;
