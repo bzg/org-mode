24555a0c009a6fd24427109c881049ff73c096c3
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2019-05-30 14:19:09 +0200

    Revert "Fix strike-through fontification on heading"
    This reverts commit 42abf5c6954dee8410e33d0c5140d3b36c9d1c15.

--- lisp/org.el
+++ lisp/org.el
@@ -5821,7 +5821,7 @@ This should be called after the variable `org-link-parameters' has changed."
 		   (not (and (save-match-data (org-match-line "[ \t]*|"))
 			     (string-match-p "|" (match-string 4))))))
 	    (pcase-let ((`(,_ ,face ,_) (assoc marker org-emphasis-alist)))
-	      (font-lock-append-text-property
+	      (font-lock-prepend-text-property
 	       (match-beginning 2) (match-end 2) 'face face)
 	      (when verbatim?
 		(org-remove-flyspell-overlays-in
