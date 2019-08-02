ff6508ad4b68d936e79640699a9f3d994ae8fe58
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2019-08-02 09:55:53 +0200

    Bump Version keyword
    * lisp/org.el: Bump Version keyword.
    
    This is necessary for GNU ELPA. Ideally, such a change should be
    tagged with the official release.

--- lisp/org.el
+++ lisp/org.el
@@ -6,7 +6,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: https://orgmode.org
-;; Version: 9.2.3
+;; Version: 9.2.5
 ;;
 ;; This file is part of GNU Emacs.
 ;;
