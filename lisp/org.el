b23b6a4abd1ab5ae94e4690ce03b28d34542bfff
Bastien <bzg@gnu.org>
2019-12-24 11:53:38 +0100

    org.el: Bump version keyword to "9.4-dev"

--- lisp/org.el
+++ lisp/org.el
@@ -6,7 +6,7 @@
 ;; Author: Carsten Dominik <carsten at orgmode dot org>
 ;; Keywords: outlines, hypermedia, calendar, wp
 ;; Homepage: https://orgmode.org
-;; Version: 9.3
+;; Version: 9.4-dev
 ;;
 ;; This file is part of GNU Emacs.
 ;;
