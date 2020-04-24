7cdc023f886707df06d7a5c5f8fbd69900e346c9
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2020-04-24 22:22:43 +0200

    bibtex: Fix `org-bibtex-goto-citation'
    * contrib/lisp/ox-bibtex.el (org-bibtex-goto-citation): Fix
    `org-bibtex-goto-citation'.
    
    Reported-by: Eric S Fraga <e.fraga@ucl.ac.uk>
    <http://lists.gnu.org/r/emacs-orgmode/2020-04/msg00383.html>

--- contrib/lisp/ox-bibtex.el
+++ contrib/lisp/ox-bibtex.el
@@ -162,10 +162,8 @@ to `org-bibtex-citation-p' predicate."
   (let ((citation (or citation (completing-read "Citation: " (obe-citations)))))
     (find-file (or org-bibtex-file
 		   (error "`org-bibtex-file' has not been configured")))
-    (goto-char (point-min))
-    (when (re-search-forward (format "  :CUSTOM_ID: %s" citation) nil t)
-      (outline-previous-visible-heading 1)
-      t)))
+    (let ((position (org-find-property "CUSTOM_ID" citation)))
+      (and position (progn (goto-char position) t)))))
 
 (let ((jump-fn (car (cl-remove-if-not #'fboundp '(ebib org-bibtex-goto-citation)))))
   (org-add-link-type "cite" jump-fn))
