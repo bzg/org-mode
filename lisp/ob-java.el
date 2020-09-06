989f50a53be7254de38263a1b52843f19690f4a7
Bastien <bzg@gnu.org>
2020-09-06 11:48:20 +0200

    ob-java.el: Inhibit Lisp evaluation when reading the result
    * lisp/ob-java.el (org-babel-execute:java): Inhibit Lisp
    evaluation when reading the result.
    
    Reported-by: Jarmo Hurri <jarmo.hurri@iki.fi>

--- lisp/ob-java.el
+++ lisp/ob-java.el
@@ -69,7 +69,7 @@ parameters may be used, like javac -verbose"
                                            " " cmdline " " classname) "")))
       (org-babel-reassemble-table
        (org-babel-result-cond (cdr (assq :result-params params))
-	 (org-babel-read results)
+	 (org-babel-read results t)
          (let ((tmp-file (org-babel-temp-file "c-")))
            (with-temp-file tmp-file (insert results))
            (org-babel-import-elisp-from-file tmp-file)))
