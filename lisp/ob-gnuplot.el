95ed6badc0683376a4cbbc893d0bd07807cb162c
fpi <git@pie.tf>
2020-09-29 18:43:07 +0200

    ob-gnuplot: Download remote input files
    * lisp/ob-gnuplot.el (org-babel-gnuplot-process-vars): When variables
    point to remote files, download them and use a local copy instead.
    
    TINYCHANGE

--- lisp/ob-gnuplot.el
+++ lisp/ob-gnuplot.el
@@ -92,7 +92,21 @@ code."
 		      (tablep (or (listp first) (symbolp first))))
 		 (if tablep val (mapcar 'list val)))
 	       (org-babel-temp-file "gnuplot-") params)
-	  val))))
+	    (if (and (file-remote-p val)  ;; check if val is a remote file
+		     (file-exists-p val)) ;; call to file-exists-p is slow, maybe remove it
+		(let* ((local-name (concat ;; create a unique filename to avoid multiple downloads
+				org-babel-temporary-directory
+				"/gnuplot/"
+				(file-remote-p val 'host)
+				(file-local-name val))))
+		  (if (and (file-exists-p local-name) ;; only download file if remote is newer
+			   (file-newer-than-file-p local-name val))
+		      local-name
+		    (make-directory (file-name-directory local-name) t)
+		    (copy-file val local-name t)
+		  ))
+	      val
+		)))))
      (org-babel--get-vars params))))
 
 (defun org-babel-expand-body:gnuplot (body params)
