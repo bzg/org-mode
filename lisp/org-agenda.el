dfc0d522f16d989897a1b54ea52c5a84dc1163ab
Bastien <bzg@gnu.org>
2021-05-04 00:20:07 +0200

    Revert "lisp/org-agenda.el: Prevent wrong scrolling"
    This reverts commit eca93742b6534db4715c3f3588aebea7823793dd.

--- lisp/org-agenda.el
+++ lisp/org-agenda.el
@@ -8344,8 +8344,7 @@ With prefix ARG, go forward that many times the current span."
 	  (org-agenda-overriding-arguments
 	   (list (car args) sd span)))
       (org-agenda-redo)
-      (org-agenda-find-same-or-today-or-agenda cnt))
-    (set-window-start nil 1)))
+      (org-agenda-find-same-or-today-or-agenda cnt))))
 
 (defun org-agenda-earlier (arg)
   "Go backward in time by the current span.
