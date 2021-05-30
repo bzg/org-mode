a0a87d55cbfea043a55e484e262c48b8fcdfdb47
Bastien <bzg@gnu.org>
2021-05-30 18:56:05 +0200

    Fix 108e5c7bd

--- lisp/ob-eshell.el
+++ lisp/ob-eshell.el
@@ -6,7 +6,6 @@
 ;; Maintainer: stardiviner <numbchild@gmail.com>
 ;; Homepage: https://github.com/stardiviner/ob-eshell
 ;; Keywords: literate programming, reproducible research
-;; Homepage: https://orgmode.org
 
 ;; This file is part of GNU Emacs.
 
