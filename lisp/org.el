9dc08c909343795aa9aefb2c47688b6f1a9c0203
TEC <tec@tecosaur.com>
2021-09-29 21:29:27 +0800

    org: Don't change image size based on font size
    * lisp/org.el (org--create-inline-image): Set :scale parameter to 1 to
    prevent font-size based rescaling.
    
    When `create-image' is called without the :scale parameter, the image
    size is expanded based on the default font size (if it is larger than
    10px).  When displaying images with a specified width in Org buffers,
    either in pixels or proportional to the text width, this width should
    not be modified according the to font size.  Giving a :scale parameter
    of 1 prevents this font-size based rescaling.

--- lisp/org.el
+++ lisp/org.el
@@ -16706,7 +16706,7 @@ according to the value of `org-display-remote-inline-images'."
 			 width
 			 'imagemagick)
 		    remote?
-		    :width width))))
+		    :width width :scale 1))))
 
 (defun org-display-inline-images (&optional include-linked refresh beg end)
   "Display inline images.
