5f6e199143d8b66077c41202ff1b0d5f7064a50b
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2021-10-06 14:34:14 +0200

    footnote: Allow footnotes at footnote definition start
    * lisp/org-footnote.el (org-footnote--allow-reference-p): Allow
    footnotes at footnote definition start, right after label.
    * testing/lisp/test-org-footnote.el (test-org-footnote/new): Add
    tests.

--- lisp/org-footnote.el
+++ lisp/org-footnote.el
@@ -282,6 +282,9 @@ otherwise."
 			    (skip-chars-backward " \r\t\n")
 			    (if (eq (org-element-class context) 'object) (point)
 			      (1+ (line-beginning-position 2))))))
+       ;; At the beginning of a footnote definition, right after the
+       ;; label, is OK.
+       ((eq type 'footnote-definition) (looking-at (rx (or space buffer-end))))
        ;; Other elements are invalid.
        ((eq (org-element-class context) 'element) nil)
        ;; Just before object is fine.
