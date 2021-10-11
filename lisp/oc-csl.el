bzg/org-mode

commit c4a357f1926ff43545c9891425c94b2771ab4e84

oc-csl: Allow nbsp after locator label
* lisp/oc-csl.el (org-cite-csl--label-regexp): Allow nbsp between
locator label and value.

Reported-by: M. ‘quintus’ Gülker <post+orgmodeml@guelker.eu>
<http://lists.gnu.org/r/emacs-orgmode/2021-10/msg00423.html>

lisp/oc-csl.el
@@ -270,11 +270,12 @@ If nil then the Chicago author-date style is used as a fallback.")
 (defconst org-cite-csl--label-regexp
   ;; Prior to Emacs-27.1 argument of `regexp' form must be a string literal.
   ;; It is the reason why `rx' is avoided here.
-  (rx-to-string `(seq word-start
-                      (regexp ,(regexp-opt (mapcar #'car org-cite-csl--label-alist) t))
-                      (0+ digit)
-                      (or word-start line-end (any ?\s ?\t)))
-                t)
+  (rx-to-string
+   `(seq word-start
+         (regexp ,(regexp-opt (mapcar #'car org-cite-csl--label-alist) t))
+         (0+ digit)
+         (or word-start line-end space ? ))
+   t)
   "Regexp matching a label in a citation reference suffix.
 Label is in match group 1.")
 

commit 3bb0cb9398404291b0ced5d14a2577b4a158432e

oc: Fix § locator name
* lisp/oc-csl.el (org-cite-csl--label-regexp): § is not a word start,
so adjust regexp accordingly.  Also fix mistake introduced in
c4a357f1926ff43545c9891425c94b2771ab4e84.
(org-cite-csl--parse-reference): Ignore any space after locator name.

lisp/oc-csl.el
@@ -271,10 +271,10 @@ If nil then the Chicago author-date style is used as a fallback.")
   ;; Prior to Emacs-27.1 argument of `regexp' form must be a string literal.
   ;; It is the reason why `rx' is avoided here.
   (rx-to-string
-   `(seq word-start
+   `(seq (or line-start space)
          (regexp ,(regexp-opt (mapcar #'car org-cite-csl--label-alist) t))
          (0+ digit)
-         (or word-start line-end space ? ))
+         (or word-end line-end space " "))
    t)
   "Regexp matching a label in a citation reference suffix.
 Label is in match group 1.")
@@ -426,7 +426,9 @@ The result is a association list.  Keys are: `id', `prefix',`suffix',
        ((re-search-forward org-cite-csl--label-regexp nil t)
         (setq location-start (match-beginning 0))
         (setq label (cdr (assoc (match-string 1) org-cite-csl--label-alist)))
-        (setq locator-start (match-end 1)))
+        (goto-char (match-end 1))
+        (skip-chars-forward "[:space:] ")
+        (setq locator-start (point)))
        ((re-search-forward (rx digit) nil t)
         (setq location-start (match-beginning 0))
         (setq label "page")