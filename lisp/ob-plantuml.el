f9dcc3d665f06ef76ef14386e55854a06d2d0c39
Sun Lin <sunlin7@yahoo.com>
2021-10-16 03:25:40 +0000

    ob-plantuml: fix DISPLAY error with html export
    * lisp/ob-plantuml.el (org-babel-execute:plantuml): Use headless mode
    for Org calling plantuml for exporting to html to avoid issues with
    operations that normally try to access the system graphics stack.

--- lisp/ob-plantuml.el
+++ lisp/ob-plantuml.el
@@ -122,6 +122,7 @@ This function is called by `org-babel-execute-src-block'."
 				((not (file-exists-p org-plantuml-jar-path))
 				 (error "Could not find plantuml.jar at %s" org-plantuml-jar-path))
 				(t (list java
+					 "-Djava.awt.headless=true"
 					 "-jar"
 					 (shell-quote-argument (expand-file-name org-plantuml-jar-path))))))
 	 (full-body (org-babel-plantuml-make-body body params))
