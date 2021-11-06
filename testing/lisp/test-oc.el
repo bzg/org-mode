ada13f916c2aa2f802fda8e325872a42a4e0a599
Kyle Meyer <kyle@kyleam.com>
2021-11-06 17:10:18 -0400

    test-oc: Fix incorrect expectation
    * testing/lisp/test-oc.el (test-org-cite/main-affixes): Fix spacing in
    an expected string.
    
    Link: https://list.orgmode.org/878ry1ovz8.fsf@nicolasgoaziou.fr

--- testing/lisp/test-oc.el
+++ testing/lisp/test-oc.el
@@ -286,7 +286,7 @@
           (org-test-with-temp-text "[cite:global pre;pre1 @key1;pre2 @key2]"
             (org-cite-main-affixes (org-element-context)))))
   (should
-   (equal '(nil . ("global post"))
+   (equal '(nil . (" global post"))
           (org-test-with-temp-text "[cite:@key1 post1;@key2 post2; global post]"
             (org-cite-main-affixes (org-element-context))))))
 
