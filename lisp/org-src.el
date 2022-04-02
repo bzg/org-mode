cbe3f2d697a973a41a412b826ab9435d5523f3d0
TEC <tec@tecosaur.com>
2022-04-03 00:36:21 +0800

    org-src: add toml and desktop lang aliases
    * lisp/org-src.el (org-src-lang-modes): It is reasonable to assume that
    src blocks with the "toml" and "desktop" lang should use
    `conf-toml-mode' and `conf-desktop-mode', respectively.

--- lisp/org-src.el
+++ lisp/org-src.el
@@ -198,12 +198,14 @@ but which mess up the display of a snippet in Org exported files.")
     ("calc" . fundamental)
     ("cpp" . c++)
     ("ditaa" . artist)
+    ("desktop" . conf-desktop)
     ("dot" . fundamental)
     ("elisp" . emacs-lisp)
     ("ocaml" . tuareg)
     ("screen" . shell-script)
     ("shell" . sh)
-    ("sqlite" . sql))
+    ("sqlite" . sql)
+    ("toml" . conf-toml))
   "Alist mapping languages to their major mode.
 
 The key is the language name.  The value is the mode name, as
