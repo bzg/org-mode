0599ddfd419c520beff32cb57e0cc511d791d2d5
Ihor Radchenko <yantar92@gmail.com>
2022-08-01 06:47:37 +0800

    Revert "lisp/ox-latex: Omit empty date"
    This reverts commit a753d0dd26018a951fe21548ae6b513594b570a8.
    
    Omitting \date in the LaTeX source makes LaTeX print today's date by
    default, which is not consistent with :with-date export option.
    
    See https://list.orgmode.org/m2v8rdguil.fsf@gmail.com/T/#t

--- lisp/ox-latex.el
+++ lisp/ox-latex.el
@@ -1980,8 +1980,7 @@ holding export options."
 	     ((or author email) (format "\\author{%s}\n" (or author email)))))
      ;; Date.
      (let ((date (and (plist-get info :with-date) (org-export-get-date info))))
-       (when date
-             (format "\\date{%s}\n" (org-export-data date info))))
+       (format "\\date{%s}\n" (org-export-data date info)))
      ;; Title and subtitle.
      (let* ((subtitle (plist-get info :subtitle))
 	    (formatted-subtitle
