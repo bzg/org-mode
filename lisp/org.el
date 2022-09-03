a1c923afda22089cbf14c7b7a175cee10bfd297b
Ihor Radchenko <yantar92@gmail.com>
2022-09-03 16:12:22 +0800

    org-scan-tags: Allow arbitrary character classes in todo keywords
    * lisp/org.el (org-scan-tags): Do not assume that todo keywords are
    words.
    
    Fixes https://orgmode.org/list/C5370FC1-ABBB-4FA1-8364-5B1DF0FB4755@irq0.org

--- lisp/org.el
+++ lisp/org.el
@@ -10799,7 +10799,7 @@ headlines matching this string."
 			 ;; Get the correct level to match
 			 (concat "\\*\\{" (number-to-string start-level) "\\} ")
 		       org-outline-regexp)
-		     " *\\(" (regexp-opt org-todo-keywords-1 'words) "\\)?"
+		     " *\\(" (regexp-opt org-todo-keywords-1 'words) " \\)?"
 		     " *\\(.*?\\)\\([ \t]:\\(?:" org-tag-re ":\\)+\\)?[ \t]*$"))
 	 (props (list 'face 'default
 		      'done-face 'org-agenda-done
