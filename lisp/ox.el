4fbd302df6f588185672c510ca82def1c0685efb
TEC <git@tecosaur.net>
2023-02-05 16:20:21 +0800

    ox: Split up the org-export-as function
    * lisp/ox.el (org-export--annotate-info, org-export-as): Separate out
    the info-collection phase of `org-export-as' into a new function,
    `org-export--annotate-info'.

--- lisp/ox.el
+++ lisp/ox.el
@@ -3048,102 +3048,19 @@ Return code as a string."
 	       (info (org-combine-plists
 		      (org-export--get-export-attributes
 		       backend subtreep visible-only body-only)
-		      (org-export--get-buffer-attributes)))
-	       (parsed-keywords
-	        (delq nil
-		      (mapcar (lambda (o) (and (eq (nth 4 o) 'parse) (nth 1 o)))
-			      (append (org-export-get-all-options backend)
-				      org-export-options-alist))))
-	       tree modified-tick)
+		      (org-export--get-buffer-attributes))))
 	  ;; Update communication channel and get parse tree.  Buffer
 	  ;; isn't parsed directly.  Instead, all buffer modifications
 	  ;; and consequent parsing are undertaken in a temporary copy.
 	  (org-export-with-buffer-copy
            (font-lock-mode -1)
-	   ;; Run first hook with current back-end's name as argument.
-	   (run-hook-with-args 'org-export-before-processing-hook
-			       (org-export-backend-name backend))
-	   (org-export-expand-include-keyword)
-	   (org-export--delete-comment-trees)
-	   (org-macro-initialize-templates org-export-global-macros)
-	   (org-macro-replace-all org-macro-templates parsed-keywords)
-	   ;; Refresh buffer properties and radio targets after previous
-	   ;; potentially invasive changes.
-	   (org-set-regexps-and-options)
-	   (org-update-radio-target-regexp)
-           (setq modified-tick (buffer-chars-modified-tick))
-	   ;;  Possibly execute Babel code.  Re-run a macro expansion
-	   ;;  specifically for {{{results}}} since inline source blocks
-	   ;;  may have generated some more.  Refresh buffer properties
-	   ;;  and radio targets another time.
-	   (when org-export-use-babel
-	     (org-babel-exp-process-buffer)
-	     (org-macro-replace-all '(("results" . "$1")) parsed-keywords)
-             (unless (eq modified-tick (buffer-chars-modified-tick))
-	       (org-set-regexps-and-options)
-	       (org-update-radio-target-regexp))
-             (setq modified-tick (buffer-chars-modified-tick)))
-	   ;; Run last hook with current back-end's name as argument.
-	   ;; Update buffer properties and radio targets one last time
-	   ;; before parsing.
-	   (goto-char (point-min))
-	   (save-excursion
-	     (run-hook-with-args 'org-export-before-parsing-hook
-			         (org-export-backend-name backend)))
-           (unless (eq modified-tick (buffer-chars-modified-tick))
-	     (org-set-regexps-and-options)
-	     (org-update-radio-target-regexp))
-           (setq modified-tick (buffer-chars-modified-tick))
-	   ;; Update communication channel with environment.
-	   (setq info
-	         (org-combine-plists
-		  info (org-export-get-environment backend subtreep ext-plist)))
-           ;; Pre-process citations environment, i.e. install
-	   ;; bibliography list, and citation processor in INFO.
-	   (org-cite-store-bibliography info)
-           (org-cite-store-export-processor info)
-	   ;; De-activate uninterpreted data from parsed keywords.
-	   (dolist (entry (append (org-export-get-all-options backend)
-				  org-export-options-alist))
-	     (pcase entry
-	       (`(,p ,_ ,_ ,_ parse)
-	        (let ((value (plist-get info p)))
-		  (plist-put info
-			     p
-			     (org-export--remove-uninterpreted-data value info))))
-	       (_ nil)))
-	   ;; Install user's and developer's filters.
-	   (setq info (org-export-install-filters info))
-	   ;; Call options filters and update export options.  We do not
-	   ;; use `org-export-filter-apply-functions' here since the
-	   ;; arity of such filters is different.
-	   (let ((backend-name (org-export-backend-name backend)))
-	     (dolist (filter (plist-get info :filter-options))
-	       (let ((result (funcall filter info backend-name)))
-	         (when result (setq info result)))))
-	   ;; Parse buffer.
-	   (setq tree (org-element-parse-buffer nil visible-only))
-	   ;; Prune tree from non-exported elements and transform
-	   ;; uninterpreted elements or objects in both parse tree and
-	   ;; communication channel.
-	   (org-export--prune-tree tree info)
-	   (org-export--remove-uninterpreted-data tree info)
-	   ;; Call parse tree filters.
-	   (setq tree
-	         (org-export-filter-apply-functions
-		  (plist-get info :filter-parse-tree) tree info))
-	   ;; Now tree is complete, compute its properties and add them
-	   ;; to communication channel.
-	   (setq info (org-export--collect-tree-properties tree info))
-           ;; Process citations and bibliography.  Replace each citation
-	   ;; and "print_bibliography" keyword in the parse tree with
-	   ;; the output of the selected citation export processor.
-           (org-cite-process-citations info)
-           (org-cite-process-bibliography info)
+           (setq info (org-export--annotate-info
+                       backend info subtreep visible-only ext-plist))
 	   ;; Eventually transcode TREE.  Wrap the resulting string into
 	   ;; a template.
 	   (let* ((body (org-element-normalize-string
-		         (or (org-export-data tree info) "")))
+		         (or (org-export-data (plist-get info :parse-tree) info)
+                             "")))
 		  (inner-template (cdr (assq 'inner-template
 					     (plist-get info :translate-alist))))
 		  (full-body (org-export-filter-apply-functions
@@ -3165,6 +3082,109 @@ Return code as a string."
 	      (org-export-filter-apply-functions
 	       (plist-get info :filter-final-output)
 	       output info)))))))))
+
+(defun org-export--annotate-info (backend info &optional subtreep visible-only ext-plist)
+  "Annotate the INFO plist according to the BACKEND.
+
+This is run in the context of the current buffer.
+
+When optional argument SUBTREEP is non-nil, transcode the
+sub-tree at point, extracting information from the headline
+properties first.
+
+When optional argument VISIBLE-ONLY is non-nil, don't process the
+contents of hidden elements.
+
+Optional argument EXT-PLIST, when provided, is a property list
+with external parameters overriding Org default settings, but
+still inferior to file-local settings."
+  (let ((parsed-keywords
+         (delq nil
+               (mapcar (lambda (o) (and (eq (nth 4 o) 'parse) (nth 1 o)))
+                       (append (org-export-get-all-options backend)
+                               org-export-options-alist))))
+        tree modified-tick)
+    ;; Run first hook with current back-end's name as argument.
+    (run-hook-with-args 'org-export-before-processing-hook
+                        (org-export-backend-name backend))
+    (org-export-expand-include-keyword)
+    (org-export--delete-comment-trees)
+    (org-macro-initialize-templates org-export-global-macros)
+    (org-macro-replace-all org-macro-templates parsed-keywords)
+    ;; Refresh buffer properties and radio targets after previous
+    ;; potentially invasive changes.
+    (org-set-regexps-and-options)
+    (org-update-radio-target-regexp)
+    (setq modified-tick (buffer-chars-modified-tick))
+    ;;  Possibly execute Babel code.  Re-run a macro expansion
+    ;;  specifically for {{{results}}} since inline source blocks
+    ;;  may have generated some more.  Refresh buffer properties
+    ;;  and radio targets another time.
+    (when org-export-use-babel
+      (org-babel-exp-process-buffer)
+      (org-macro-replace-all '(("results" . "$1")) parsed-keywords)
+      (unless (eq modified-tick (buffer-chars-modified-tick))
+        (org-set-regexps-and-options)
+        (org-update-radio-target-regexp))
+      (setq modified-tick (buffer-chars-modified-tick)))
+    ;; Run last hook with current back-end's name as argument.
+    ;; Update buffer properties and radio targets one last time
+    ;; before parsing.
+    (goto-char (point-min))
+    (save-excursion
+      (run-hook-with-args 'org-export-before-parsing-hook
+                          (org-export-backend-name backend)))
+    (unless (eq modified-tick (buffer-chars-modified-tick))
+      (org-set-regexps-and-options)
+      (org-update-radio-target-regexp))
+    (setq modified-tick (buffer-chars-modified-tick))
+    ;; Update communication channel with environment.
+    (setq info
+          (org-combine-plists
+           info (org-export-get-environment backend subtreep ext-plist)))
+    ;; Pre-process citations environment, i.e. install
+    ;; bibliography list, and citation processor in INFO.
+    (org-cite-store-bibliography info)
+    (org-cite-store-export-processor info)
+    ;; De-activate uninterpreted data from parsed keywords.
+    (dolist (entry (append (org-export-get-all-options backend)
+                           org-export-options-alist))
+      (pcase entry
+        (`(,p ,_ ,_ ,_ parse)
+         (let ((value (plist-get info p)))
+           (plist-put info
+                      p
+                      (org-export--remove-uninterpreted-data value info))))
+        (_ nil)))
+    ;; Install user's and developer's filters.
+    (setq info (org-export-install-filters info))
+    ;; Call options filters and update export options.  We do not
+    ;; use `org-export-filter-apply-functions' here since the
+    ;; arity of such filters is different.
+    (let ((backend-name (org-export-backend-name backend)))
+      (dolist (filter (plist-get info :filter-options))
+        (let ((result (funcall filter info backend-name)))
+          (when result (setq info result)))))
+    ;; Parse buffer.
+    (setq tree (org-element-parse-buffer nil visible-only))
+    ;; Prune tree from non-exported elements and transform
+    ;; uninterpreted elements or objects in both parse tree and
+    ;; communication channel.
+    (org-export--prune-tree tree info)
+    (org-export--remove-uninterpreted-data tree info)
+    ;; Call parse tree filters.
+    (setq tree
+          (org-export-filter-apply-functions
+           (plist-get info :filter-parse-tree) tree info))
+    ;; Now tree is complete, compute its properties and add them
+    ;; to communication channel.
+    (setq info (org-export--collect-tree-properties tree info))
+    ;; Process citations and bibliography.  Replace each citation
+    ;; and "print_bibliography" keyword in the parse tree with
+    ;; the output of the selected citation export processor.
+    (org-cite-process-citations info)
+    (org-cite-process-bibliography info)
+    info))
 
 ;;;###autoload
 (defun org-export-string-as (string backend &optional body-only ext-plist)
