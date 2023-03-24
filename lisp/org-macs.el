44e1cbb09484c8f8c49ef49376ef7988b04decc2
Ihor Radchenko <yantar92@posteo.net>
2023-03-24 14:59:36 +0100

    org-compile-file: Fix when `org-batch-test' is not defined
    * lisp/org-macs.el (org-compile-file): Avoid error when
    `org-batch-test' is not defined.

--- lisp/org-macs.el
+++ lisp/org-macs.el
@@ -382,7 +382,7 @@ it for output."
     (unless (org-file-newer-than-p output time)
       (ignore (defvar org-batch-test))
       ;; Display logs when running tests.
-      (when org-batch-test
+      (when (bound-and-true-p org-batch-test)
         (message "org-compile-file log ::\n-----\n%s\n-----\n"
                  (with-current-buffer log-buf (buffer-string))))
       (error (format "File %S wasn't produced%s" output err-msg)))
