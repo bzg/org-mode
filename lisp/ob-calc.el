8aa6e8125d4fd16fc4b45667690b2383cf305c52
Ihor Radchenko <yantar92@posteo.net>
2023-08-01 13:31:22 +0300

    lisp/ob-cal.el: Fix docstrings
    * lisp/ob-calc.el (org-babel-execute:calc): Mention all the args.
    (org-babel-calc-maybe-resolve-var): Add docstring.

--- lisp/ob-calc.el
+++ lisp/ob-calc.el
@@ -50,7 +50,7 @@
 (defvar org--var-syms) ; Dynamically scoped from org-babel-execute:calc
 
 (defun org-babel-execute:calc (body params)
-  "Execute a block of calc code with Babel."
+  "Execute BODY of calc code with Babel using PARAMS."
   (unless (get-buffer "*Calculator*")
     (save-window-excursion (calc) (calc-quit)))
   (let* ((vars (org-babel--get-vars params))
@@ -99,6 +99,8 @@
         (calc-pop 1)))))
 
 (defun org-babel-calc-maybe-resolve-var (el)
+"Resolve user variables in EL.
+EL is taken from the output of `math-read-exprs'."
   (if (consp el)
       (if (and (eq 'var (car el)) (member (cadr el) org--var-syms))
 	  (progn
