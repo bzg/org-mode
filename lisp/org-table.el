bdc60fb936b2f28f1546b0e89243cefa53da7847
Ihor Radchenko <yantar92@posteo.net>
2024-01-11 14:18:35 +0100

    org-table-make-reference: Disable compiler warning
    * lisp/org-table.el (org-table-make-reference): Comment out the code
    that always returns nil, replacing it with explicit nil.  This line is
    certainly a fault in logic, but we do not know of any associated bugs.
    So, just leave a FIXME to potential future refactoring and avoid the
    compiler complaining about this code every time.

--- lisp/org-table.el
+++ lisp/org-table.el
@@ -2864,13 +2864,14 @@ list, `literal' is for the format specifier L."
       (if lispp
 	  (if (eq lispp 'literal)
 	      elements
-	    (if (and (eq elements "") (not keep-empty))
+	    (if nil
+                ;; (and (eq elements "") (not keep-empty))
                 ;; FIXME: This branch of `if' is never used because
                 ;; strings are never `eq' here.  But changing to
                 ;; `equal' breaks tests.
                 ;; See
                 ;; https://list.orgmode.org/orgmode/20230827214320.46754-1-salutis@me.com/
-		""
+	        ""
 	      (prin1-to-string
 	       (if numbers (string-to-number elements) elements))))
 	(if (string-match "\\S-" elements)
