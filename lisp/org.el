a8443f2c791388d9c1c57bac0eea2350cd027b1e
Martin Edström <meedstrom91@gmail.com>
2024-02-18 20:29:48 +0100

    org-latex-to-html-convert-command: Enhance the docstring
    * org.el (org-latex-to-html-convert-command): Add a note in the
    docstring about proper shell-quoting.
    
    It can trip you up because wrongly quoted input still works with some
    math snippets, so the command may work during testing but not later
    when you have different math snippets in play.
    
    TINYCHANGE

--- lisp/org.el
+++ lisp/org.el
@@ -3267,7 +3267,7 @@ When using LaTeXML set this option to
 	  (string :tag "\nShell command")))
 
 (defcustom org-latex-to-html-convert-command nil
-  "Command to convert LaTeX fragments to HTML.
+  "Shell command to convert LaTeX fragments to HTML.
 This command is very open-ended: the output of the command will
 directly replace the LaTeX fragment in the resulting HTML.
 Replace format-specifiers in the command as noted below and use
@@ -3275,7 +3275,12 @@ Replace format-specifiers in the command as noted below and use
 %i:     The LaTeX fragment to be converted.
 
 For example, this could be used with LaTeXML as
-\"latexmlc \\='literal:%i\\=' --profile=math --preload=siunitx.sty 2>/dev/null\"."
+\"latexmlc \\='literal:%i\\=' --profile=math --preload=siunitx.sty 2>/dev/null\".
+
+The LaTeX fragment is replaced as is, without escaping special shell
+syntax.  It may be necessary to use single-quotes around \\='%i\\=', not
+double-quotes.  Else a math fragment such as \"$y = 200$\" may be
+expanded to \" = 200\"."
   :group 'org-latex
   :package-version '(Org . "9.4")
   :type '(choice
