3e11b2eb88fcc728c11423b488e5ea5bbf1e817d
Ihor Radchenko <yantar92@posteo.net>
2024-04-29 19:46:31 +0300

    org-make-tags-matcher: Byte-compile the returned function
    * lisp/org.el (org-make-tags-matcher): Byte-compiling the constructed
    function yields noticeable improvements in at least clocksum
    generation.
    
    Link: https://orgmode.org/list/CH3PR84MB3424390023D1A552D57395FAC51B2@CH3PR84MB3424.NAMPRD84.PROD.OUTLOOK.COM

--- lisp/org.el
+++ lisp/org.el
@@ -11737,7 +11737,7 @@ See also `org-scan-tags'."
 		     (or tagsmatcher todomatcher t))))
       (when org--matcher-tags-todo-only
 	(setq matcher `(and (member todo org-not-done-keywords) ,matcher)))
-      (cons match0 `(lambda (todo tags-list level) ,matcher)))))
+      (cons match0 (byte-compile `(lambda (todo tags-list level) ,matcher))))))
 
 (defun org--tags-expand-group (group tag-groups expanded)
   "Recursively expand all tags in GROUP, according to TAG-GROUPS.
