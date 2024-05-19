3fa18371ee02e43ba97d25ad551504ae2bca04ef
Stefan Monnier <monnier@iro.umontreal.ca>
2024-05-19 17:15:21 -0400

    mk/default.mk (BTEST): Remove `-l cl'
    The CL library is deprecated and it's not needed here any more.
    I suspect other `-l' could be removed here since `org-test' loads
    those libraries anyway.

--- mk/default.mk
+++ mk/default.mk
@@ -78,20 +78,20 @@ BTEST_LOAD  = \
 	--eval '(add-to-list `load-path (concat default-directory "testing"))'
 BTEST_INIT  = $(BTEST_PRE) $(BTEST_LOAD) $(BTEST_POST)
 
-BTEST = $(BATCH) $(BTEST_INIT) \
-	  -l org-batch-test-init \
-	  --eval '(setq \
-		org-batch-test t \
-		org-babel-load-languages \
-		  (quote ($(foreach ob-lang,\
-				$(BTEST_OB_LANGUAGES) emacs-lisp shell org,\
-				$(lst-ob-lang)))) \
-		org-test-select-re "$(BTEST_RE)" \
-		)' \
-	  -l org-loaddefs.el \
-	  -l cl -l testing/org-test.el \
-	  -l ert -l org -l ox -l ol \
-	  $(foreach req,$(BTEST_EXTRA),$(req-extra)) \
+BTEST = $(BATCH) $(BTEST_INIT) 						    \
+	  -l org-batch-test-init 					    \
+	  --eval '(setq 						    \
+		org-batch-test t 					    \
+		org-babel-load-languages 				    \
+		  (quote ($(foreach ob-lang,				    \
+				$(BTEST_OB_LANGUAGES) emacs-lisp shell org, \
+				$(lst-ob-lang)))) 			    \
+		org-test-select-re "$(BTEST_RE)" 			    \
+		)' 							    \
+	  -l org-loaddefs.el 						    \
+	  -l testing/org-test.el 					    \
+	  -l ert -l org -l ox -l ol 					    \
+	  $(foreach req,$(BTEST_EXTRA),$(req-extra)) 			    \
 	  --eval '(org-test-run-batch-tests org-test-select-re)'
 
 # Running a plain emacs with no config and this Org mode loaded.  This
