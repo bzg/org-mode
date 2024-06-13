a2ae06986981e2588e0692c6b7bb048be7c2882d
Ihor Radchenko <yantar92@posteo.net>
2024-06-13 15:22:28 +0200

    ob-*: Do not emit redundant messages about code block evaluation
    * lisp/ob-C.el (org-babel-C-header-to-C):
    * lisp/ob-awk.el (org-babel-execute:awk):
    * lisp/ob-ditaa.el (org-babel-execute:ditaa):
    * lisp/ob-gnuplot.el (org-babel-execute:gnuplot):
    (org-babel-prep-session:gnuplot):
    * lisp/ob-groovy.el (org-babel-execute:groovy):
    * lisp/ob-maxima.el (org-babel-execute:maxima):
    * lisp/ob-screen.el (org-babel-execute:screen):
    * lisp/ob-sed.el (org-babel-execute:sed):
    `org-babel-execute-src-block' already emits status messages about code
    block evaluation.  Doing it from inside individual babel backends is
    thus redundant.

--- lisp/ob-ditaa.el
+++ lisp/ob-ditaa.el
@@ -113,9 +113,8 @@ This function is called by `org-babel-execute-src-block'."
     (unless (file-exists-p org-ditaa-jar-path)
       (error "Could not find ditaa.jar at %s" org-ditaa-jar-path))
     (with-temp-file in-file (insert body))
-    (unless noninteractive (message cmd))
     (shell-command cmd)
-    (when pdf-cmd (unless noninteractive (message pdf-cmd)) (shell-command pdf-cmd))
+    (when pdf-cmd (shell-command pdf-cmd))
     nil)) ;; signal that output has already been written to file
 
 (defun org-babel-prep-session:ditaa (_session _params)
