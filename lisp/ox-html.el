d4a13e6a062771ccc873a72c694da7f338bb1f5d
Ihor Radchenko <yantar92@posteo.net>
2024-12-22 13:14:07 +0100

    org-html-format-latex: Store LaTeX images alongside the HTML output
    * lisp/ox-html.el (org-html-format-latex): Use output file dir to
    store LaTeX preview images.
    * etc/ORG-NEWS (ox-latex: LaTeX images are now stored alongside the exported =.html= file):
    Announce the change.
    
    Reported-by: Pranshu Sharma <pranshu@bauherren.ovh>
    Link: https://orgmode.org/list/87r05z9alk.fsf@localhost

--- lisp/ox-html.el
+++ lisp/ox-html.el
@@ -3063,7 +3063,7 @@ INFO is a plist containing export properties."
 	      (concat (file-name-as-directory org-preview-latex-image-directory)
 		      (file-name-sans-extension
 		       (file-name-nondirectory bfn)))
-	      cache-dir (file-name-directory bfn))
+	      cache-dir (file-name-directory (plist-get info :output-file)))
 	;; Re-create LaTeX environment from original buffer in
 	;; temporary buffer so that dvipng/imagemagick can properly
 	;; turn the fragment into an image.
