7fac67e2e0a087319109ac222fa8e385740b29a0
Ihor Radchenko <yantar92@posteo.net>
2025-04-20 19:31:45 +0200

    org-latex-preview: Disable previews in untrusted buffers
    * lisp/org.el (org-latex-preview): Make use of the new
    `trusted-content-p' that gives more control to users over which
    buffers should be trusted.
    
    Link: https://debbugs.gnu.org/cgi/bugreport.cgi?bug=74879#74

--- lisp/org.el
+++ lisp/org.el
@@ -16201,7 +16201,16 @@ fragments in the buffer."
   (interactive "P")
   (cond
    ((not (display-graphic-p)) nil)
-   ((and untrusted-content (not org--latex-preview-when-risky)) nil)
+   ((and
+     ;; Since Emacs 30, prefer `trusted-content-p' API.
+     (not (fboundp 'trusted-content-p))
+     untrusted-content
+     (not org--latex-preview-when-risky))
+    nil)
+   ((and (fboundp 'trusted-content-p) ; Available since Emacs 30
+         (not (trusted-content-p))
+         (not org--latex-preview-when-risky))
+    nil)
    ;; Clear whole buffer.
    ((equal arg '(64))
     (org-clear-latex-preview (point-min) (point-max))
