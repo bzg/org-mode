0e638c7e4da6d13fd925d7cb2294ecafcb32066f
Morgan Smith <Morgan.J.Smith@outlook.com>
2025-10-31 13:25:13 -0400

    org-agenda-filter-completion-function: Return correct boundary
    * lisp/org-agenda.el (org-agenda-filter-completion-function):
    Previously a string was returned when the boundry should be a number.
    Now it is a number.
    
    Reported-by: "martin" <law@martinmarshall.com>
    Link: https://list.orgmode.org/CAOFDpFWvY6ouZRZ3Qkn7AqLYvPGS3exX6Qa-FTgFj1C7oAkEnQ@mail.gmail.com/

--- lisp/org-agenda.el
+++ lisp/org-agenda.el
@@ -8219,7 +8219,7 @@ which see."
       (`lambda (assoc string table)) ;exact match?
       (`(boundaries . ,suffix)
        (let ((end (if (string-match "[-+<>=]" suffix)
-                      (match-string 0 suffix)
+                      (match-beginning 0)
                     (length suffix))))
          `(boundaries ,(or begin 0) . ,end)))
       (`nil
