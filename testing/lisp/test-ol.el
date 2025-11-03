bzg/org-mode

commit 2556f75bf34558979fcf52ac50754dfce85458b7

DND: Follow link format defined in org-attach-store-link-p
* lisp/org-attach.el (org-attach-attach): Return (LINK DESCRIPTION)
list to the attachment.
* lisp/ol.el (org-link-get-description):
(org-link-make-string-for-buffer): Factor out link cleanup logic from
`org-insert-link'.
(org-insert-link): Use the new helper functions.
* lisp/org.el (org--image-yank-media-handler):
(org--dnd-attach-file):
(org--dnd-local-file-handler): Make use of `org-attach-attach' return
value.  Obey file: link cleanups better by deferring to
`org-link-make-string-for-buffer' instead of partially emulating it
via `org-link--normalize-filename'.
* etc/ORG-NEWS (~org-attach-attach~ now returns a link to file stored):
(New functions exposing link formatting done by ~org-insert-link~):
Announce the changes.

lisp/ol.el
@@ -1435,6 +1435,45 @@ E.g. \"%C3%B6\" becomes the German o-Umlaut."
      (concat (make-string (/ (- (match-end 1) (match-beginning 1)) 2) ?\\)))
    link nil t 1))
 
+(defun org-link-get-description (link &optional default-description)
+  "Return description for LINK.
+When link has :insert-description link property defined, set description
+accordingly.  Otherwise, try `org-link-make-description-function'.
+When the above two methods are not defined, return DEFAULT-DESCRIPTION."
+  (let* ((abbrevs org-link-abbrev-alist-local)
+         (all-prefixes (append (mapcar #'car abbrevs)
+			       (mapcar #'car org-link-abbrev-alist)
+			       (org-link-types)))
+         (type
+          (cond
+           ((and all-prefixes
+                 (string-match (rx-to-string `(: string-start (submatch (or ,@all-prefixes)) ":")) link))
+            (match-string 1 link))
+           ((file-name-absolute-p link) "file")
+           ((string-match "\\`\\.\\.?/" link) "file"))))
+    (cond
+     ((org-link-get-parameter type :insert-description)
+      (let ((def (org-link-get-parameter type :insert-description)))
+        (condition-case nil
+            (cond
+             ((stringp def) def)
+             ((functionp def)
+              (funcall def link default-description)))
+          (error
+           (message "Can't get link description from org link parameter `:insert-description': %S"
+                    def)
+           (sit-for 2)
+           nil))))
+     (org-link-make-description-function
+      (condition-case nil
+          (funcall org-link-make-description-function link default-description)
+        (error
+         (message "Can't get link description from %S"
+                  org-link-make-description-function)
+         (sit-for 2)
+         nil)))
+     (t default-description))))
+
 (defun org-link-make-string (link &optional description)
   "Make a bracket link, consisting of LINK and DESCRIPTION.
 LINK is escaped with backslashes for inclusion in buffer."
@@ -1457,6 +1496,70 @@ LINK is escaped with backslashes for inclusion in buffer."
       (format "[[%s]%s]"
 	      (org-link-escape link)
 	      (if description (format "[%s]" description) "")))))
+
+(defun org-link-make-string-for-buffer (link &optional description interactive-p)
+  "Format LINK with DESCRIPTION for current buffer.
+When BUFFER is nil, format LINK for current buffer.
+When DESCRIPTION is nil, set it according to :insert-description link
+parameter or `org-link-make-description-function'.
+When current buffer is a file buffer and link points to that file,
+strip the file name from the link.
+Make sure that file: link follows `org-link-file-path-type'.
+Strip <..> brackets from LINK, if it is a plain link.
+
+When INTERACTIVE-P is non-nil, prompt user for description.
+
+Return link with description, as a string."
+  (when (and (string-match org-link-plain-re link)
+	     (not (string-match org-ts-regexp link)))
+    ;; URL-like link, normalize the use of angular brackets.
+    (setq link (org-unbracket-string "<" ">" link)))
+
+  ;; Check if we are linking to the current file with a search
+  ;; option If yes, simplify the link by using only the search
+  ;; option.
+  (when (and (buffer-file-name (buffer-base-buffer))
+	     (let ((case-fold-search nil))
+	       (string-match "\\`file:\\(.+?\\)::" link)))
+    (let ((path (match-string-no-properties 1 link))
+	  (search (substring-no-properties link (match-end 0))))
+      (save-match-data
+	(when (equal (file-truename (buffer-file-name (buffer-base-buffer)))
+		     (file-truename path))
+	  ;; We are linking to this same file, with a search option
+	  (setq link search)))))
+
+  ;; Check if we can/should use a relative path.  If yes, simplify
+  ;; the link.
+  (let ((case-fold-search nil))
+    (when (string-match "\\`\\(file\\|docview\\):" link)
+      (let* ((type (match-string-no-properties 0 link))
+	     (path-start (match-end 0))
+	     (search (and (string-match "::\\(.*\\)\\'" link path-start)
+			  (match-string 1 link)))
+	     (path
+	      (if search
+		  (substring-no-properties
+		   link path-start (match-beginning 0))
+		(substring-no-properties link (match-end 0))))
+             ;; file:::search
+             (path (if (org-string-nw-p path) path (buffer-file-name)))
+	     (origpath path))
+	(setq path (org-link--normalize-filename path org-link-file-path-type))
+	(setq link (concat type path (and search (concat "::" search))))
+	(when (equal description origpath)
+	  (setq description path)))))
+
+  (unless description
+    (setq description (org-link-get-description link)))
+
+  (when interactive-p
+    (setq description (read-string "Description: " description)))
+
+  (unless (org-string-nw-p description)
+    (setq description nil))
+
+  (org-link-make-string link description))
 
 (defun org-store-link-functions ()
   "List of functions that are called to create and store a link.
@@ -2661,7 +2764,7 @@ non-interactively, don't allow editing the default description."
 	 (all-prefixes (append (mapcar #'car abbrevs)
 			       (mapcar #'car org-link-abbrev-alist)
 			       (org-link-types)))
-         entry link-original)
+         entry)
     (cond
      (link-location)		      ; specified by arg, just use it.
      ((org-in-regexp org-link-bracket-re 1)
@@ -2755,93 +2858,18 @@ non-interactively, don't allow editing the default description."
       (or entry (push link org-link--insert-history))
       (setq desc (or desc (nth 1 entry)))))
 
-    (setq link-original link)
-    (when (and (string-match org-link-plain-re link)
-	       (not (string-match org-ts-regexp link)))
-      ;; URL-like link, normalize the use of angular brackets.
-      (setq link (org-unbracket-string "<" ">" link)))
-
-    ;; Check if we are linking to the current file with a search
-    ;; option If yes, simplify the link by using only the search
-    ;; option.
-    (when (and (buffer-file-name (buffer-base-buffer))
-	       (let ((case-fold-search nil))
-		 (string-match "\\`file:\\(.+?\\)::" link)))
-      (let ((path (match-string-no-properties 1 link))
-	    (search (substring-no-properties link (match-end 0))))
-	(save-match-data
-	  (when (equal (file-truename (buffer-file-name (buffer-base-buffer)))
-		       (file-truename path))
-	    ;; We are linking to this same file, with a search option
-	    (setq link search)))))
-
-    ;; Check if we can/should use a relative path.  If yes, simplify
-    ;; the link.
-    (let ((case-fold-search nil))
-      (when (string-match "\\`\\(file\\|docview\\):" link)
-	(let* ((type (match-string-no-properties 0 link))
-	       (path-start (match-end 0))
-	       (search (and (string-match "::\\(.*\\)\\'" link path-start)
-			    (match-string 1 link)))
-	       (path
-		(if search
-		    (substring-no-properties
-		     link path-start (match-beginning 0))
-		  (substring-no-properties link (match-end 0))))
-               ;; file:::search
-               (path (if (org-string-nw-p path) path (buffer-file-name)))
-	       (origpath path))
-	  (setq path (org-link--normalize-filename
-                      path
-                      (if (equal complete-file '(16))
-                          'absolute
-                        org-link-file-path-type)))
-	  (setq link (concat type path (and search (concat "::" search))))
-	  (when (equal desc origpath)
-	    (setq desc path)))))
-
-    (let* ((type
-            (cond
-             ((and all-prefixes
-                   (string-match (rx-to-string `(: string-start (submatch (or ,@all-prefixes)) ":")) link))
-              (match-string 1 link))
-             ((file-name-absolute-p link) "file")
-             ((string-match "\\`\\.\\.?/" link) "file")))
-           (initial-input
-            (cond
-             (description)
-             (desc)
-             ((org-link-get-parameter type :insert-description)
-              (let ((def (org-link-get-parameter type :insert-description)))
-                (condition-case nil
-                    (cond
-                     ((stringp def) def)
-                     ((functionp def)
-                      (funcall def link desc)))
-                  (error
-                   (message "Can't get link description from org link parameter `:insert-description': %S"
-                            def)
-                   (sit-for 2)
-                   nil))))
-             (org-link-make-description-function
-              (condition-case nil
-                  (funcall org-link-make-description-function link desc)
-                (error
-                 (message "Can't get link description from %S"
-                          org-link-make-description-function)
-                 (sit-for 2)
-                 nil))))))
-      (setq desc (if (called-interactively-p 'any)
-                     (read-string "Description: " initial-input)
-                   initial-input)))
+    (let ((org-link-file-path-type
+           (if (equal complete-file '(16))
+               'absolute
+             org-link-file-path-type)))
+      (insert (org-link-make-string-for-buffer
+               link (or description desc)
+               (called-interactively-p 'any))))
 
     (when (funcall (if (equal complete-file '(64)) 'not 'identity)
                    (not org-link-keep-stored-after-insertion))
-      (setq org-stored-links (delq (assoc link-original org-stored-links)
-                                   org-stored-links)))
-    (unless (org-string-nw-p desc) (setq desc nil))
+      (setq org-stored-links (delq (assoc link org-stored-links) org-stored-links)))
     (when remove (apply #'delete-region remove))
-    (insert (org-link-make-string link desc))
     ;; Redisplay so as the new link has proper invisible characters.
     (sit-for 0)))
 

lisp/org-attach.el
@@ -586,7 +586,13 @@ if it would overwrite an existing filename."
   "Move/copy/link FILE into the attachment directory of the current outline node.
 If VISIT-DIR is non-nil, visit the directory with `dired'.
 METHOD may be `cp', `mv', `ln', `lns' or `url' default taken from
-`org-attach-method'."
+`org-attach-method'.
+
+Return a list (LINK DESCRIPTION), representing the file stored.
+When `org-attach-store-link-p' is non-nil, LINK and DESCRIPTION will
+be the same as in the link stored.
+When `org-attach-store-link-p' is nil, LINK will be an attachment: link
+and DESCRIPTION be the file name."
   (interactive
    (list
     (read-file-name "File to keep as an attachment: "
@@ -599,7 +605,8 @@ METHOD may be `cp', `mv', `ln', `lns' or `url' default taken from
   (setq method (or method org-attach-method))
   (when (file-directory-p file)
     (setq file (directory-file-name file)))
-  (let ((basename (file-name-nondirectory file)))
+  (let ((basename (file-name-nondirectory file))
+        link description)
     (let* ((attach-dir (org-attach-dir 'get-create))
            (attach-file (expand-file-name basename attach-dir)))
       (cond
@@ -618,20 +625,25 @@ METHOD may be `cp', `mv', `ln', `lns' or `url' default taken from
       (run-hook-with-args 'org-attach-after-change-hook attach-dir)
       (org-attach-tag)
       (cond ((eq org-attach-store-link-p 'attached)
-	     (push (list (concat "attachment:" (file-name-nondirectory attach-file))
-			 (file-name-nondirectory attach-file))
-		   org-stored-links))
+             (setq link (concat "attachment:" (file-name-nondirectory attach-file))
+                   description (file-name-nondirectory attach-file))
+	     (push (list link description) org-stored-links))
             ((eq org-attach-store-link-p t)
-             (push (list (concat "file:" file)
-			 (file-name-nondirectory file))
-		   org-stored-links))
+             (setq link (concat "file:" file)
+                   description (file-name-nondirectory file))
+             (push (list link description) org-stored-links))
 	    ((eq org-attach-store-link-p 'file)
-	     (push (list (concat "file:" attach-file)
-			 (file-name-nondirectory attach-file))
-		   org-stored-links)))
+             (setq link (concat "file:" attach-file)
+                   description (file-name-nondirectory attach-file))
+	     (push (list link description) org-stored-links))
+            (t
+             ;; Do not save link, just return.
+             (setq link (concat "attachment:" (file-name-nondirectory attach-file))
+                   description (file-name-nondirectory attach-file))))
       (if visit-dir
           (dired attach-dir)
-        (message "File %S is now an attachment" basename)))))
+        (message "File %S is now an attachment" basename))
+      (list link description))))
 
 (defun org-attach-attach-cp ()
   "Attach a file by copying it."

lisp/org.el
@@ -20642,12 +20642,11 @@ end."
         (insert data)))
     (insert
      (if (not (eq org-yank-image-save-method 'attach))
-         (org-link-make-string (concat "file:" (org-link--normalize-filename absname)))
+         (org-link-make-string-for-buffer (concat "file:" absname))
        (progn
          (require 'org-attach)
-         (org-attach-attach absname nil 'mv)
-         (org-link-make-string (concat "attachment:" filename)))))
-    ))
+         (apply #'org-link-make-string-for-buffer
+                (org-attach-attach absname nil 'mv)))))))
 
 ;; I cannot find a spec for this but
 ;; https://indigo.re/posts/2021-12-21-clipboard-data.html and pcmanfm
@@ -20779,9 +20778,9 @@ in which case, space is inserted."
       (`open (dnd-open-local-file url action))
       (`file-link
        (let ((filename (dnd-get-local-file-name url)))
-         (insert (org-link-make-string
-                  (concat "file:" (org-link--normalize-filename filename)))
-                 separator))))))
+         (insert
+          (org-link-make-string-for-buffer filename)
+          separator))))))
 
 (defun org--dnd-attach-file (url action separator)
   "Attach filename given by URL using method pertaining to ACTION.
@@ -20808,31 +20807,29 @@ SEPARATOR is the string to insert after each link."
                             (?l "hard link" ln)
                             (?s "symbolic link" lns))))
                    ('private (or org-yank-dnd-default-attach-method
-                                 org-attach-method)))))
+                                 org-attach-method))))
+         link description)
     (if separatep
         (progn
           (unless (file-directory-p org-yank-image-save-method)
             (make-directory org-yank-image-save-method t))
-          (funcall
-           (pcase method
-             ('cp #'copy-file)
-             ('mv #'rename-file)
-             ('ln #'add-name-to-file)
-             ('lns #'make-symbolic-link))
-           filename
-           (expand-file-name (file-name-nondirectory filename)
-                             org-yank-image-save-method)))
-      (org-attach-attach filename nil method))
+          (let ((stored-filename
+                 (expand-file-name
+                  (file-name-nondirectory filename)
+                  org-yank-image-save-method)))
+            (funcall
+             (pcase method
+               ('cp #'copy-file)
+               ('mv #'rename-file)
+               ('ln #'add-name-to-file)
+               ('lns #'make-symbolic-link))
+             filename stored-filename)
+            (setq link (concat "file:" stored-filename))))
+      (let ((link-pair (org-attach-attach filename nil method)))
+        (setq link (car link-pair)
+              description (cdr link-pair))))
     (insert
-     (org-link-make-string
-      (concat (if separatep
-                  "file:"
-                "attachment:")
-              (if separatep
-                  (org-link--normalize-filename
-                   (expand-file-name (file-name-nondirectory filename)
-                                     org-yank-image-save-method))
-                (file-name-nondirectory filename))))
+     (org-link-make-string-for-buffer link description)
      separator)
     'private))
 
@@ -20857,11 +20854,10 @@ When NEED-NAME is nil, the drop is complete."
           (`open (expand-file-name (make-temp-name "emacs.") temporary-file-directory))
           (`file-link (read-file-name "Write file to: " nil nil nil filename))))
     (pcase org--dnd-xds-method
-      (`attach (insert (org-link-make-string
+      (`attach (insert (org-link-make-string-for-buffer
                         (concat "attachment:" (file-name-nondirectory filename)))))
-      (`file-link (insert (org-link-make-string
-                           (concat "file:"
-                                   (org-link--normalize-filename filename)))))
+      (`file-link (insert (org-link-make-string-for-buffer
+                           (concat "file:" filename))))
       (`open (find-file filename)))
     (setq-local org--dnd-xds-method nil)))
 

commit cca46a167646747f91fc51491989a5c05cadea08

Fix regression introduced in 2556f75bf
* lisp/ol.el (org-insert-link): Insert updated link after removing
link at point, when we are updating link at point, not before.
* testing/lisp/test-ol.el (test-org-link/insert-link): New test case.

Reported-by: Bruno Cardoso <cardoso.bc@gmail.com>
Link: https://orgmode.org/list/87v7jr9op3.fsf@gmail.com

lisp/ol.el
@@ -2858,18 +2858,20 @@ non-interactively, don't allow editing the default description."
       (or entry (push link org-link--insert-history))
       (setq desc (or desc (nth 1 entry)))))
 
-    (let ((org-link-file-path-type
-           (if (equal complete-file '(16))
-               'absolute
-             org-link-file-path-type)))
-      (insert (org-link-make-string-for-buffer
-               link (or description desc)
-               (called-interactively-p 'any))))
+    (let (new-link)
+      (let ((org-link-file-path-type
+             (if (equal complete-file '(16))
+                 'absolute
+               org-link-file-path-type)))
+        (setq new-link (org-link-make-string-for-buffer
+                        link (or description desc)
+                        (called-interactively-p 'any))))
 
-    (when (funcall (if (equal complete-file '(64)) 'not 'identity)
-                   (not org-link-keep-stored-after-insertion))
-      (setq org-stored-links (delq (assoc link org-stored-links) org-stored-links)))
-    (when remove (apply #'delete-region remove))
+      (when (funcall (if (equal complete-file '(64)) 'not 'identity)
+                     (not org-link-keep-stored-after-insertion))
+        (setq org-stored-links (delq (assoc link org-stored-links) org-stored-links)))
+      (when remove (apply #'delete-region remove))
+      (insert new-link))
     ;; Redisplay so as the new link has proper invisible characters.
     (sit-for 0)))
 

testing/lisp/test-ol.el
@@ -852,7 +852,13 @@ API in `org-link-parameters'.  Used in test
       (org-insert-link nil "file:::going")
       (should
        (equal (buffer-string)
-              (format "[[file:%s::going]]" file-name))))))
+              (format "[[file:%s::going]]" file-name)))))
+  ;; Test altering the link at point
+  (org-test-with-temp-text "[[file:file.org][<point>description]]"
+    (cl-letf (((symbol-function #'read-string)
+               (lambda (&rest _) "file:file.org")))
+      (org-insert-link nil nil "altered description"))
+    (should (equal (buffer-string) "[[file:file.org][altered description]]"))))
 
 (provide 'test-ol)
 ;;; test-ol.el ends here