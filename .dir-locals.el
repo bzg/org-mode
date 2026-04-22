a9bde91331e44466493d138c9a5cc997d557a5c3
Morgan Smith <Morgan.J.Smith@outlook.com>
2026-04-22 10:24:23 -0400

    .dir-locals.el: Set `elisp-flymake-byte-compile-load-path' for tests
    This will allow flymake to display diagnostics when editing the test
    files.
    
    Also set `indent-tabs-mode' to nil in `lisp-data-mode' as well since
    newer Emacs open the ".dir-locals.el" file in that mode now.
    
    After using `add-dir-local-variable', Emacs reformated the file for
    me.
    
    .dir-locals.el: Set `elisp-flymake-byte-compile-load-path' for the
    "testing/lisp" directory.  Set `indent-tabs-mode' for
    `lisp-data-mode'.  Reformat.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,22 +1,21 @@
-;;; Directory Local Variables            -*- no-byte-compile: t; -*-
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
-  (indent-tabs-mode . t)
-  (tab-width . 8)
-  (fill-column . 70)
-  (sentence-end-double-space . t))
- (emacs-lisp-mode
-  (indent-tabs-mode))
- (org-mode
-  (indent-tabs-mode)
-  (org-adapt-indentation)
-  (org-src-content-indentation . 0)
-  (org-footnote-auto-adjust . t)
-  (org-footnote-auto-label . t)
-  (org-footnote-define-inline . nil)
-  (org-footnote-section . "Footnotes")
-  (org-hide-emphasis-markers . nil)))
+((nil . ((indent-tabs-mode . t)
+         (tab-width . 8)
+         (fill-column . 70)
+         (sentence-end-double-space . t)))
+ (emacs-lisp-mode . ((indent-tabs-mode . nil)))
+ (lisp-data-mode . ((indent-tabs-mode . nil)))
+ (org-mode . ((indent-tabs-mode . nil)
+              (org-adapt-indentation . nil)
+              (org-src-content-indentation . 0)
+              (org-footnote-auto-adjust . t)
+              (org-footnote-auto-label . t)
+              (org-footnote-define-inline . nil)
+              (org-footnote-section . "Footnotes")
+              (org-hide-emphasis-markers . nil)))
+ ("testing/lisp" . ((emacs-lisp-mode . ((elisp-flymake-byte-compile-load-path "./" "../../lisp/"))))))
 
 
 
